4a7c4ed427c681f4302346b086f056c594b157b6
isabel <isabel@isabelroses.com>
2023-07-08 10:34:48 +0100

    refactor: move to option

---
+++ modules/common/options/device.nix
@@ -0,0 +1,50 @@
+{lib, ...}:
+with lib; {
+  options.modules.device = {
+    type = mkOption {
+      type = types.enum ["laptop" "desktop" "server" "hybrid" "lite"];
+    };
+
+    # the type of cpu your system has - vm and regular cpus currently do not differ
+    # as I do not work with vms, but they have been added for forward-compatibility
+    cpu = mkOption {
+      type = types.enum ["pi" "intel" "vm-intel" "amd" "vm-amd" null];
+      default = null;
+    };
+
+    gpu = mkOption {
+      type = types.enum ["amd" "intel" "nvidia" null];
+      default = null;
+      description = "the manifacturer/type of the primary system gpu";
+    };
+
+    monitors = mkOption {
+      type = types.listOf types.string;
+      default = [];
+      description = ''
+        this does not affect any drivers and such, it is only necessary for
+        declaring things like monitors in window manager configurations
+        you can avoid declaring this, but I'd rather if you did declare
+      '';
+    };
+
+    # bluetooth is an insecure protocol if left unchedked, so while this defaults to true
+    # but the bluetooth.enable option does and should not.
+    hasBluetooth = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Whether or not the system has bluetooth support";
+    };
+
+    hasSound = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Whether the system has sound support (usually true except for servers)";
+    };
+
+    keyboard = mkOption {
+      type = types.enum ["us" "gb"];
+      default = "gb";
+    };
+  };
+}
---
+++ modules/common/options/programs.nix
@@ -0,0 +1,72 @@
+{lib, ...}:
+with lib; {
+  # this module provides overrides for certain defaults and lets you set
+  # default programs for referencing in other config files.
+  options.modules = {
+    services = {
+      jellyfin.enable = mkEnableOption "Enables the jellyfin service";
+      cloudflare = {
+        enable = mkEnableOption "Enables cloudflared tunnels";
+        token = mkOption {
+          type = types.str;
+          default = "";
+          description = "The token which is used by cloudflared tunnels";
+        };
+      };
+    };
+
+    programs = {
+      # "override" is a simple option that sets the programs' state to the oppossite of their default
+      override = {
+        # override basic desktop applications
+        # an example override for the libreoffice program
+        # if set to true, libreoffice module will not be enabled as it is by default
+        libreoffice = mkEnableOption "Override Libreoffice suite";
+      };
+
+      # TODO: turn those into overrides
+      # load GUI and CLI programs by default, but check if those overrides are enabled
+      # so that they can be disabled at will
+      cli = {
+        enable = mkEnableOption "Enable CLI programs";
+      };
+      gui = {
+        enable = mkEnableOption "Enable GUI programs";
+      };
+
+      git = {
+        signingKey = mkOption {
+          type = types.str;
+          default = "";
+          description = "The default gpg key used for signing commits";
+        };
+      };
+
+      # default program options
+      default = {
+        # what program should be used as the default terminal
+        # do note this is NOT the command, but just the name. i.e setting footclient will
+        # not work because the program name will be references as "foot" in the rest of the config
+        terminal = mkOption {
+          type = types.enum ["alacritty" "kitty" "wezterm"];
+          default = "alacritty";
+        };
+
+        fileManager = mkOption {
+          type = types.enum ["thunar" "dolphin" "nemo"];
+          default = "thunar";
+        };
+
+        browser = mkOption {
+          type = types.enum ["firefox" "chromium"];
+          default = "chromium";
+        };
+
+        editor = mkOption {
+          type = types.enum ["nvim" "code"];
+          default = "nvim";
+        };
+      };
+    };
+  };
+}
