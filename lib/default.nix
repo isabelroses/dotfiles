7b4e620112dd247d54fc68a664d8d6bedaed7bd5
isabel <isabel@isabelroses.com>
2023-07-27 13:37:24 +0100

    refctor(lib): update and refaine

--- lib/default.nix
+++ lib/default.nix
@@ -4,28 +4,9 @@
   inputs,
   ...
 }: let
-  inherit (lib) lists mapAttrsToList filterAttrs hasSuffix;
-
   builders = import ./builders.nix {inherit lib inputs nixpkgs;};
   services = import ./services.nix {inherit lib;};
   validators = import ./validators.nix {inherit lib;};
-
-  # filter files that have the .nix suffix
-  filterNixFiles = k: v: v == "regular" && hasSuffix ".nix" k;
-
-  # import files that are selected by filterNixFiles
-  importNixFiles = path:
-    (lists.forEach (mapAttrsToList (name: _: path + ("/" + name))
-        (filterAttrs filterNixFiles (builtins.readDir path))))
-    import;
-
-  # return an int (1/0) based on boolean value
-  boolToNum = bool:
-    if bool
-    then 1
-    else 0;
+  helpers = import ./helpers.nix {inherit lib;};
 in
-  nixpkgs.lib.extend (
-    self: super:
-      {inherit filterNixFiles importNixFiles boolToNum;} // builders // services // validators
-  )
+  nixpkgs.lib.extend (_: _: builders // services // validators // helpers)
