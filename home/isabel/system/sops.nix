1fc939f9d897c81fb6a2412c94618bcff4a5bcc4
isabel <isabel@isabelroses.com>
2023-07-27 19:52:30 +0100

    style: format

--- home/isabel/system/sops.nix
+++ home/isabel/system/sops.nix
@@ -9,29 +9,29 @@
   sops = {
     gnupg.home = config.programs.gpg.homedir;
     defaultSopsFile = ./secrets.yaml;
-    
-    secrets = let 
+
+    secrets = let
       homedir = config.home.homeDirectory;
     in {
-        git-credentials.path = "${homedir}/.git-credentials";
+      git-credentials.path = "${homedir}/.git-credentials";
 
-        # git ssh keys
-        gh-key.path = "${homedir}/.ssh/github";
-        gh-key-pub.path = "${homedir}/.ssh/github.pub";
-        aur-key.path = "${homedir}/.ssh/aur";
-        aur-key-pub.path = "${homedir}/.ssh/aur.pub";
+      # git ssh keys
+      gh-key.path = "${homedir}/.ssh/github";
+      gh-key-pub.path = "${homedir}/.ssh/github.pub";
+      aur-key.path = "${homedir}/.ssh/aur";
+      aur-key-pub.path = "${homedir}/.ssh/aur.pub";
 
-        # ORACLE vps'
-        openvpn-key.path = "${homedir}/.ssh/openvpn";
-        luz-key.path = "${homedir}/.ssh/luz";
-        edalyn-key.path = "${homedir}/.ssh/edalyn";
-        king-key.path = "${homedir}/.ssh/king";
+      # ORACLE vps'
+      openvpn-key.path = "${homedir}/.ssh/openvpn";
+      luz-key.path = "${homedir}/.ssh/luz";
+      edalyn-key.path = "${homedir}/.ssh/edalyn";
+      king-key.path = "${homedir}/.ssh/king";
 
-        # my local servers / clients
-        alpha-key.path = "${homedir}/.ssh/alpha";
-        alpha-key-pub.path = "${homedir}/.ssh/alpha.pub";
-        hydra-key.path = "${homedir}/.ssh/hydra";
-        hydra-key-pub.path = "${homedir}/.ssh/hydra.pub";
-      };
+      # my local servers / clients
+      alpha-key.path = "${homedir}/.ssh/alpha";
+      alpha-key-pub.path = "${homedir}/.ssh/alpha.pub";
+      hydra-key.path = "${homedir}/.ssh/hydra";
+      hydra-key-pub.path = "${homedir}/.ssh/hydra.pub";
+    };
   };
 }
--- modules/common/system/smb/recive/default.nix
+++ modules/common/system/smb/recive/default.nix
@@ -1,4 +1,4 @@
-_:{
+_: {
   imports = [
     ./media.nix
     ./general.nix
