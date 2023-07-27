994a786738aa0e83d3461f9ea08294bff3b3b657
isabel <isabel@isabelroses.com>
2023-07-27 13:33:37 +0100

    feat: add vscode

--- home/isabel/programs/gui/confs/editors/default.nix
+++ home/isabel/programs/gui/confs/editors/default.nix
@@ -2,5 +2,6 @@ _: {
   imports = [
     ./micro
     ./nvim
+    ./vscode
   ];
 }
---
+++ home/isabel/programs/gui/confs/editors/vscode/default.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  pkgs,
+  osConfig,
+  ...
+}:
+with lib; let
+  device = osConfig.modules.device;
+  programs = osConfig.modules.programs;
+  sys = osConfig.modules.system;
+  acceptedTypes = ["laptop" "desktop" "hybrid"];
+in {
+  config = mkIf (builtins.elem device.type acceptedTypes && programs.gui.enable && sys.video.enable) {
+    programs.vscode = {
+      enable = true;
+      package = pkgs.vscodium;
+      extensions = with pkgs.vscode-extensions; [
+        bradlc.vscode-tailwindcss
+        catppuccin.catppuccin-vsc-icons
+        catppuccin.catppuccin-vsc
+        eamodio.gitlens
+        github.copilot
+        github.vscode-pull-request-github
+        jnoortheen.nix-ide
+        kamadorueda.alejandra
+        rust-lang.rust-analyzer
+        devsense.phptools-vscode
+        shd101wyy.markdown-preview-enhanced
+        ms-vscode-remote.remote-ssh
+        ms-vscode.live-server
+      ];
+      mutableExtensionsDir = true;
+    };
+  };
+}
