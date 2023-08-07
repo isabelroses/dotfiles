32e20b5fc0e73df7d6e453f3cdca28e8f9195ff7
isabel <isabel@isabelroses.com>
2023-08-07 15:42:17 +0100

    more comits lol

--- hosts/hydra/default.nix
+++ hosts/hydra/default.nix
@@ -63,7 +63,6 @@ in {
         vscode-server.enable = true;
         cloudflare = {
           enable = true;
-          token = "eyJhIjoiNjA0YTQxZmY1ZDI1NzQ5MzllZmJiMWM1NWJhYzA5MGUiLCJ0IjoiN2U5MzZlMjAtODE4ZS00N2UwLWE2ZjEtYmIxOTE3NmY0ZTE4IiwicyI6IllUSXdZelJqTm1RdE5qY3lNeTAwWmpNMExUbGpOell0TmpKall6RmxPVEpsTWpkaiJ9";
         };
         jellyfin = {
           enable = true;
