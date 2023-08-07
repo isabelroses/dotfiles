79f0bd5650332a1202feb62e3c26be9256d579ca
isabel <isabel@isabelroses.com>
2023-08-07 16:09:47 +0100

    feat: give discord vencord

--- home/isabel/programs/gui/confs/discord.nix
+++ home/isabel/programs/gui/confs/discord.nix
@@ -14,6 +14,7 @@ in {
     home.packages = with pkgs; [
       ((discord.override {
           withOpenASAR = true;
+          withVencord = true;
         })
         .overrideAttrs (old: {
           libPath = old.libPath + ":${pkgs.libglvnd}/lib";
