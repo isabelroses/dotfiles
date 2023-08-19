f8516bd987ddbc5ae0ddec2361cc13890bb4c90e
isabel <isabel@isabelroses.com>
2023-08-19 12:58:24 +0100

    feat(jellyfin): non as docker run mode

--- modules/common/types/server/services/jellyfin/default.nix
+++ modules/common/types/server/services/jellyfin/default.nix
@@ -3,18 +3,30 @@
   lib,
   ...
 }:
-with lib; let
+let
+  inherit (lib) mkIf;
   device = config.modules.device;
-  cfg = config.modules.services;
+  cfg = config.modules.services.jellyfin;
   acceptedTypes = ["server" "hybrid"];
-in {
-  config = mkIf (builtins.elem device.type acceptedTypes && cfg.jellyfin.enable && cfg.jellyfin.asDockerContainer) {
-    modules.system.virtualization = {
+in { 
+  config = mkIf (builtins.elem device.type acceptedTypes && cfg.enable) {
+    # NOT docker
+    services = mkIf (!cfg.asDockerContainer){
+      jellyfin = {
+        enable = true;
+        group = "jellyfin";
+        user = "jellyfin";
+        openFirewall = true;
+      };
+    };
+
+    # with docker
+    modules.system.virtualization = mkIf (cfg.asDockerContainer) {
       enable = true;
       docker.enable = true;
     };
 
-    virtualisation.oci-containers.containers.jellyfin = {
+    virtualisation.oci-containers.containers.jellyfin = mkIf (cfg.asDockerContainer) {
       image = "lscr.io/linuxserver/jellyfin:latest";
       environment = {
         "PUID" = "1000";
