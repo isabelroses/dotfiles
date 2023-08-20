dee614c70176507d8a44bd9209d12a9be0dc179f
isabel <isabel@isabelroses.com>
2023-08-20 12:40:30 +0100

    style stuff

--- home/isabel/themes/global.nix
+++ home/isabel/themes/global.nix
@@ -6,14 +6,9 @@ in {
     pointerCursor = {
       package = cfg.pointerCursor.package;
       name = "${cfg.pointerCursor.name}";
-      size = 24;
+      size = cfg.pointerCursor.size;
       gtk.enable = true;
       x11.enable = true;
     };
   };
-
-  /*
-  i18n.inputMethod.enabled = "fcitx5";
-  i18n.inputMethod.fcitx5.addons = with pkgs; [fcitx5-mozc];
-  */
 }
