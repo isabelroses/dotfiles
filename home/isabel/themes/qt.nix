7acf368525410bcfc9d9be63a3988cf60767bdf1
isabel <isabel@isabelroses.com>
2023-08-21 08:56:08 +0100

    style: format

--- home/isabel/themes/qt.nix
+++ home/isabel/themes/qt.nix
@@ -36,39 +36,40 @@ in {
       cfg.qt.theme.package
     ];
 
-    home.sessionVariables = {
-      #QT_QPA_PLATFORMTHEME = "kvantum"; # can't be used alongside kvantum, nix above knows why
-      #QT_STYLE_OVERRIDE = "kvantum";
-      QT_AUTO_SCREEN_SCALE_FACTOR = "1";
-      QT_QPA_PLATFORM = "wayland;xcb";
-      QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-      DISABLE_QT5_COMPAT = "0";
+    home.sessionVariables =
+      {
+        #QT_QPA_PLATFORMTHEME = "kvantum"; # can't be used alongside kvantum, nix above knows why
+        #QT_STYLE_OVERRIDE = "kvantum";
+        QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+        QT_QPA_PLATFORM = "wayland;xcb";
+        QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+        DISABLE_QT5_COMPAT = "0";
 
-      # tell calibre to use the dark theme, because the light one hurts my eyes
-      CALIBRE_USE_DARK_PALETTE = "1";
-    }
-    // optionalAttrs cfg.useKvantum {
-      xdg.configFile."Kvantum/catppuccin/catppuccin.kvconfig".source = builtins.fetchurl {
-        url = "https://raw.githubusercontent.com/catppuccin/Kvantum/main/src/Catppuccin-Mocha-Blue/Catppuccin-Mocha-Blue.kvconfig";
-        sha256 = "";
+        # tell calibre to use the dark theme, because the light one hurts my eyes
+        CALIBRE_USE_DARK_PALETTE = "1";
+      }
+      // optionalAttrs cfg.useKvantum {
+        xdg.configFile."Kvantum/catppuccin/catppuccin.kvconfig".source = builtins.fetchurl {
+          url = "https://raw.githubusercontent.com/catppuccin/Kvantum/main/src/Catppuccin-Mocha-Blue/Catppuccin-Mocha-Blue.kvconfig";
+          sha256 = "";
+        };
+
+        xdg.configFile."Kvantum/catppuccin/catppuccin.svg".source = builtins.fetchurl {
+          url = "https://raw.githubusercontent.com/catppuccin/Kvantum/main/src/Catppuccin-Mocha-Blue/Catppuccin-Mocha-Blue.svg";
+          sha256 = "";
+        };
+
+        xdg.configFile."Kvantum/kvantum.kvconfig".source = (pkgs.formats.ini {}).generate "kvantum.kvconfig" {
+          General.Theme = "Catppuccin-Mocha-Mauve";
+        };
+
+        xdg.configFile."Kvantum/kvantum.kvconfig".text = ''
+          [General]
+          theme=catppuccin
+
+          [Applications]
+          catppuccin=qt5ct, org.kde.dolphin, org.kde.kalendar, org.qbittorrent.qBittorrent, hyprland-share-picker, dolphin-emu, Nextcloud, nextcloud
+        '';
       };
-
-      xdg.configFile."Kvantum/catppuccin/catppuccin.svg".source = builtins.fetchurl {
-        url = "https://raw.githubusercontent.com/catppuccin/Kvantum/main/src/Catppuccin-Mocha-Blue/Catppuccin-Mocha-Blue.svg";
-        sha256 = "";
-      };
-
-      xdg.configFile."Kvantum/kvantum.kvconfig".source = (pkgs.formats.ini {}).generate "kvantum.kvconfig" {
-        General.Theme = "Catppuccin-Mocha-Mauve";
-      };
-
-      xdg.configFile."Kvantum/kvantum.kvconfig".text = ''
-        [General]
-        theme=catppuccin
-
-        [Applications]
-        catppuccin=qt5ct, org.kde.dolphin, org.kde.kalendar, org.qbittorrent.qBittorrent, hyprland-share-picker, dolphin-emu, Nextcloud, nextcloud
-      '';
-    };
   };
 }
