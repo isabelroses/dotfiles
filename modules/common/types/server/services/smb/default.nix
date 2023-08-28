de013d1a1036fe70f2e67a9d85d82e1a6257dde7
isabel <isabel@isabelroses.com>
2023-08-28 17:59:34 +0100

    smb

---
+++ modules/common/types/server/services/smb/default.nix
@@ -0,0 +1,51 @@
+{
+  config,
+  lib,
+  ...
+}: let
+  inherit (lib) mkIf;
+  device = config.modules.device;
+  cfg = config.modules.services.smb;
+  acceptedTypes = ["server" "hybrid"];
+in {
+  config = mkIf (builtins.elem device.type acceptedTypes && cfg.enable) {
+    services = {
+      # https://nixos.wiki/wiki/Samba
+      # make shares visible for windows 10 clients
+      samba-wsdd = {
+        enable = true;
+        openFirewall = true;
+      };
+
+      samba = {
+        enable = true;
+        openFirewall = true;
+        securityType = "user";
+        extraConfig = ''
+          security = user
+          guest account = nobody
+        '';
+        shares = {
+          general = mkIf (cfg.host.general.enable) {
+            path = "/general";
+            browseable = "yes";
+            "read only" = "no";
+            "guest ok" = "yes";
+            "create mask" = "0644";
+            "directory mask" = "0755";
+          };
+          private = (cfg.host.media.enable) {
+            path = "/media";
+            browseable = "yes";
+            "read only" = "no";
+            "guest ok" = "no";
+            "create mask" = "0644";
+            "directory mask" = "0755";
+            "force user" = "isabel";
+            "force group" = "users";
+          };
+        };
+      };
+    };
+  };
+}
