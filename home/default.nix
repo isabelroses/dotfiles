521eb7ae3a83bb9508e4a5e8d7aefa907b82cc38
isabel <isabel@isabelroses.com>
2023-08-30 18:03:22 +0100

    etc

--- home/default.nix
+++ home/default.nix
@@ -7,13 +7,15 @@
   lib,
   ...
 }: let
+  inherit (config) modules;
+  env = modules.usrEnv;
   defaults = config.modules.programs.default;
 in {
-  home-manager = {
+  home-manager = lib.mkIf env.useHomeManager {
     verbose = true;
     useUserPackages = true;
     useGlobalPkgs = true;
-    backupFileExtension = "old";
+    backupFileExtension = "bak";
     extraSpecialArgs = {inherit inputs self inputs' self' defaults;};
     users = lib.genAttrs config.modules.system.users (name: ./${name});
   };
