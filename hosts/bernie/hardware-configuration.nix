50cf03b484e74c25acf8c1da06411f9dc5e56cdc
isabel <isabel@isabelroses.com>
2023-09-02 22:55:15 +0100

    fix bernies issues

--- hosts/bernie/hardware-configuration.nix
+++ hosts/bernie/hardware-configuration.nix
@@ -1,1 +1,18 @@
-_: {}
+{ lib, config, modulesPath, ... }:
+{
+  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
+  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi" ];
+  boot.initrd.kernelModules = [ "nvme" ];
+  boot.kernelModules = ["kvm-amd"];
+  fileSystems."/" = { device = "/dev/sda1"; fsType = "ext4"; };
+  
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.ens3.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
