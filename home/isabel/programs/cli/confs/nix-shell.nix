7bdeeb38dc987b1de8f4bb16819fccdfd5c8bba2
isabel <isabel@isabelroses.com>
2023-09-06 18:21:02 +0100

    use direnv system level

--- home/isabel/programs/cli/confs/nix-shell.nix
+++ home/isabel/programs/cli/confs/nix-shell.nix
@@ -4,38 +4,12 @@
   pkgs,
   ...
 }: {
-  config = lib.mkIf (osConfig.modules.usrEnv.programs.cli.enable) {
-    home = {
-      packages = with pkgs; [
-        alejandra
-        deadnix
-        nix-index
-        nix-tree
-        statix
-        nil
-      ];
-
-      sessionVariables = {
-        DIRENV_LOG_FORMAT = "";
-      };
-    };
-
-    programs.direnv = {
-      enable = true;
-      nix-direnv = {
-        enable = true;
-      };
-
-      stdlib = ''
-        : ''${XDG_CACHE_HOME:=$HOME/.cache}
-        declare -A direnv_layout_dirs
-        direnv_layout_dir() {
-          echo "''${direnv_layout_dirs[$PWD]:=$(
-              echo -n "$XDG_CACHE_HOME"/direnv/layouts/
-              echo -n "$PWD" | shasum | cut -d ' ' -f 1
-          )}"
-        }
-      '';
-    };
+  config = lib.mkIf osConfig.modules.usrEnv.programs.cli.enable {
+    home.packages = with pkgs; [
+      alejandra
+      nix-index
+      nix-tree
+      nil
+    ];
   };
 }
