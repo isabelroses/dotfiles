cb6fd060bcc284ad9dc2c7a082964910ae4eddb8
isabel <isabel@isabelroses.com>
2023-09-07 21:44:26 +0100

    feat(cyberchef): init

---
+++ modules/common/types/server/services/cybershef/default.nix
@@ -0,0 +1,24 @@
+{
+  self,
+  lib,
+  config,
+  pkgs,
+  ...
+}: let
+  inherit (config.networking) domain;
+in {
+  services.nginx.virtualHosts."chef.${domain}" = lib.mkIf config.modules.usrEnv.services.cyberchef.enable {
+    default = true;
+    forceSSL = true;
+    enableACME = true;
+    listen = [
+      {
+        ssl = false;
+        port = 8000;
+        addr = "127.0.0.1";
+      }
+    ];
+    root = self.packages.${pkgs.hostPlatform.system}.cyberchef;
+  };
+  networking.firewall.allowedTCPPorts = [8000];
+}
