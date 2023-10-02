2ae0a73ba0d10df4042e569757d87e23edd9e7b7
isabel <isabel@isabelroses.com>
2023-10-02 19:44:08 +0100

    syntax fixes

--- home/isabel/programs/cli/confs/shells/nushell/default.nix
+++ home/isabel/programs/cli/confs/shells/nushell/default.nix
@@ -6,20 +6,20 @@
   osConfig,
   ...
 }: let
-  inherit (lib) mkIf;
-  flakePath = osConfig.modules.system.flakePath;
+  inherit (lib) mkIf mkForce;
+  inherit (osConfig.modules.system) flakePath;
 in {
-  config = mkIf (osConfig.modules.usrEnv.programs.cli.enable) {
+  config = mkIf osConfig.modules.usrEnv.programs.cli.enable {
     programs.nushell = {
       enable = true;
       package = pkgs.nushell;
     };
 
     home.file = {
-      "${config.xdg.configHome}/nushell/config.nu" = lib.mkForce {
+      "${config.xdg.configHome}/nushell/config.nu" = mkForce {
         source = config.lib.file.mkOutOfStoreSymlink "${flakePath}/home/isabel/packages/cli/confs/nushell/config.nu";
       };
-      "${config.xdg.configHome}/nushell/env.nu" = lib.mkForce {
+      "${config.xdg.configHome}/nushell/env.nu" = mkForce {
         source = config.lib.file.mkOutOfStoreSymlink "${flakePath}/home/isabel/packages/cli/confs/nushell/env.nu";
       };
       "${config.xdg.configHome}/nushell/history.txt" = {
