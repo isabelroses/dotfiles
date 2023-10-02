1ae781d3334a6e9f80399e7b60360802512e98b0
isabel <isabel@isabelroses.com>
2023-10-02 19:43:56 +0100

    feat(btop): use default theme

--- home/isabel/programs/tui/confs/btop.nix
+++ home/isabel/programs/tui/confs/btop.nix
@@ -5,12 +5,12 @@
 }: let
   acceptedTypes = ["desktop" "laptop" "lite" "hybrid"];
 in {
-  config = lib.mkIf ((lib.isAcceptedDevice osConfig acceptedTypes) && (osConfig.modules.usrEnv.programs.tui.enable)) {
+  config = lib.mkIf ((lib.isAcceptedDevice osConfig acceptedTypes) && osConfig.modules.usrEnv.programs.tui.enable) {
     programs.btop = {
       enable = true;
       catppuccin.enable = true;
       settings = {
-        # color_theme = "catppuccin_mocha";
+        color_theme = "catppuccin_mocha";
         vim_keys = true;
         rounded_corners = true;
       };
