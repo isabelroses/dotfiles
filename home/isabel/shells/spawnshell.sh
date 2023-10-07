66d8412042d1d1311a7389032375e096ea92abd0
isabel <isabel@isabelroses.com>
2023-10-07 17:35:13 +0100

    remove redundant nix shell spawning

--- home/isabel/shells/spawnshell.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ $# -gt 1 ]]; then
-	extras="${*:2}"
-	nix-shell ~/shells/"$1".nix --arg extras "with import <nixpkgs> {}; [ $extras ]"
-else
-	nix-shell ~/shells/"$1".nix
-fi
