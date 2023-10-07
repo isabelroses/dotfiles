c3cb9751b7d80d454f778643b6e025576314a008
isabel <isabel@isabelroses.com>
2023-10-07 12:53:18 +0100

    fix(nushell): multiuser compatablity

--- home/isabel/programs/cli/confs/shells/nushell/default.nix
+++ home/isabel/programs/cli/confs/shells/nushell/default.nix
@@ -7,7 +7,7 @@
   ...
 }: let
   inherit (lib) mkIf mkForce;
-  inherit (osConfig.modules.system) flakePath;
+  inherit (osConfig.modules.system) flakePath mainUser;
 in {
   config = mkIf osConfig.modules.usrEnv.programs.cli.enable {
     programs.nushell = {
@@ -17,10 +17,10 @@ in {
 
     home.file = {
       "${config.xdg.configHome}/nushell/config.nu" = mkForce {
-        source = config.lib.file.mkOutOfStoreSymlink "${flakePath}/home/isabel/packages/cli/confs/nushell/config.nu";
+        source = config.lib.file.mkOutOfStoreSymlink "${flakePath}/home/${mainUser}/packages/cli/confs/nushell/config.nu";
       };
       "${config.xdg.configHome}/nushell/env.nu" = mkForce {
-        source = config.lib.file.mkOutOfStoreSymlink "${flakePath}/home/isabel/packages/cli/confs/nushell/env.nu";
+        source = config.lib.file.mkOutOfStoreSymlink "${flakePath}/home/${mainUser}/packages/cli/confs/nushell/env.nu";
       };
       "${config.xdg.configHome}/nushell/history.txt" = {
         source = config.lib.file.mkOutOfStoreSymlink "${config.xdg.dataHome}/history";
