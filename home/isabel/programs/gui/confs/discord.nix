f2d2f38305314c9d38d657edda9ef6d9daa2782a
isabel <isabel@isabelroses.com>
2023-10-07 12:54:21 +0100

    disable discord tts and install patcher

--- home/isabel/programs/gui/confs/discord.nix
+++ home/isabel/programs/gui/confs/discord.nix
@@ -2,6 +2,7 @@
   osConfig,
   lib,
   pkgs,
+  self,
   ...
 }: let
   inherit (osConfig.modules.system) video;
@@ -9,10 +10,13 @@
 in {
   config = lib.mkIf ((lib.isAcceptedDevice osConfig acceptedTypes) && osConfig.modules.usrEnv.programs.gui.enable && video.enable) {
     home.packages = with pkgs; [
+      self.packages.${pkgs.hostPlatform.system}.discord-krisp-patcher
+
       ((discord.override {
           nss = pkgs.nss_latest;
           withOpenASAR = true;
           withVencord = true;
+          withTTS = false;
         })
         .overrideAttrs (old: {
           libPath = old.libPath + ":${pkgs.libglvnd}/lib";
