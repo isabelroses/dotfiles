909d4ebdb31d77b81bc2c8e25b0a6228e1d93769
isabel <isabel@isabelroses.com>
2023-10-11 00:02:48 +0100

    improve security

--- modules/common/core/system/default.nix
+++ modules/common/core/system/default.nix
@@ -6,6 +6,7 @@ _: {
     ./encryption # just let me hide my stuff stop asking questions
     ./hardware # hardware - i.e bluetooth, sound, tpm etc.
     ./media # enable multimedia
+    ./nix # nix the package manger options
     ./os # system configurations
     ./security # keeping the system safe
     ./smb # host and recive smb shares
