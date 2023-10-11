074754860626190e682caa094f8e1fde6c6961a5
isabel <isabel@isabelroses.com>
2023-10-11 20:19:04 +0100

    feat!: remove discord-krisp-patcher

--- flake/pkgs/default.nix
+++ flake/pkgs/default.nix
@@ -7,8 +7,7 @@ _: {
   perSystem = {pkgs, ...}: {
     packages = {
       cybershef = pkgs.callPackage ./cyberchef.nix {};
-      # Stolen from https://github.com/BlankParticle/nixos-flake/blob/d4976617aaa2d2b1f9362fde547f835a4e3bed57/pkgs/discord-krisp-patcher/default.nix
-      discord-krisp-patcher = pkgs.callPackage ./discord-krisp-patcher/default.nix {};
+      # https://github.com/NixOS/nixpkgs/issues/195512
     };
   };
 }
