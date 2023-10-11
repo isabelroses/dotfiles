04e25b875f52cf78b0cf3cd581799bdc937ee3fb
isabel <isabel@isabelroses.com>
2023-10-12 00:09:49 +0100

    make pavucontrol optional with sound

--- home/isabel/programs/gui/wayland.nix
+++ home/isabel/programs/gui/wayland.nix
@@ -7,12 +7,13 @@
   acceptedTypes = ["desktop" "laptop" "lite" "hybrid"];
 in {
   config = lib.mkIf ((lib.isAcceptedDevice osConfig acceptedTypes) && (lib.isWayland osConfig) && osConfig.modules.programs.gui.enable) {
-    home.packages = with pkgs; [
-      swappy # used for screenshot area selection
-      # swaynotificationcenter
-      wlsunset # reduce blue light at night
-      wl-gammactl
-      pavucontrol # TODO move audio
-    ];
+    home.packages = with pkgs;
+      [
+        swappy # used for screenshot area selection
+        # swaynotificationcenter
+        wlsunset # reduce blue light at night
+        wl-gammactl
+      ]
+      ++ lib.optionals osConfig.modules.system.sound.enable [pavucontrol];
   };
 }
