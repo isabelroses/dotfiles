c244b185006c6f0baa14548412e5dc6b41d995af
isabel <isabel@isabelroses.com>
2023-10-13 05:08:37 +0100

    feat(services/datababes): init

---
+++ modules/common/types/server/services/databases/default.nix
@@ -0,0 +1,7 @@
+_: {
+  imports = [
+    ./mongodb
+    ./postgresql
+    ./mysql
+  ];
+}
--- modules/options/services/default.nix
+++ modules/options/services/default.nix
@@ -1,8 +1,24 @@
-{lib, ...}: let
+{
+  config,
+  lib,
+  ...
+}: let
   inherit (lib) mkEnableOption;
+  cfg = config.modules.services;
+
+  # stolen the functions https://github.com/NotAShelf/nyx/blob/614c3b0ee09b41a21bbd2395d1294bb55028657b/modules/common/options/system/services.nix
+
+  # ifOneEnabled takes a parent option and 3 child options and checks if at least one of them is enabled
+  # => ifOneEnabled config.modules.services "service1" "service2" "service3"
+  ifOneEnabled = cfg: a: b: c: cfg.a || cfg.b || cfg.c;
+
+  # mkEnableOption is the same as mkEnableOption but with the default value being equal to cfg.monitoring.enable
+  mkEnableOption' = desc: mkEnableOption "${desc}" // {default = cfg.monitoring.enable;};
 in {
   options.modules.services = {
     mailserver.enable = mkEnableOption "Enable the mailserver service";
+    matrix.enable = mkEnableOption "Enable matrix server";
+    miniflux.enable = mkEnableOption "Enable miniflux rss news aggreator service";
     gitea.enable = mkEnableOption "Enable the gitea service";
     cyberchef.enable = mkEnableOption "Enable the cyberchef website";
     vaultwarden.enable = mkEnableOption "Enable the vaultwarden service";
@@ -17,6 +33,21 @@ in {
     jellyfin = {
       enable = mkEnableOption "Enables the jellyfin service";
       asDockerContainer = mkEnableOption "Enables the service as a docker container";
+    };
+
+    # monitoring tools
+    monitoring = {
+      enable = mkEnableOption "system monitoring services" // {default = ifOneEnabled cfg "grafana" "prometheus" "loki";};
+      prometheus.enable = mkEnableOption' "Prometheus monitoring service";
+      grafana.enable = mkEnableOption' "Grafana monitoring service";
+      loki.enable = mkEnableOption' "Loki monitoring service";
+    };
+
+    # database backends
+    database = {
+      mysql.enable = mkEnableOption "MySQL database service";
+      mongodb.enable = mkEnableOption "MongoDB service";
+      postgresql.enable = mkEnableOption "Postgresql service";
     };
 
     smb = {
