6288d16d15db1916acb4527b3e6007d9e4a93fd2
isabel <isabel@isabelroses.com>
2023-10-17 02:18:00 +0100

    use firefox default

--- home/isabel/programs/gui/confs/browsers/schizofox.nix
+++ home/isabel/programs/gui/confs/browsers/schizofox.nix
@@ -9,10 +9,9 @@
   acceptedTypes = ["desktop" "laptop" "hybrid"];
 in {
   imports = [inputs.schizofox.homeManagerModule];
-  config = lib.mkIf ((lib.isAcceptedDevice osConfig acceptedTypes) && osConfig.modules.programs.gui.enable && video.enable && defaults.browser == "firefox") {
+  config = lib.mkIf ((lib.isAcceptedDevice osConfig acceptedTypes) && osConfig.modules.programs.gui.enable && video.enable && defaults.browser == "schizofox") {
     programs.schizofox = {
       enable = true;
-      package = "firefox-esr-115-unwrapped";
 
       theme = {
         background-darker = "181825";
@@ -49,9 +48,8 @@ in {
         # Addon IDs are in manifest.json or manifest-firefox.json
         "{446900e4-71c2-419f-a6a7-df9c091e268b}".install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
         "sponsorBlocker@ajay.app".install_url = "https://addons.mozilla.org/firefox/downloads/latest/sponsorblock/latest.xpi";
-        "1018e4d6-728f-4b20-ad56-37578a4de76".install_url = "https://addons.mozilla.org/firefox/downloads/latest/flagfox/latest.xpi";
         "{a4c4eda4-fb84-4a84-b4a1-f7c1cbf2a1ad}".install_url = "https://addons.mozilla.org/firefox/downloads/latest/refined-github-/latest.xpi";
-        "{7a7a4a92-a2a0-41d1-9fd7-1e92480d612d}".install_url = "https://addons.mozilla.org/firefox/downloads/latest/stylus/latest.xpi";
+        "{fd451f3d-9895-426a-8caf-c621899d72bc}".install_url = "https://addons.mozilla.org/firefox/downloads/latest/stylus/latest.xpi";
       };
     };
   };
