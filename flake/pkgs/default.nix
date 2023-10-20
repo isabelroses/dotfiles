f6f1c209ee8b88e7ca7eb7634b27bb4340a76a26
isabel <isabel@isabelroses.com>
2023-10-20 22:49:24 +0100

    chore(pkgs): update pkgs exposed to flake

--- flake/pkgs/default.nix
+++ flake/pkgs/default.nix
@@ -4,10 +4,20 @@ _: {
     "aarch64-linux"
   ];
 
-  perSystem = {pkgs, ...}: {
+  perSystem = {
+    pkgs,
+    inputs',
+    ...
+  }: {
     packages = {
       cybershef = pkgs.callPackage ./cyberchef.nix {};
       # https://github.com/NixOS/nixpkgs/issues/195512
+      lutgen-rs = pkgs.callPackage ./lutgen-rs.nix {};
+      patched-gjs = pkgs.callPackage ./patched-gjs.nix {};
+
+      bellado = inputs'.bellado.packages.default;
+      catppuccinifier-cli = inputs'.catppuccinifier.packages.cli;
+      ags = inputs'.ags.packages.default;
     };
   };
 }
