68b76fc39e156abf91cfe7934a58d90668e77317
isabel <isabel@isabelroses.com>
2023-10-25 10:13:37 +0100

    sudo smellss

--- modules/common/core/system/security/sudo.nix
+++ modules/common/core/system/security/sudo.nix
@@ -1,8 +1,10 @@
 {
-  pkgs,
   lib,
+  pkgs,
   ...
-}: {
+}: let
+  inherit (lib) mkDefault;
+in {
   security = {
     # sudo-rs is still a feature-incomplete sudo fork that can and will mess things up
     sudo-rs.enable = lib.mkForce false;
@@ -12,7 +14,7 @@
 
       # wheelNeedsPassword = false means wheel group can execute commands without a password
       # so just disable it, it only hurt security, BUT ... see below what commands can be run without password
-      wheelNeedsPassword = false;
+      wheelNeedsPassword = mkDefault false;
 
       # only allow members of the wheel group to execute sudo
       execWheelOnly = true;
@@ -21,7 +23,7 @@
       extraConfig = ''
         Defaults lecture = never
         Defaults pwfeedback
-        Defaults env_keep += "EDITOR PATH"
+        Defaults env_keep += "EDITOR PATH DISPLAY"
         Defaults timestamp_timeout = 300
       '';
 
@@ -29,10 +31,34 @@
         {
           # allow wheel group to run nixos-rebuild without password
           groups = ["sudo" "wheel"];
-          commands = [
+          commands = let
+            currentSystem = "/run/current-system/";
+            storePath = "/nix/store/";
+          in [
+            {
+              command = "${storePath}/*/bin/switch-to-configuration";
+              options = ["SETENV" "NOPASSWD"];
+            }
+            {
+              command = "${currentSystem}/sw/bin/nix-store";
+              options = ["SETENV" "NOPASSWD"];
+            }
+            {
+              command = "${currentSystem}/sw/bin/nix-env";
+              options = ["SETENV" "NOPASSWD"];
+            }
+            {
+              # let wheel group collect garbage without password
+              command = "${currentSystem}/sw/bin/nix-collect-garbage";
+              options = ["SETENV" "NOPASSWD"];
+            }
             {
               command = "${pkgs.nixos-rebuild}/bin/nixos-rebuild";
-              options = ["NOPASSWD"];
+              options = ["SETENV" "NOPASSWD"];
+            }
+            {
+              command = "/nix/var/nix/profiles/system/specialisation/*/bin/switch-to-configuration";
+              options = ["SETENV" "NOPASSWD"];
             }
           ];
         }
