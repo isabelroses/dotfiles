6c3918191e569178aa388e2121ce8c4f5c0df076
isabel <isabel@isabelroses.com>
2023-10-27 12:35:50 +0100

    feat(polkit): lighter running polkit

--- home/isabel/services/shared/polkit/default.nix
+++ home/isabel/services/shared/polkit/default.nix
@@ -10,10 +10,12 @@
 in {
   config = mkIf ((isAcceptedDevice osConfig acceptedTypes) && video.enable) {
     # gnome polkit agent
-    systemd.user.services.polkit-gnome-authentication-agent-1 = mkGraphicalService {
-      Service.ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-      # pantheon alternate, probaly runs lighter
-      # Service.ExecStart = "${pkgs.pantheon.pantheon-agent-polkit}/libexec/policykit-1-pantheon/io.elementary.desktop.agent-polkit";
+    # systemd.user.services.polkit-gnome-authentication-agent-1 = mkGraphicalService {
+    # Service.ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+
+    # pantheon alternate, probaly runs lighter
+    systemd.user.services.polkit-pantheon-authentication-agent-1 = mkGraphicalService {
+      Service.ExecStart = "${pkgs.pantheon.pantheon-agent-polkit}/libexec/policykit-1-pantheon/io.elementary.desktop.agent-polkit";
     };
   };
 }
