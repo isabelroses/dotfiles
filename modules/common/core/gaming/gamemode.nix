7f040c18e2bfd71134aa1da920b547ae53b6ff72
isabel <isabel@isabelroses.com>
2023-10-27 12:36:03 +0100

    regorganize lib

--- modules/common/core/gaming/gamemode.nix
+++ modules/common/core/gaming/gamemode.nix
@@ -5,29 +5,37 @@
   inputs,
   ...
 }: let
-  inherit (lib) makeBinPath mkIf;
+  inherit (lib) makeBinPath mkIf optionalString;
+
+  env = config.modules.usrEnv;
 
   programs = makeBinPath (with pkgs; [
-    inputs.hyprland.packages.${pkgs.system}.default
+    inputs.hyprland.packages.${stdenv.system}.default
     coreutils
     power-profiles-daemon
     systemd
   ]);
 
   startscript = pkgs.writeShellScript "gamemode-start" ''
-    export PATH=$PATH:${programs}
-    export HYPRLAND_INSTANCE_SIGNATURE=$(ls -w1 /tmp/hypr | tail -1)
-    hyprctl --batch 'keyword decoration:blur 0 ; keyword animations:enabled 0 ; keyword misc:vfr 0'
+    ${optionalString (env.desktop == "Hyprland") ''
+      export PATH=$PATH:${programs}
+      export HYPRLAND_INSTANCE_SIGNATURE=$(ls -w1 /tmp/hypr | tail -1)
+      hyprctl --batch 'keyword decoration:blur 0 ; keyword animations:enabled 0 ; keyword misc:vfr 0'
+    ''}
+
     powerprofilesctl set performance
     ${pkgs.libnotify}/bin/notify-send -a 'Gamemode' 'Optimizations activated'
   '';
 
   endscript = pkgs.writeShellScript "gamemode-end" ''
-    export PATH=$PATH:${programs}
-    export HYPRLAND_INSTANCE_SIGNATURE=$(ls -w1 /tmp/hypr | tail -1)
-    hyprctl --batch 'keyword decoration:blur 1 ; keyword animations:enabled 1 ; keyword misc:vfr 1'
-    ${pkgs.libnotify}/bin/notify-send -a 'Gamemode' 'Optimizations deactivated'
-    powerprofilesctl set power-saver
+    ${optionalString (env.desktop == "Hyprland") ''
+      export PATH=$PATH:${programs}
+      export HYPRLAND_INSTANCE_SIGNATURE=$(ls -w1 /tmp/hypr | tail -1)
+      hyprctl --batch 'keyword decoration:blur 1 ; keyword animations:enabled 1 ; keyword misc:vfr 1'
+    ''}
+
+      powerprofilesctl set power-saver
+      ${pkgs.libnotify}/bin/notify-send -a 'Gamemode' 'Optimizations deactivated'
   '';
 
   cfg = config.modules.programs.gaming;
