4a31f7b3701929f9f12663c238cdad428da37474
isabel <isabel@isabelroses.com>
2023-11-03 15:34:38 +0000

    disabe xserver by default

--- modules/profiles/workstation/services/xserver.nix
+++ modules/profiles/workstation/services/xserver.nix
@@ -10,15 +10,17 @@
 in {
   config = mkIf (system.video.enable && builtins.elem device.type acceptedTypes) {
     services.xserver = {
-      enable = true;
-      displayManager.gdm.enable = programs.defaults.loginManager == "gdm";
-      displayManager.lightdm.enable = programs.defaults.loginManager == "lightdm";
-      displayManager.sddm = {
-        enable = programs.defaults.loginManager == "sddm";
-        wayland.enable = true;
-        theme = "${import ../../../../../parts/pkgs/sddm.nix {inherit pkgs lib;}}";
-        settings = {
-          General = {InputMethod = "";};
+      enable = programs.defaults.loginManager != "greetd";
+      displayManager = {
+        gdm.enable = programs.defaults.loginManager == "gdm";
+        lightdm.enable = programs.defaults.loginManager == "lightdm";
+        sddm = {
+          enable = programs.defaults.loginManager == "sddm";
+          wayland.enable = true;
+          theme = "${import ../../../../../parts/pkgs/sddm.nix {inherit pkgs lib;}}";
+          settings = {
+            General = {InputMethod = "";};
+          };
         };
       };
     };
