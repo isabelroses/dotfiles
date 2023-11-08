cf74035fee2f93bca3f3ae58a6812fb775d04b6a
isabel <isabel@isabelroses.com>
2023-11-08 13:49:52 +0000

    fix: opsies

--- modules/base/common/services/monitoring/loki/default.nix
+++ modules/base/common/services/monitoring/loki/default.nix
@@ -5,9 +5,9 @@
 }: let
   inherit (lib) mkIf;
 
-  sys = config.modules.system;
+  cfg = config.modules.services.monitoring.loki;
 in {
-  config = mkIf sys.services.monitoring.loki.enable {
+  config = mkIf cfg.enable {
     # https://gist.github.com/rickhull/895b0cb38fdd537c1078a858cf15d63e
     services.loki = {
       enable = true;
