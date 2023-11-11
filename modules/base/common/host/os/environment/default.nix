13608c48b56e5ebdd06ab331b8086188092f3de3
isabel <isabel@isabelroses.com>
2023-11-11 11:49:18 +0000

    feat(os): console colors

--- modules/base/common/host/os/environment/default.nix
+++ modules/base/common/host/os/environment/default.nix
@@ -1,6 +1,7 @@
 _: {
   imports = [
     ./aliases.nix # shell aliases
+    ./console.nix # changes to the console
     ./etc.nix # misc
     ./locale.nix # locale settings
     ./packages.nix # packages
