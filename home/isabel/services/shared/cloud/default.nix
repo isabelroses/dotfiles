7b7128921a3ad0e8aa405426791d1a85671878ab
isabel <isabel@isabelroses.com>
2023-11-11 11:45:06 +0000

    chore: remove deadcode

--- home/isabel/services/shared/cloud/default.nix
+++ home/isabel/services/shared/cloud/default.nix
@@ -8,13 +8,6 @@
   acceptedTypes = ["desktop" "laptop" "lite" "hybrid"];
 in {
   config = mkIf (isAcceptedDevice osConfig acceptedTypes) {
-    /*
-    services = {
-      nextcloud-client.enable = true;
-      nextcloud-client.startInBackground = true;
-    };
-    */
-
     home.packages = [pkgs.nextcloud-client];
 
     systemd.user.services.nextcloud = mkGraphicalService {
