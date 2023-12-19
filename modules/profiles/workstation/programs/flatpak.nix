9f774dcc4f9caaf985fc47d8172b31c0f8cdebd3
isabel <isabel@isabelroses.com>
2023-12-19 14:13:47 +0000

    chore: renable flatpaks

--- modules/profiles/workstation/programs/flatpak.nix
+++ modules/profiles/workstation/programs/flatpak.nix
@@ -1,6 +1,6 @@
 _: {
   # enable flatpak
-  services.flatpak.enable = false;
+  services.flatpak.enable = true;
 
   environment.sessionVariables.XDG_DATA_DIRS = ["/var/lib/flatpak/exports/share"];
 }
