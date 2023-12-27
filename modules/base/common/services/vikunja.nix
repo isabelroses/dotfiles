3a88eab672094e75a3443ea2806f19c822dbe35d
isabel <isabel@isabelroses.com>
2023-12-27 21:47:41 +0000

    fix(vikunja): start using postgres + small cleanup

--- modules/base/common/services/vikunja.nix
+++ modules/base/common/services/vikunja.nix
@@ -7,7 +7,7 @@
 
   cfg = config.modules.services.vikunja;
 
-  inherit (lib) mkIf;
+  inherit (lib) mkIf template;
 in {
   config = mkIf cfg.enable {
     modules.services.database = {
@@ -21,14 +21,53 @@ in {
         frontendHostname = "todo.${domain}";
         frontendScheme = "https";
 
+        environmentFiles = [config.sops.secrets.vikunja-env.path];
+
         database = {
           type = "postgres";
           host = "/run/postgresql";
-          user = "vikunja";
-          database = "vikunja";
+          user = "vikunja-api";
+          database = "vikunja-api";
         };
 
-        # settings = {};
+        settings = {
+          service.enableregistration = false;
+
+          defaultsettings = {
+            avatar_provider = "gravatar";
+            week_start = 1; # monday
+          };
+
+          mailer = {
+            enabled = true;
+            host = "mail.isabelroses.com";
+            port = 465;
+            forcessl = true;
+
+            authtype = "login";
+            fromemail = "noreply@isabelroses.com";
+            username = "noreply@isabelroses.com";
+          };
+
+          # redis
+          # redis = {
+          #   enabled = true;
+          #   host = "/run/redis-vikunja/redis.sock";
+          #   db = 0;
+          # };
+        };
+      };
+
+      nginx.virtualHosts.${config.services.vikunja.frontendHostname} = template.ssl;
+    };
+
+    users = {
+      groups.vikunja-api = {};
+
+      users."vikunja-api" = {
+        group = "vikunja-api";
+        createHome = false;
+        isSystemUser = true;
       };
     };
   };
