863dc6a9ba114438253d55807acb72d3858d4d5e
isabel <isabel@isabelroses.com>
2024-01-10 15:53:48 +0000

    chore: cleanup

--- modules/base/options/themes/qt.nix
+++ modules/base/options/themes/qt.nix
@@ -7,37 +7,31 @@
   inherit (lib) mkEnableOption mkOption types;
   cfg = config.modules.style;
 in {
-  options = {
-    modules = {
-      style = {
-        qt = {
-          enable = mkEnableOption "QT Style Module";
+  options.modules.style.qt = {
+    enable = mkEnableOption "QT Style Module";
 
-          theme = {
-            package = mkOption {
-              type = types.package;
-              default = pkgs.catppuccin-kde.override {
-                flavour = ["mocha"];
-                accents = ["sapphire"];
-                winDecStyles = ["modern"];
-              };
-              description = "The theme package to be used for QT programs";
-            };
-
-            name = mkOption {
-              type = types.str;
-              default = "Catppuccin-Mocha-Dark";
-              description = "The name for the QT theme package";
-            };
-          };
-
-          kdeglobals.source = mkOption {
-            type = types.path;
-            default = "${cfg.qt.theme.package}/share/color-schemes/CatppuccinMochaSapphire.colors";
-            description = "The source file for the kdeglobals file. Usually provided by the qt theme package";
-          };
+    theme = {
+      package = mkOption {
+        type = types.package;
+        default = pkgs.catppuccin-kde.override {
+          flavour = ["mocha"];
+          accents = ["sapphire"];
+          winDecStyles = ["modern"];
         };
+        description = "The theme package to be used for QT programs";
       };
+
+      name = mkOption {
+        type = types.str;
+        default = "Catppuccin-Mocha-Dark";
+        description = "The name for the QT theme package";
+      };
+    };
+
+    kdeglobals.source = mkOption {
+      type = types.path;
+      default = "${cfg.qt.theme.package}/share/color-schemes/CatppuccinMochaSapphire.colors";
+      description = "The source file for the kdeglobals file. Usually provided by the qt theme package";
     };
   };
 }
