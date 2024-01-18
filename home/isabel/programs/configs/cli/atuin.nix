9d3fa04848442d66df5703d9510eb0cc4337c492
isabel <isabel@isabelroses.com>
2024-01-18 14:49:44 +0000

    chore(atuin): update config

--- home/isabel/programs/configs/cli/atuin.nix
+++ home/isabel/programs/configs/cli/atuin.nix
@@ -14,11 +14,15 @@ in {
       enableFishIntegration = config.programs.fish.enable;
       enableZshIntegration = config.programs.zsh.enable;
 
+      flags = ["--disable-up-arrow"];
       settings = {
         dialect = "uk";
         show_preview = true;
+        inline_height = 30;
         style = "compact";
         update_check = false;
+        sync_address = "https://atuin.isabelroses.com";
+        sync_frequency = "5m";
       };
     };
   };
