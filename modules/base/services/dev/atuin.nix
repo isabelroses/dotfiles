e4961755f7cc65166141b0d496c8d34e986c1b0e
isabel <isabel@isabelroses.com>
2024-01-18 14:56:05 +0000

    chore: cleanup atuin

--- modules/base/services/dev/atuin.nix
+++ modules/base/services/dev/atuin.nix
@@ -8,19 +8,18 @@
   rdomain = config.networking.domain;
   cfg = config.modules.services.dev.atuin;
 in {
-  services = mkIf (cfg.enable) {
+  services = mkIf cfg.enable {
     atuin = {
       enable = true;
+      inherit (cfg) port host;
       openRegistration = false;
       maxHistoryLength = 1024 * 16;
-      port = cfg.port;
-      host = cfg.host;
     };
 
     nginx.virtualHosts.${cfg.domain} =
       {
         locations."/" = {
-          proxyPass = "http://${cfg.host}:${toString cfg.gport}";
+          proxyPass = "http://${cfg.host}:${toString cfg.port}";
         };
       }
       // template.ssl rdomain;
