434ff2edd3ac55ce7d96ed132fa8283dd300acbc
isabel <isabel@isabelroses.com>
2024-01-26 16:42:59 +0000

    fix: change dir to file

--- modules/base/host/default.nix
+++ modules/base/host/default.nix
@@ -4,10 +4,10 @@ _: {
     ./hardware # hardware - bluetooth etc.
     ./os # system configurations
     ./security # keeping the system safe
-    ./virtualization # docker, QEMU, waydroid etc.
 
     ./emulation.nix # emulation setup
     ./encryption.nix # keeping my stuff hidden from you strange people
     ./nix.nix # nix the package manger options
+    ./virtualization.nix # docker, QEMU, waydroid etc.
   ];
 }
