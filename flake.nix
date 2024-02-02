dfb5bab66384f2d8fe69bf6dad9c690067cd5a32
isabel <isabel@isabelroses.com>
2024-02-02 14:56:59 +0000

    chore: update neovim

--- flake.nix
+++ flake.nix
@@ -185,7 +185,7 @@
     };
 
     neovim = {
-      url = "github:isabelroses/nvim/b7de8792760b480c9de1029ecc09185cd94225ac";
+      url = "github:isabelroses/nvim";
       # url = "git+file:///home/isabel/dev/nvim";
       inputs = {
         nixpkgs.follows = "nixpkgs";
