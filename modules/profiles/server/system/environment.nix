9b21ad8685041190b00f2112b3523d7ab403deee
isabel <isabel@isabelroses.com>
2024-02-08 20:03:40 +0000

    chore: disbale xdg on servers

--- modules/profiles/server/system/environment.nix
+++ modules/profiles/server/system/environment.nix
@@ -1,4 +1,4 @@
-_: {
+{
   environment = {
     # normally we wouldn't need any Xlibs on a headless server but for whatever reason
     # this affects whether or not some programs can build - such as pipewire
@@ -6,5 +6,13 @@ _: {
 
     # print the URL instead on servers
     variables.BROWSER = "echo";
+  };
+
+  xdg = {
+    sounds.enable = false;
+    mime.enable = false;
+    menus.enable = false;
+    icons.enable = false;
+    autostart.enable = false;
   };
 }
