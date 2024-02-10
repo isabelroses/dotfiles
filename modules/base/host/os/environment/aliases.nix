61d68d36fcd49413c3678c57bdb5eaa8003d0963
isabel <isabel@isabelroses.com>
2024-02-10 17:28:25 +0000

    fix(shell-aliases): order ldTernary correctly

--- modules/base/host/os/environment/aliases.nix
+++ modules/base/host/os/environment/aliases.nix
@@ -9,8 +9,8 @@ in {
   environment.shellAliases = {
     rebuild =
       lib.ldTernary pkgs
-      "nix-store --verify; darwin-rebuild switch --flake ${environment.flakePath}#${system.hostname}"
-      "nix-store --verify; sudo nixos-rebuild switch --flake ${environment.flakePath}#${system.hostname}";
+      "nix-store --verify; sudo nixos-rebuild switch --flake ${environment.flakePath}#${system.hostname}"
+      "nix-store --verify; darwin-rebuild switch --flake ${environment.flakePath}#${system.hostname}";
     nixclean = "sudo nix-collect-garbage --delete-older-than 3d && nix-collect-garbage -d";
     nixrepair = "nix-store --verify --check-contents --repair";
   };
