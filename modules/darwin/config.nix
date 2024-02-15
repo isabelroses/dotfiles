452faeba503b5455d4c2ddce3816c1811cc37d30
isabel <isabel@isabelroses.com>
2024-02-15 23:24:49 +0000

    docs(darwin): improve notes about defaults apis

--- modules/darwin/config.nix
+++ modules/darwin/config.nix
@@ -3,13 +3,15 @@
 {
   system.defaults = {
     menuExtraClock = {
-      Show24Hour = false; # show 24 hour clock
+      Show24Hour = true; # show 12 hour clock
       IsAnalog = false; # show digital clock
       ShowAMPM = true; # show AM/PM
-      ShowDate = 2; # 0 = Show the date 1 = Don’t show 2 = Don’t show (theres a todo marked for the diffrence between 1 and 2 upstream)
-      ShowDayOfMonth = false; # show day of month
-      ShowDayOfWeek = false; # show day of week
-      ShowSeconds = false; # show seconds
+
+      # Show date can imply the result of ShowDayOfMonth, ShowDayOfWeek, and ShowSeconds.
+      ShowDate = 2; # 0 = Show the date 1 = Don’t show 2 = Don’t show (hides date)
+      # ShowDayOfMonth = false; # show day of month
+      # ShowDayOfWeek = false; # show day of week
+      # ShowSeconds = false; # show seconds
     };
 
     dock = {
@@ -40,6 +42,8 @@
     };
 
     NSGlobalDomain = {
+      AppleICUForce24HourTime = false; # use 12 hour time
+
       "com.apple.swipescrolldirection" = false; # enable natural scrolling
       "com.apple.sound.beep.feedback" = 0; # disable beep sound when pressing volume up/down key
       "com.apple.sound.beep.volume" = null; # disable beep sound
