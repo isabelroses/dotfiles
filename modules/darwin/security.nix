700f53d103bb54406d2ed76ee1916bb914373d40
isabel <isabel@isabelroses.com>
2024-02-15 23:24:19 +0000

    chore(darwin): improve security

--- modules/darwin/security.nix
+++ modules/darwin/security.nix
@@ -1,3 +1,7 @@
 {
   security.pam.enableSudoTouchIdAuth = true;
+
+  # Apple menu > System Preferences > Security and firewall
+  # Drops incoming requests via ICMP such as ping requests. Default is 0.
+  system.defaults.alf.stealthenabled = 1; # enabled
 }
