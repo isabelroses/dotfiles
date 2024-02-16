da03aabc9e167ac7e7841b37eba9cc8616e270e8
isabel <isabel@isabelroses.com>
2024-02-16 23:33:20 +0000

    feat(direnv): store in xdg cache

--- home/isabel/programs/configs/cli/direnv.nix
+++ home/isabel/programs/configs/cli/direnv.nix
@@ -1,10 +1,21 @@
 {
   lib,
   pkgs,
+  config,
   ...
 }: {
   programs.direnv = lib.mkIf pkgs.stdenv.isDarwin {
     enable = true;
     nix-direnv.enable = true;
+    stdlib = ''
+      # taken from @i077
+      # store direnv in cache and not pre project
+      direnv_layout_dir() {
+        echo "''${direnv_layout_dirs[$PWD]:=$(
+          echo -n "${config.xdg.cacheHome}"/direnv/layouts/
+          echo -n "$PWD" | shasum | cut -d ' ' -f 1
+        )
+      }"
+    '';
   };
 }
