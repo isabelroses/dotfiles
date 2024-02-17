ffd53f1908827fcd38513d9829d7dfd1d15a4969
isabel <isabel@isabelroses.com>
2024-02-17 23:24:42 +0000

    feat(darwin): macos dock support

--- modules/darwin/non-native.nix
+++ modules/darwin/non-native.nix
@@ -1,4 +1,4 @@
-{
+{pkgs, ...}: {
   # Customize settings that not supported by nix-darwin directly
   # see the source code of this project to get more undocumented options:
   #    https://github.com/rgcr/m-cli
@@ -10,6 +10,7 @@
       # Add a context menu item for showing the Web Inspector in web views
       WebKitDeveloperExtras = true;
     };
+
     "com.apple.finder" = {
       ShowExternalHardDrivesOnDesktop = true;
       ShowHardDrivesOnDesktop = true;
@@ -19,14 +20,17 @@
       # When performing a search, search the current folder by default
       FXDefaultSearchScope = "SCcf";
     };
+
     "com.apple.desktopservices" = {
       # Avoid creating .DS_Store files on network or USB volumes
       DSDontWriteNetworkStores = true;
       DSDontWriteUSBStores = true;
     };
+
     "com.apple.spaces" = {
       "spans-displays" = 0; # Display have seperate spaces
     };
+
     "com.apple.WindowManager" = {
       EnableStandardClickToShowDesktop = 0; # Click wallpaper to reveal desktop
       StandardHideDesktopIcons = 0; # Show items on desktop
@@ -34,19 +38,34 @@
       StageManagerHideWidgets = 0;
       StandardHideWidgets = 0;
     };
+
     "com.apple.screensaver" = {
       # Require password immediately after sleep or screen saver begins
       askForPassword = 1;
       askForPasswordDelay = 0;
     };
+
     "com.apple.screencapture" = {
       location = "~/Pictures/screenshots";
       type = "png";
     };
+
     "com.apple.AdLib" = {
       allowApplePersonalizedAdvertising = false;
     };
+
     # Prevent Photos from opening automatically when devices are plugged in
     "com.apple.ImageCapture".disableHotPlug = true;
+
+    # modified from https://github.com/nmasur/dotfiles/blob/275863795317f8ce65486b138c1fb4eb6dbd65f8/modules/darwin/system.nix#L134-L148
+    "com.apple.dock".persistent-apps = let
+      dockText = app: "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>${app}</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>";
+    in
+      map dockText [
+        "/Applications/Arc.app"
+        "/Users/isabel/Applications/Home Manager Apps/WezTerm.app"
+        "/Users/isabel/Applications/Home Manager Apps/Discord.app"
+        "${pkgs.obsidian}/Applications/Obsidian.app"
+      ];
   };
 }
