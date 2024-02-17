2c7653fc62f56cc41b5098d055ecb1a7e24bef95
isabel <isabel@isabelroses.com>
2024-02-17 16:12:50 +0000

    chore: nix cleanup

--- modules/base/nix/default.nix
+++ modules/base/nix/default.nix
@@ -3,6 +3,7 @@
     ./environment.nix # environment settings that nix requires
     ./nix.nix # nix the package manager's settings
     ./nixpkgs.nix # nixpkgs configuration
+    ./substituters.nix # nixpkgs substituters
     ./system.nix # system settings that nix needs
   ];
 }
---
+++ modules/base/nix/substituters.nix
@@ -0,0 +1,36 @@
+{
+  # substituters to use
+  nix.settings = {
+    substituters = [
+      "https://cache.nixos.org/" # offical binary cache (yes the trailing slash is really neccacery)
+      "https://nixpkgs-wayland.cachix.org" # some wayland packages
+      "https://nix-community.cachix.org" # nix-community cache
+      "https://hyprland.cachix.org" # hyprland
+      "https://nix-gaming.cachix.org" # nix-gaming
+      "https://nixpkgs-unfree.cachix.org" # unfree-package cache
+      "https://numtide.cachix.org" # another unfree package cache
+      "https://isabelroses.cachix.org" # precompiled binarys from flake
+      "https://nekowinston.cachix.org" # precompiled binarys from nekowinston NUR
+      "https://catppuccin.cachix.org" # a cache for ctp nix
+      "https://pre-commit-hooks.cachix.org" # pre commit hooks
+      "https://cache.garnix.io" # extra things here and there
+      "https://ags.cachix.org" # ags
+    ];
+
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+      "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
+      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+      "isabelroses.cachix.org-1:mXdV/CMcPDaiTmkQ7/4+MzChpOe6Cb97njKmBQQmLPM="
+      "nekowinston.cachix.org-1:lucpmaO+JwtoZj16HCO1p1fOv68s/RL1gumpVzRHRDs="
+      "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
+      "pre-commit-hooks.cachix.org-1:Pkk3Panw5AW24TOv6kz3PvLhlH8puAsJTBbOPmBo7Rc="
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      "ags.cachix.org-1:naAvMrz0CuYqeyGNyLgE010iUiuf/qx6kYrUv3NwAJ8="
+    ];
+  };
+}
