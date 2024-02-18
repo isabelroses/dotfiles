7a587ed82020442f14996c0e6a55aadb969bfc8c
isabel <isabel@isabelroses.com>
2024-02-18 17:58:46 +0000

    fix(hosts/amatrasu): build

--- hosts/amaterasu/default.nix
+++ hosts/amaterasu/default.nix
@@ -8,79 +8,82 @@
 in {
   imports = [./hardware.nix];
 
-  config.modules = {
-    device = {
-      type = "desktop";
-      cpu = "intel";
-      gpu = "nvidia";
-      hasTPM = true;
-      monitors = ["HDMI-1" "DP-1"];
-      hasBluetooth = true;
-      hasSound = true;
-      keyboard = "us";
-    };
-    system = {
-      mainUser = "isabel";
+  config = {
+    modules = {
+      device = {
+        type = "desktop";
+        cpu = "intel";
+        gpu = "nvidia";
+        hasTPM = true;
+        monitors = ["HDMI-1" "DP-1"];
+        hasBluetooth = true;
+        hasSound = true;
+        keyboard = "us";
+      };
 
-      boot = {
-        loader = "systemd-boot";
-        secureBoot = false;
+      system = {
+        mainUser = "isabel";
 
-        plymouth = {
+        boot = {
+          loader = "systemd-boot";
+          secureBoot = false;
+
+          plymouth = {
+            enable = true;
+            withThemes = true;
+          };
+
+          enableKernelTweaks = true;
+
+          initrd = {
+            enableTweaks = true;
+            optimizeCompressor = true;
+          };
+
+          loadRecommendedModules = true;
+          tmpOnTmpfs = true;
+        };
+
+        fs = ["ext4" "vfat"];
+        video.enable = true;
+        sound.enable = true;
+        bluetooth.enable = false;
+        printing.enable = false;
+        yubikeySupport.enable = true;
+
+        security.auditd.enable = true;
+
+        networking.optimizeTcp = true;
+
+        virtualization = {
+          enable = false;
+          docker.enable = false;
+          qemu.enable = false;
+          podman.enable = false;
+          distrobox.enable = false;
+        };
+      };
+
+      environment = {
+        desktop = "Hyprland";
+        useHomeManager = true;
+      };
+
+      programs = {
+        agnostic.git.signingKey = "7F2F6BD6997FCDF7";
+
+        cli = {
           enable = true;
-          withThemes = true;
+          modernShell.enable = true;
         };
 
-        enableKernelTweaks = true;
+        tui.enable = true;
 
-        initrd = {
-          enableTweaks = true;
-          optimizeCompressor = true;
+        gui = {
+          enable = true;
+
+          zathura.enable = true;
         };
-
-        loadRecommendedModules = true;
-        tmpOnTmpfs = true;
-      };
-
-      fs = ["ext4" "vfat"];
-      video.enable = true;
-      sound.enable = true;
-      bluetooth.enable = false;
-      printing.enable = false;
-      yubikeySupport.enable = true;
-
-      security.auditd.enable = true;
-
-      networking.optimizeTcp = true;
-
-      virtualization = {
-        enable = false;
-        docker.enable = false;
-        qemu.enable = false;
-        podman.enable = false;
-        distrobox.enable = false;
-      };
-    };
-
-    environment = {
-      desktop = "Hyprland";
-      useHomeManager = true;
-    };
-
-    programs = {
-      agnostic.git.signingKey = "7F2F6BD6997FCDF7";
-
-      cli = {
-        enable = true;
-        modernShell.enable = true;
-      };
-
-      tui.enable = true;
-
-      gui = {
-        enable = true;
-
-        zathura.enable = true;
       };
     };
   };
