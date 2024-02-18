e521e6c5d9a504d8b0c3419cea0a48e77b56a729
isabel <isabel@isabelroses.com>
2024-02-18 20:51:58 +0000

    chore: cleanup ssh and firewall

--- home/isabel/system/ssh.nix
+++ home/isabel/system/ssh.nix
@@ -1,61 +1,49 @@
 {osConfig, ...}: {
-  programs = let
+  programs.ssh = let
     inherit (osConfig.age) secrets;
   in {
-    ssh = {
-      enable = true;
-      hashKnownHosts = true;
-      compression = true;
+    enable = true;
+    hashKnownHosts = true;
+    compression = true;
 
-      matchBlocks = let
-        nixSystem = {
-          identityFile = secrets.nixos-key.path;
-        };
-      in {
-        # git clients
-        "aur.archlinux.org" = {
-          user = "aur";
-          hostname = "aur.archlinux.org";
-          identityFile = secrets.aur-key.path;
-        };
+    matchBlocks = {
+      # git clients
+      "aur.archlinux.org" = {
+        user = "aur";
+        hostname = "aur.archlinux.org";
+        identityFile = secrets.aur-key.path;
+      };
 
-        "github.com" = {
-          user = "git";
-          hostname = "github.com";
-          identityFile = secrets.gh-key.path;
-        };
+      "github.com" = {
+        user = "git";
+        hostname = "github.com";
+        identityFile = secrets.gh-key.path;
+      };
 
-        # ORACLE vps'
-        "openvpn" = {
-          hostname = "132.145.55.42";
-          user = "openvpnas";
-          identityFile = secrets.openvpn-key.path;
-        };
+      # ORACLE vps'
+      "openvpn" = {
+        hostname = "132.145.55.42";
+        user = "openvpnas";
+        identityFile = secrets.openvpn-key.path;
+      };
 
-        "amity" = {
-          hostname = "143.47.240.116";
-          identityFile = secrets.amity-key.path;
-        };
+      "amity" = {
+        hostname = "143.47.240.116";
+        identityFile = secrets.amity-key.path;
+      };
 
-        # hetzner cloud vps
-        "luz" =
-          nixSystem
-          // {
-            hostname = "91.107.198.173";
-          };
+      # hetzner cloud vps
+      "luz" = {
+        hostname = "91.107.198.173";
+      };
 
-        # my local servers / clients
-        "hydra" =
-          nixSystem
-          // {
-            hostname = "10.82.7.9";
-          };
+      # my local servers / clients
+      "hydra" = {
+        hostname = "10.82.7.9";
+      };
 
-        "tatsumaki" =
-          nixSystem
-          // {
-            hostname = "10.82.9.147";
-          };
+      "tatsumaki" = {
+        hostname = "10.82.9.147";
       };
     };
   };
