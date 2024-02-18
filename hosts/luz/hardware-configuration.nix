64fd08997654b8eee6864cefd85b05d93274491d
isabel <isabel@isabelroses.com>
2024-02-18 16:27:27 +0000

    chore(luz): hardware cleanup

--- hosts/luz/hardware-configuration.nix
+++ hosts/luz/hardware-configuration.nix
@@ -5,9 +5,16 @@
   ...
 }: {
   imports = [(modulesPath + "/profiles/qemu-guest.nix")];
-  boot.initrd.availableKernelModules = ["ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi"];
-  boot.initrd.kernelModules = ["nvme"];
-  boot.kernelModules = ["kvm-amd"];
+
+  boot = {
+    kernelModules = ["kvm-amd"];
+
+    boot.initrd = {
+      availableKernelModules = ["ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi"];
+      kernelModules = ["nvme"];
+    };
+  };
+
   fileSystems."/" = {
     device = "/dev/sda1";
     fsType = "ext4";
