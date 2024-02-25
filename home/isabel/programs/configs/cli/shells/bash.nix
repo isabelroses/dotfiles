5c74f009e7eb93f68d8e74c78cee953f4d34c728
isabel <isabel@isabelroses.com>
2024-02-25 18:27:47 +0000

    fix: use bash to put me into fish

--- home/isabel/programs/configs/cli/shells/bash.nix
+++ home/isabel/programs/configs/cli/shells/bash.nix
@@ -1,6 +1,11 @@
-{config, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   programs.bash = {
     enable = true;
+
     historyFile = "${config.xdg.stateHome}/bash/history";
     historyFileSize = 1000;
     historySize = 100;
@@ -13,5 +18,13 @@
       "histappend"
       "no_empty_cmd_completion"
     ];
+
+    initExtra = ''
+      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+      then
+        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+      fi
+    '';
   };
 }
