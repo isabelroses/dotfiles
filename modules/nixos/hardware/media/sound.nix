9355828835833c51e5e2fdbe51d2373d4330cd55
isabel <isabel@isabelroses.com>
2024-03-03 15:58:39 +0000

    chore: update to new apis

--- modules/nixos/hardware/media/sound.nix
+++ modules/nixos/hardware/media/sound.nix
@@ -21,6 +21,9 @@ in {
     # able to change scheduling policies, e.g. to SCHED_RR
     security.rtkit.enable = config.services.pipewire.enable;
 
+    # pulseaudio backup
+    hardware.pulseaudio.enable = !config.services.pipewire.enable;
+
     # pipewire is newer and just better
     services.pipewire = {
       enable = true;
@@ -32,28 +35,28 @@ in {
       };
 
       audio.enable = true;
-      wireplumber.enable = true;
       pulse.enable = true;
       jack.enable = true;
+
       alsa = {
         enable = true;
         support32Bit = isx86Linux pkgs;
       };
-    };
 
-    # pulseaudio backup
-    hardware.pulseaudio.enable = !config.services.pipewire.enable;
+      wireplumber = {
+        inherit (config.services.pipewire) enable;
 
-    # write bluetooth rules if and only if pipewire is enabled AND the device has bluetooth
-    environment.etc = mkIf (config.services.pipewire.enable && device.hasBluetooth) {
-      "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
-        bluez_monitor.properties = {
-          ["bluez5.enable-sbc-xq"] = true,
-          ["bluez5.enable-msbc"] = true,
-          ["bluez5.enable-hw-volume"] = true,
-          ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-        }
-      '';
+        configPackages = lib.optionals dev.hasBluetooth [
+          (pkgs.writeTextDir "share/bluetooth.lua.d/51-bluez-config.lua" ''
+            bluez_monitor.properties = {
+              ["bluez5.enable-sbc-xq"] = true,
+              ["bluez5.enable-msbc"] = true,
+              ["bluez5.enable-hw-volume"] = true,
+              ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+            }
+          '')
+        ];
+      };
     };
 
     systemd.user.services = {
