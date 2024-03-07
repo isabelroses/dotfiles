b73c20c20519e9c59aba0d5fd81749919a99b538
isabel <isabel@isabelroses.com>
2024-03-07 23:19:49 +0000

    chore: disable all fonts on servers

--- modules/profiles/meta/headless/fonts.nix
+++ modules/profiles/meta/headless/fonts.nix
@@ -1,5 +1,11 @@
 {lib, ...}: {
-  # we don't need fontconfig on a server
+  # we don't need fonts on a server
   # since there are no fonts to be configured outside the console
-  fonts.fontconfig.enable = lib.mkDefault false;
+  fonts = let
+    inherit (lib) mkForce;
+  in {
+    package = mkForce [];
+    fontDir.enable = mkForce false;
+    fontconfig.enable = mkForce false;
+  };
 }
