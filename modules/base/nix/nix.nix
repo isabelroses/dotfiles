021b47a67f35c9cfe231a0b42e8a4742362e7879
isabel <isabel@isabelroses.com>
2024-03-11 14:26:36 +0000

    chore: move to nix 2.18 without breaking anything hopefully

--- modules/base/nix/nix.nix
+++ modules/base/nix/nix.nix
@@ -12,7 +12,7 @@ in {
   nix = {
     # https://github.com/nix-community/home-manager/issues/4692#issuecomment-1848832609
     # package = pkgs.nixVersions.unstable;
-    package = pkgs.nixVersions.nix_2_17;
+    package = pkgs.nixVersions.nix_2_18;
 
     # pin the registry to avoid downloading and evaluating a new nixpkgs version everytime
     registry = mapAttrs (_: v: {flake = v;}) flakeInputs;
