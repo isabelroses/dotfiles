64a1e5b8c75400f8c96b534d54db7e5366f97363
isabel <isabel@isabelroses.com>
2024-03-14 20:59:16 +0000

    chore(openssh): fix typo

--- modules/nixos/os/networking/ssh.nix
+++ modules/nixos/os/networking/ssh.nix
@@ -29,7 +29,7 @@
 
       # Use Macs recommended by `nixpkgs#ssh-audit`
       Macs = [
-        "mac-sha2-512-etm@openssh.com"
+        "hmac-sha2-512-etm@openssh.com"
         "hmac-sha2-256-etm@openssh.com"
         "umac-128-etm@openssh.com"
       ];
