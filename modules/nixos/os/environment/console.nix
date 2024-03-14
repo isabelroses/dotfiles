a3997cc542bde51680e3be8eabb4d465bb6a4bb5
isabel <isabel@isabelroses.com>
2024-03-14 20:30:15 +0000

    chore: use ctp module

--- modules/nixos/os/environment/console.nix
+++ modules/nixos/os/environment/console.nix
@@ -5,28 +5,10 @@
 }: {
   console = {
     enable = lib.mkDefault true;
+    catppuccin.enable = true;
     earlySetup = true;
 
     keyMap = "en";
     font = "${pkgs.terminus_font}/share/consolefonts/ter-d18n.psf.gz";
-
-    colors = [
-      "1e1e2e"
-      "585b70"
-      "bac2de"
-      "a6adc8"
-      "f38ba8"
-      "f38ba8"
-      "a6e3a1"
-      "a6e3a1"
-      "f9e2af"
-      "f9e2af"
-      "89b4fa"
-      "89b4fa"
-      "f5c2e7"
-      "f5c2e7"
-      "94e2d5"
-      "94e2d5"
-    ];
   };
 }
