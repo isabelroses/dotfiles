3b93853fcda68ac96441ca0706dfff05a4cdc817
isabel <isabel@isabelroses.com>
2024-03-16 01:52:42 +0000

    chore: don't build my website

--- parts/pkgs/default.nix
+++ parts/pkgs/default.nix
@@ -16,7 +16,6 @@
       headscale-ui = pkgs.callPackage ./headscale-ui.nix {};
 
       bellado = inputs'.bellado.packages.default;
-      isabelroses-website = inputs'.isabelroses-website.packages.default;
       catppuccinifier-cli = inputs'.catppuccinifier.packages.cli;
     };
   };
