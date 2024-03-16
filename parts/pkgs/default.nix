9d461e1f788f5480be7a55eb7a2d213cae0f03cd
isabel <isabel@isabelroses.com>
2024-03-16 15:38:13 +0000

    revert: webstite changes

--- parts/pkgs/default.nix
+++ parts/pkgs/default.nix
@@ -16,6 +16,7 @@
       headscale-ui = pkgs.callPackage ./headscale-ui.nix {};
 
       bellado = inputs'.bellado.packages.default;
+      isabelroses-website = inputs'.isabelroses-website.packages.default;
       catppuccinifier-cli = inputs'.catppuccinifier.packages.cli;
     };
   };
