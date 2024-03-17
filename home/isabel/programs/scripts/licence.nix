5907a1bb0c1051a5be55f9400df7f31eaf84d3fd
isabel <isabel@isabelroses.com>
2024-03-17 22:11:21 +0000

    fix: licence script get the correct MIT licence

--- home/isabel/programs/scripts/licence.nix
+++ home/isabel/programs/scripts/licence.nix
@@ -1,10 +1,16 @@
-{pkgs, ...}: ''
-  CHOOSE=$(${pkgs.gum}/bin/gum choose "cc by-nc-sa 4.0" "MIT" "GPLv3")
+{
+  lib,
+  pkgs,
+  ...
+}: let
+  inherit (lib) getExe;
+in ''
+  CHOOSE=$(${getExe pkgs.gum} choose "cc by-nc-sa 4.0" "MIT" "GPLv3")
 
   if [ "$CHOOSE" = "cc by-nc-sa 4.0" ]; then
     LICENSE="https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode.txt"
   elif [ "$CHOOSE" = "MIT" ]; then
-    LICENSE="https://opensource.org/licenses/MIT"
+    LICENSE="https://gist.githubusercontent.com/isabelroses/fa6f71651be564ada535bb56dec1e13b/raw/c766f746e94771d92bea73db881a567215b5fe77/MIT%2520License"
   elif [ "$CHOOSE" = "GPLv3" ]; then
     LICENSE="https://www.gnu.org/licenses/gpl-3.0.txt"
   else
@@ -12,5 +18,5 @@
     exit 1
   fi
 
-  ${pkgs.curl}/bin/curl -o LICENSE $LICENSE
+  ${getExe pkgs.curl} -o LICENSE $LICENSE
 ''
