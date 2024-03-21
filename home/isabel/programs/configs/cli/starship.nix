b9cce54b14e849791e7b9123b67bfc6e437b3b2a
isabel <isabel@isabelroses.com>
2024-03-21 10:33:42 +0000

    fix(starship): dedup home icon

--- home/isabel/programs/configs/cli/starship.nix
+++ home/isabel/programs/configs/cli/starship.nix
@@ -3,7 +3,16 @@
   config,
   osConfig,
   ...
-}: {
+}: let
+  ss = symbol: style: {
+    inherit symbol;
+    format = "[$symbol ](${style})";
+  };
+  ssv = symbol: style: {
+    inherit symbol;
+    format = "via [$symbol](${style})";
+  };
+in {
   programs.starship = {
     inherit (osConfig.modules.programs.cli) enable;
     catppuccin.enable = true;
@@ -17,108 +26,96 @@
         "[╭╴](238)$os"
         "$all[╰─󰁔](237)$character"
       ];
+
       character = {
         success_symbol = "";
         error_symbol = "";
       };
-      container = {
-        symbol = " 󰏖";
-        format = "[$symbol ](yellow dimmed)";
-      };
+
       username = {
         style_user = "white";
         style_root = "black";
         format = "[$user]($style) ";
         show_always = true;
       };
+
       directory = {
         truncation_length = 3;
         truncation_symbol = "…/";
-        home_symbol = "󰋞 ~";
+        home_symbol = "󰋞 ";
         read_only_style = "197";
         read_only = "  ";
         format = "at [$path]($style)[$read_only]($read_only_style) ";
+
+        substitutions = {
+          "󰋞 /Documents" = " ";
+          "󰋞 /documents" = " ";
+
+          "󰋞 /Downloads" = " ";
+          "󰋞 /downloads" = " ";
+
+          "󰋞 /media/music" = " ";
+          "󰋞 /media/pictures" = " ";
+          "󰋞 /media/videos" = " ";
+          "󰋞 /Music" = " ";
+          "󰋞 /Pictures" = " ";
+          "󰋞 /Videos" = " ";
+
+          "󰋞 /dev" = "󱌢 ";
+          "󰋞 /Dev" = "󱌢 ";
+
+          "󰋞 /skl" = "󰑴 ";
+          "󰋞 /.config" = " ";
+        };
       };
-      directory.substitutions = {
-        "documents" = " ";
-        "downloads" = " ";
-        "media/music" = " ";
-        "media/pictures" = " ";
-        "media/videos" = " ";
-        "dev" = "󱌢 ";
-        "skl" = "󰑴 ";
-        ".config" = " ";
-      };
+
       os = {
-        # disabled = true;
         style = "bold white";
         format = "[$symbol]($style)";
+
+        symbols = {
+          Arch = "";
+          Artix = "";
+          Debian = "";
+          # Kali = "󰠥";
+          EndeavourOS = "";
+          Fedora = "";
+          NixOS = "";
+          openSUSE = "";
+          SUSE = "";
+          Ubuntu = "";
+          Raspbian = "";
+          #elementary = "";
+          #Coreos = "";
+          Gentoo = "";
+          #mageia = ""
+          CentOS = "";
+          #sabayon = "";
+          #slackware = "";
+          Mint = "";
+          Alpine = "";
+          #aosc = "";
+          #devuan = "";
+          Manjaro = "";
+          #rhel = "";
+          Macos = "󰀵";
+          Linux = "";
+          Windows = "";
+        };
       };
-      # If it is commented out it does not exist yet
-      os.symbols = {
-        Arch = "";
-        Artix = "";
-        Debian = "";
-        # Kali = "󰠥";
-        EndeavourOS = "";
-        Fedora = "";
-        NixOS = "";
-        openSUSE = "";
-        SUSE = "";
-        Ubuntu = "";
-        Raspbian = "";
-        #elementary = "";
-        #Coreos = "";
-        Gentoo = "";
-        #mageia = ""
-        CentOS = "";
-        #sabayon = "";
-        #slackware = "";
-        Mint = "";
-        Alpine = "";
-        #aosc = "";
-        #devuan = "";
-        Manjaro = "";
-        #rhel = "";
-        Macos = "󰀵";
-        Linux = "";
-        Windows = "";
-      };
-      python = {
-        symbol = "";
-        format = "[$symbol ](yellow)";
-      };
-      nodejs = {
-        symbol = " ";
-        format = "[$symbol ](yellow)";
-      };
-      lua = {
-        symbol = "󰢱";
-        format = "[$symbol ](blue)";
-      };
-      rust = {
-        symbol = "";
-        format = "[$symbol ](red)";
-      };
-      docker_context = {
-        symbol = "";
-        format = "[$symbol ](blue)";
-      };
-      java = {
-        symbol = "";
-        format = "[$symbol ](red)";
-      };
-      c = {
-        symbol = "";
-        format = "[$symbol ](blue)";
-      };
-      golang = {
-        symbol = "";
-        format = "[$symbol ](blue)";
-      };
-      battery = {
-        disabled = true;
-      };
+
+      container = ss " 󰏖" "yellow dimmed";
+      python = ss "" "yellow";
+      nodejs = ss " " "yellow";
+      lua = ss "󰢱 " "blue";
+      rust = ss "" "red";
+      java = ss " " "red";
+      c = ss " " "blue";
+      golang = ss "" "blue";
+      docker_context = ss " " "blue";
+
+      nix_shell = ssv " " "blue";
+
       git_branch = {
         symbol = "󰊢 ";
         format = "on [$symbol$branch]($style) ";
@@ -141,6 +138,8 @@
         renamed = "󰖷 ";
         deleted = " ";
       };
+
+      battery.disabled = true;
     };
   };
 }
