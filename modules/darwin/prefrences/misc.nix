a57e3e49605cb9dfc0a6784f12c66ebf131ee332
isabel <isabel@isabelroses.com>
2024-03-29 20:15:57 +0000

    fix(darwin): call apis correctly

--- modules/darwin/prefrences/misc.nix
+++ modules/darwin/prefrences/misc.nix
@@ -1,9 +1,6 @@
 {
   system.defaults = {
     NSGlobalDomain = {
-      # Add a context menu item for showing the Web Inspector in web views
-      WebKitDeveloperExtras = true;
-
       NSAutomaticCapitalizationEnabled = false; # disable auto capitalization
       NSAutomaticDashSubstitutionEnabled = false; # disable auto dash substitution
       NSAutomaticPeriodSubstitutionEnabled = false; # disable auto period substitution
@@ -12,11 +9,16 @@
       NSNavPanelExpandedStateForSaveMode = true; # expand save panel by default
       NSNavPanelExpandedStateForSaveMode2 = true; # ^
     };
-  };
 
-  CustomUserPreferences."com.apple.desktopservices" = {
-    # Avoid creating .DS_Store files on network or USB volumes
-    DSDontWriteNetworkStores = true;
-    DSDontWriteUSBStores = true;
+    CustomUserPreferences = {
+      # Add a context menu item for showing the Web Inspector in web views
+      NSGlobalDomain.WebKitDeveloperExtras = true;
+
+      "com.apple.desktopservices" = {
+        # Avoid creating .DS_Store files on network or USB volumes
+        DSDontWriteNetworkStores = true;
+        DSDontWriteUSBStores = true;
+      };
+    };
   };
 }
