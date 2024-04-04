aa11252480438140362f8e033d494a631b1c810c
isabel <isabel@isabelroses.com>
2024-04-04 23:08:56 +0100

    refactor: auto import new templates

--- parts/templates/default.nix
+++ parts/templates/default.nix
@@ -1,33 +1,10 @@
-{
-  flake.templates = {
-    # C/C++
-    c = {
-      path = ./c;
-      description = "Development environment for C/C++";
-    };
-
-    # golang
-    go = {
-      path = ./go;
-      description = "Development environment for Golang";
-    };
-
-    # NodeJS
-    node = {
-      path = ./node;
-      description = "Development environment for NodeJS";
-    };
-
-    # Python
-    python = {
-      path = ./python;
-      description = "Development environment for Python";
-    };
-
-    # Rust
-    rust = {
-      path = ./rust;
-      description = "Development environment for Rust";
-    };
-  };
+{lib, ...}: {
+  flake.templates = lib.pipe ./. [
+    builtins.readDir
+    (builtins.filter (n: n != "default.nix"))
+    (builtins.mapAttrs (name: _: {
+      description = name;
+      path = ./${name};
+    }))
+  ];
 }
