e28362518e530e0b35470365da6befe7d0439cd7
isabel <isabel@isabelroses.com>
2024-04-05 19:29:24 +0100

    feat(gh-dash): add theme

--- home/isabel/programs/configs/cli/git.nix
+++ home/isabel/programs/configs/cli/git.nix
@@ -22,6 +22,11 @@ in {
       gitflow # Extend git with the Gitflow branching model
     ];
 
+    xdg.configFile."gh-dash/config.yml".source = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/isabelroses/gh-dash/main/themes/mocha/pink.yml";
+      sha256 = "0yk4qpprhkyg9k3xjh2fsil5a815jr3ssfi61d5pw43lhkwm5f99";
+    };
+
     programs = {
       # github cli
       gh = {
