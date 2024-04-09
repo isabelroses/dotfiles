55a7cb9ed7c2e443b360f9fb79c0a11db0c48233
isabel <isabel@isabelroses.com>
2024-04-09 11:00:17 +0100

    feat(wezterm/catppuccin): add new custom flavor

--- home/isabel/configs/gui/terminals/wezterm/catppuccin.lua
+++ home/isabel/configs/gui/terminals/wezterm/catppuccin.lua
@@ -115,9 +115,38 @@ local colors = {
     mantle = "#181825",
     crust = "#11111b",
   },
+  espresso = {
+    rosewater = "#ece3e1",
+    flamingo = "#e1d2d2",
+    pink = "#ddccd8",
+    mauve = "#bbb2c9",
+    red = "#c4a2aa",
+    maroon = "#cbadb1",
+    peach = "#d5beb4",
+    yellow = "#ece3d3",
+    green = "#b9ddb6",
+    teal = "#badad4",
+    sky = "#b8d4db",
+    sapphire = "#a9c0ce",
+    blue = "#aab3c7",
+    lavender = "#bfc1d2",
+    text = "#d3d6e1",
+    subtext1 = "#bec2d2",
+    subtext0 = "#a8adc3",
+    overlay2 = "#9299b4",
+    overlay1 = "#7c84a5",
+    overlay0 = "#686f94",
+    surface2 = "#555a7b",
+    surface1 = "#434664",
+    surface0 = "#30314b",
+    base = "#101010",
+    mantle = "#090909",
+    crust = "#080808",
+  },
 }
 
 local mappings = {
+  espresso = "Catppuccin Espresso", -- custom flavor
   mocha = "Catppuccin Mocha",
   macchiato = "Catppuccin Macchiato",
   frappe = "Catppuccin Frappe",
@@ -235,8 +264,8 @@ function M.apply_to_config(c, opts)
     accent = "pink",
     sync = false,
     sync_flavors = { light = "latte", dark = "mocha" },
-    color_overrides = { mocha = {}, macchiato = {}, frappe = {}, latte = {} },
-    token_overrides = { mocha = {}, macchiato = {}, frappe = {}, latte = {} },
+    color_overrides = { espresso = {}, mocha = {}, macchiato = {}, frappe = {}, latte = {} },
+    token_overrides = { espresso = {}, mocha = {}, macchiato = {}, frappe = {}, latte = {} },
   }
 
   local o = tableMerge(defaults, opts)
