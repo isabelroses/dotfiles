dfc9dfd354d52035395c48a69d300c64bdc3aaa6
isabel <isabel@isabelroses.com>
2024-04-12 12:59:24 +0100

    feat(homebrew): dont give hints

--- modules/darwin/brew/environment.nix
+++ modules/darwin/brew/environment.nix
@@ -4,6 +4,7 @@
       HOMEBREW_NO_ANALYTICS = "1";
       HOMEBREW_NO_INSECURE_REDIRECT = "1";
       HOMEBREW_NO_EMOJI = "1";
+      HOMEBREW_NO_ENV_HINTS = "0";
     };
 
     systemPath = [config.homebrew.brewPrefix];
