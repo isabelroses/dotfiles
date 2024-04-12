47d114c0bce4108a912d71c4c832be80c2016f6d
isabel <isabel@isabelroses.com>
2024-04-12 12:51:58 +0100

    feat(homebrew): more env confs

--- modules/darwin/brew/environment.nix
+++ modules/darwin/brew/environment.nix
@@ -2,6 +2,8 @@
   environment = {
     variables = {
       HOMEBREW_NO_ANALYTICS = "1";
+      HOMEBREW_NO_INSECURE_REDIRECT = "1";
+      HOMEBREW_NO_EMOJI = "1";
     };
 
     systemPath = [config.homebrew.brewPrefix];
