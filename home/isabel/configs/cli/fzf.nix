69f3712ac1c8ba5b5b3dfaa6ebcf1c47d703b145
isabel <isabel@isabelroses.com>
2024-04-14 11:02:44 +0100

    refactor(fzf): use ctp instead

--- home/isabel/configs/cli/fzf.nix
+++ home/isabel/configs/cli/fzf.nix
@@ -9,23 +9,12 @@ in {
   config = lib.mkIf ((lib.isAcceptedDevice osConfig acceptedTypes) && lib.isModernShell osConfig) {
     programs.fzf = {
       enable = true;
+      catppuccin.enable = true;
 
       enableBashIntegration = config.programs.bash.enable;
       enableZshIntegration = config.programs.zsh.enable;
       enableFishIntegration = config.programs.fish.enable;
 
-      colors = {
-        fg = "#cdd6f4";
-        "fg+" = "#cdd6f4";
-        hl = "#f38ba8";
-        "hl+" = "#f38ba8";
-        header = "#ff69b4";
-        info = "#cba6f7";
-        marker = "#f5e0dc";
-        pointer = "#f5e0dc";
-        prompt = "#cba6f7";
-        spinner = "#f5e0dc";
-      };
       defaultOptions = ["--height=30%" "--layout=reverse" "--info=inline"];
     };
   };
