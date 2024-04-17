0a684a751e6f86d5b2b78e2b46e93130c0126793
isabel <isabel@isabelroses.com>
2024-04-17 17:13:19 +0100

    chore: update rss feeds

--- home/isabel/configs/tui/izrss.nix
+++ home/isabel/configs/tui/izrss.nix
@@ -13,13 +13,13 @@ in {
       enable = true;
 
       urls = [
+        "https://isabelroses.com/feed.xml"
         "https://uncenter.dev/feed.xml"
-        "https://isabelroses.com/rss.xml"
         "https://antfu.me/feed.xml"
         "https://fasterthanli.me/index.xml"
         "https://blog.orhun.dev/rss.xml"
         "https://mitchellh.com/feed.xml"
-        "https://dataswamp.org/~solene/rss.xml"
+        "https://dataswamp.org/~solene/rss-html.xml"
         "https://ayats.org/index.xml"
         "https://nixpkgs.news/rss.xml"
         "https://maia.crimew.gay/feed.xml"
