c1051bc663fa526b69b1b0c4f4f2fb2a6ea74c66
isabel <isabel@isabelroses.com>
2024-04-21 14:16:47 +0100

    feat: nap pull and bakup

--- home/isabel/configs/shell/shellAlias.nix
+++ home/isabel/configs/shell/shellAlias.nix
@@ -8,7 +8,8 @@
     jctl = "journalctl -p 3 -xb"; # get error messages from journalctl
     lg = "lazygit";
 
-    napbk = "cd ~/.local/share/nap ; git add . ; git commit -m 'chore: sync changes'; git push ; cd -";
+    nappl = "cd ~/.local/share/nap ; git pull ; git push ; cd -";
+    napbk = "cd ~/.local/share/nap ; git add . ; git commit -m 'chore: sync changes' ; git push ; cd -";
 
     # Remap docker to podman
     docker = "podman";
