024004a190c55e43e7e40408c458748845bfcbb1
isabel <isabel@isabelroses.com>
2024-04-21 18:04:00 +0100

    feat: stuff

--- modules/nixos/services/networking/cloudflared.nix
+++ modules/nixos/services/networking/cloudflared.nix
@@ -1,23 +1,30 @@
 {
-  config,
   lib,
+  config,
   ...
 }: let
   inherit (lib) mkIf;
 
-  rdomain = config.networking.domain;
   cfg = config.modules.services.networking.cloudflared;
 in {
+  users.users.cloudflared = {
+    description = "Cloudflared user";
+    home = "/var/lib/cloudflared";
+    isSystemUser = true;
+    createHome = true;
+  };
+
   services.cloudflared = mkIf cfg.enable {
     enable = true;
 
     tunnels.${config.networking.hostName} = {
-      credentialsFile = "${config.age.secrets.cloudflared-hydra.path}";
+      credentialsFile = config.age.secrets.cloudflared-hydra.path;
       default = "http_status:404";
 
       # example of jellyfin
       ingress = {
-        "tv.${rdomain}" = "http://${cfg.host}:8096";
+        # "tv.${rdomain}" = "http://${cfg.host}:8096";
+        "ctp-wiki.${cfg.domain}".service = "http://${cfg.host}:80";
       };
     };
   };
