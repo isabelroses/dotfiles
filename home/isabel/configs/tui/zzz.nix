429419c9f604900585808a9ad21f1102ea4e3920
isabel <isabel@isabelroses.com>
2024-04-22 11:24:48 +0100

    feat!: clean config

--- home/isabel/configs/tui/zzz.nix
+++ home/isabel/configs/tui/zzz.nix
@@ -14,19 +14,6 @@ in {
 
     xdg.configFile."zzz/config.yaml".text = ''
       default_language: go
-      theme: "catppuccin-mocha"
-
-      background: "#1e1e2e"
-      foreground: "#cdd6f4"
-      primary_color: "#74c7ec"
-      primary_color_subdued: "#74c7ec"
-      green: "#a6e3a1"
-      bright_green: "#f9e2af"
-      bright_red: "#eba0ac"
-      red: "#f38ba8"
-      black: "#cdd6f4"
-      gray: "#313244"
-      white: "#11111b"
     '';
   };
 }
