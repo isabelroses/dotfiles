ac856d61bb8a787b05ac372ba42cfe5cf72400e3
isabel <isabel@isabelroses.com>
2024-04-27 11:27:18 +0100

    feat: use upstream-ish gh dash

--- home/isabel/configs/git/gh-dash.nix
+++ home/isabel/configs/git/gh-dash.nix
@@ -1,14 +1,4 @@
+# required for catppuccin nix
 {
-  lib,
-  pkgs,
-  ...
-}: {
-  programs.gh-dash = {
-    enable = true;
-  };
-
-  xdg.configFile."gh-dash/config.yml".source = lib.mkForce (pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/isabelroses/gh-dash/main/themes/mocha/pink.yml";
-    sha256 = "0yk4qpprhkyg9k3xjh2fsil5a815jr3ssfi61d5pw43lhkwm5f99";
-  });
+  programs.gh-dash.enable = true;
 }
