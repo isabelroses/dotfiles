a52849006f72a7cf61e569dfc142587a9976e67a
isabel <isabel@isabelroses.com>
2024-06-12 13:49:15 +0100

    feat(nixpkgs): maintainerless?????

--- modules/base/nix/nixpkgs.nix
+++ modules/base/nix/nixpkgs.nix
@@ -9,11 +9,23 @@
     # pkgs = self.legacyPackages.${config.nixpkgs.system};
 
     config = {
+      # I want to install packages that are not FOSS sometimes
       allowUnfree = true;
       allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [ "emojis" ];
+
+      # If a package is broken, I don't want it
+      # But occasionally we need to install some anyway so we can predicated those
       allowBroken = false;
       allowUnsupportedSystem = true;
-      permittedInsecurePackages = [ "electron-25.9.0" ];
+      permittedInsecurePackages = [ ];
+
+      # I don't want to use aliases for packages, usually because its slow
+      # and also because it can get confusing
+      allowAliases = false;
+
+      # Maybe I can pickup so packages
+      # Also a good idea to know which packages might be very out of date or broken
+      showDerivationWarnings = [ "maintainerless" ];
     };
 
     overlays = [
