753d0ddadfdae4d2a129d4656d19cad41e04079d
isabel <isabel@isabelroses.com>
2024-06-17 11:48:45 +0100

    refactor: themes

--- modules/base/options/default.nix
+++ modules/base/options/default.nix
@@ -2,7 +2,6 @@
   imports = [
     ./device
     ./programs
-    ./themes
     ./system
     ./environment
   ];
--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -7,6 +7,7 @@
     ./nix # nix settings for nixos only systems
     ./security # keeping the system safe
     ./services # allows for per-system system services to be enabled
+    ./themes # themes for the system
 
     ./emulation.nix # emulation setup
     ./encryption.nix # keeping my stuff hidden from you strange people
