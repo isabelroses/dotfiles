e7c16335d709f2f2d805bc8e19e3a8d78fbfebaf
isabel <isabel@isabelroses.com>
2024-06-17 12:31:09 +0100

    refactor: break up networking

--- modules/nixos/os/networking/wireless.nix
+++ modules/nixos/os/networking/wireless.nix
@@ -1,39 +1,56 @@
-{ config, ... }:
+{ lib, config, ... }:
 let
+  inherit (lib) types mkOption;
   inherit (config.modules.system.networking) wirelessBackend;
 in
 {
-  # enable wireless database, it helps keeping wifi speedy
-  hardware.wirelessRegulatoryDatabase = true;
 
-  networking.wireless = {
-    # wpa_supplicant
-    enable = wirelessBackend == "wpa_supplicant";
-    userControlled.enable = true;
-    allowAuxiliaryImperativeNetworks = true;
-
-    extraConfig = ''
-      update_config=1
+  options.modules.system.networking.wirelessBackend = mkOption {
+    type = types.enum [
+      "iwd"
+      "wpa_supplicant"
+    ];
+    default = "wpa_supplicant";
+    description = ''
+      Backend that will be used for wireless connections using either `networking.wireless`
+      or `networking.networkmanager.wifi.backend`
+      Defaults to wpa_supplicant until iwd is stable.
     '';
+  };
 
-    # iwd
-    iwd = {
-      enable = wirelessBackend == "iwd";
+  config = {
+    # enable wireless database, it helps keeping wifi speedy
+    hardware.wirelessRegulatoryDatabase = true;
 
-      settings = {
-        Settings.AutoConnect = true;
+    networking.wireless = {
+      # wpa_supplicant
+      enable = wirelessBackend == "wpa_supplicant";
+      userControlled.enable = true;
+      allowAuxiliaryImperativeNetworks = true;
 
-        General = {
-          # more things that my uni hates me for
-          # AddressRandomization = "network";
-          # AddressRandomizationRange = "full";
-          EnableNetworkConfiguration = true;
-          RoamRetryInterval = 15;
-        };
+      extraConfig = ''
+        update_config=1
+      '';
 
-        Network = {
-          EnableIPv6 = true;
-          RoutePriorityOffset = 300;
+      # iwd
+      iwd = {
+        enable = wirelessBackend == "iwd";
+
+        settings = {
+          Settings.AutoConnect = true;
+
+          General = {
+            # more things that my uni hates me for
+            # AddressRandomization = "network";
+            # AddressRandomizationRange = "full";
+            EnableNetworkConfiguration = true;
+            RoamRetryInterval = 15;
+          };
+
+          Network = {
+            EnableIPv6 = true;
+            RoutePriorityOffset = 300;
+          };
         };
       };
     };
