89f6b322676324a8850bdc5beabd72408faee025
isabel <isabel@isabelroses.com>
2024-06-17 10:47:57 +0100

    feat(lib): add mkServiceOption & secrets

---
+++ parts/lib/secrets.nix
@@ -0,0 +1,40 @@
+{ self, ... }:
+let
+  mkSecret =
+    {
+      file,
+      owner ? "root",
+      group ? "root",
+      mode ? "400",
+      ...
+    }:
+    {
+      file = "${self}/secrets/${file}.age";
+      inherit owner group mode;
+    };
+
+  mkSecretWithPath =
+    {
+      file,
+      path,
+      owner ? "root",
+      group ? "root",
+      mode ? "400",
+      ...
+    }:
+    mkSecret {
+      inherit
+        file
+        owner
+        group
+        mode
+        ;
+    }
+    // {
+      inherit path;
+    };
+
+in
+{
+  inherit mkSecret mkSecretWithPath;
+}
