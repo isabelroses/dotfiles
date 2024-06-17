eba146752c1a476918b2ce0add2e21110db5f495
isabel <isabel@isabelroses.com>
2024-06-17 12:33:00 +0100

    refactor: brakeup fs

--- modules/nixos/options/system/default.nix
+++ modules/nixos/options/system/default.nix
@@ -1,51 +1,11 @@
-{ lib, config, ... }:
+{ lib, ... }:
 let
-  inherit (lib)
-    mkOption
-    mkEnableOption
-    optionals
-    types
-    ;
+  inherit (lib) mkEnableOption;
 in
 {
-  imports = [
-    ./boot.nix
-    ./networking.nix
-    ./security.nix
-  ];
-
-  config.warnings = optionals (config.modules.system.fs == [ ]) [
-    ''
-      You have not added any filesystems to be supported by your system. You may end up with an unbootable system!
-
-      Consider setting {option}`config.modules.system.fs` in your configuration
-    ''
-  ];
+  imports = [ ./boot.nix ];
 
   options.modules.system = {
-    autoLogin = mkOption {
-      type = types.bool;
-      default = false;
-      description = ''
-        Whether to enable passwordless login. This is generally useful on systems with
-        FDE (Full Disk Encryption) enabled. It is a security risk for systems without FDE.
-      '';
-    };
-
-    fs = mkOption {
-      type = with types; listOf str;
-      default = [
-        "vfat"
-        "ext4"
-      ];
-      description = ''
-        A list of filesystems available supported by the system
-        it will enable services based on what strings are found in the list.
-
-        It would be a good idea to keep vfat and ext4 so you can mount USBs.
-      '';
-    };
-
     sound.enable = mkEnableOption "Does the device have sound and its related programs be enabled";
     video.enable = mkEnableOption "Does the device allow for graphical programs";
     bluetooth.enable = mkEnableOption "Should the device load bluetooth drivers and enable blueman";
