6b71517294f120dd325192e851c443060d76de1b
isabel <isabel@isabelroses.com>
2024-06-20 12:33:00 +0100

    fix(editors): provide idea jdk22

--- home/isabel/configs/editors/extra.nix
+++ home/isabel/configs/editors/extra.nix
@@ -12,6 +12,7 @@ in
     with pkgs;
     lib.optionals cfg.gui.enable [
       jetbrains.idea-ultimate # eww java
+      jdk22
       # arduino # thank god I don't have to use this anymore
     ];
 }
