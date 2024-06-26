30b1b875c073b960a61c3d9a785037fb488f5f02
isabel <isabel@isabelroses.com>
2024-06-26 16:04:58 +0100

    docs(nix/system): explain stateVersion

--- modules/base/nix/system.nix
+++ modules/base/nix/system.nix
@@ -1,7 +1,20 @@
-{ lib, pkgs, ... }:
+{
+  lib,
+  self,
+  pkgs,
+  ...
+}:
 let
   inherit (lib) mkDefault ldTernary;
 in
 {
-  system.stateVersion = ldTernary pkgs (mkDefault "23.05") (mkDefault 4);
+  system = {
+    # this is the NixOS version that the configuration was generated with
+    # this should be change to the version of the NixOS release that the configuration was generated with
+    stateVersion = mkDefault (ldTernary pkgs "23.05" 4);
+
+    # we can get the git rev that we are working on and set that to the configurationRevision
+    # this *might* be useful for debugging
+    configurationRevision = self.shortRev or self.dirtyShortRev;
+  };
 }
