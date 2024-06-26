5762ad1eb506092c099bac097de5fcee64529a73
isabel <isabel@isabelroses.com>
2024-06-26 16:05:55 +0100

    chore: misc gui cleanup

--- home/isabel/packages/gui/shared.nix
+++ home/isabel/packages/gui/shared.nix
@@ -1,29 +1,19 @@
 {
-  pkgs,
   lib,
+  pkgs,
   osConfig,
   ...
 }:
-let
-  acceptedTypes = [
-    "laptop"
-    "desktop"
-    "hybrid"
-    "lite"
-  ];
-in
 {
-  config =
-    lib.mkIf ((lib.isAcceptedDevice osConfig acceptedTypes) && osConfig.modules.programs.gui.enable)
-      {
-        home.packages = with pkgs; [
-          bitwarden-desktop # password manager
-          obsidian # note taking with markdown
-          pamixer # move evntually
-          # jellyfin-media-player
-          # mangal # tui manga finder + reader
-          # insomnia # rest client
-          nextcloud-client # cloud storage
-        ];
-      };
+  config = lib.mkIf osConfig.modules.programs.gui.enable {
+    home.packages = with pkgs; [
+      bitwarden-desktop # password manager
+      obsidian # note taking with markdown
+      pamixer # move evntually
+      # jellyfin-media-player
+      # mangal # tui manga finder + reader
+      # insomnia # rest client
+      nextcloud-client # cloud storage
+    ];
+  };
 }
--- home/isabel/packages/gui/wayland.nix
+++ home/isabel/packages/gui/wayland.nix
@@ -4,29 +4,14 @@
   osConfig,
   ...
 }:
-let
-  acceptedTypes = [
-    "desktop"
-    "laptop"
-    "lite"
-    "hybrid"
-  ];
-in
 {
-  config =
-    lib.mkIf
-      (
-        (lib.isAcceptedDevice osConfig acceptedTypes)
-        && (lib.isWayland osConfig)
-        && osConfig.modules.programs.gui.enable
-      )
-      {
-        home.packages =
-          with pkgs;
-          [
-            swappy # used for screenshot area selection
-            wl-gammactl
-          ]
-          ++ lib.optionals osConfig.modules.system.sound.enable [ pavucontrol ];
-      };
+  config = lib.mkIf (lib.isWayland osConfig) {
+    home.packages =
+      with pkgs;
+      [
+        swappy # used for screenshot area selection
+        wl-gammactl
+      ]
+      ++ lib.optionals osConfig.modules.system.sound.enable [ pavucontrol ];
+  };
 }
