32f0731f85cb34166d25862747bcc542c5815a74
isabel <isabel@isabelroses.com>
2024-06-27 21:00:05 +0100

    fix(firefox): check error

--- home/isabel/configs/gui/browsers/firefox.nix
+++ home/isabel/configs/gui/browsers/firefox.nix
@@ -14,6 +14,14 @@ in
     programs.firefox = {
       enable = true;
 
+      extensions = {
+        # Addon IDs are in manifest.json or manifest-firefox.json
+        "{446900e4-71c2-419f-a6a7-df9c091e268b}".install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+        "sponsorBlocker@ajay.app".install_url = "https://addons.mozilla.org/firefox/downloads/latest/sponsorblock/latest.xpi";
+        "{a4c4eda4-fb84-4a84-b4a1-f7c1cbf2a1ad}".install_url = "https://addons.mozilla.org/firefox/downloads/latest/refined-github-/latest.xpi";
+        "{7a7a4a92-a2a0-41d1-9fd7-1e92480d612d}".install_url = "https://addons.mozilla.org/firefox/downloads/latest/styl-us/latest.xpi";
+      };
+
       arkenfox = {
         enable = true;
         version = "126.1";
@@ -21,40 +29,6 @@ in
 
       profiles.arkenfox = {
         isDefault = true;
-
-        search = {
-          default = "Searx";
-          engines = {
-            Searx = {
-              urls = [
-                { template = "https://search.isabelroses.com/search?q={searchTerms}&categories=general"; }
-              ];
-            };
-
-            Nix = {
-              urls = [ { template = "https://search.nixos.org/packages?channel=unstable&query={searchTerms}"; } ];
-              definedAliases = [ "nix" ];
-            };
-
-            GitHub = {
-              urls = [ { template = "https://github.com/search?q={searchTerms}&ref=opensearch"; } ];
-              definedAliases = [ "gh" ];
-            };
-
-            YouTube = {
-              urls = [ { template = "https://www.youtube.com/results?search_query={searchTerms}"; } ];
-              definedAliases = [ "yt" ];
-            };
-          };
-        };
-
-        extensions = {
-          # Addon IDs are in manifest.json or manifest-firefox.json
-          "{446900e4-71c2-419f-a6a7-df9c091e268b}".install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
-          "sponsorBlocker@ajay.app".install_url = "https://addons.mozilla.org/firefox/downloads/latest/sponsorblock/latest.xpi";
-          "{a4c4eda4-fb84-4a84-b4a1-f7c1cbf2a1ad}".install_url = "https://addons.mozilla.org/firefox/downloads/latest/refined-github-/latest.xpi";
-          "{7a7a4a92-a2a0-41d1-9fd7-1e92480d612d}".install_url = "https://addons.mozilla.org/firefox/downloads/latest/styl-us/latest.xpi";
-        };
 
         # modified from getchoo since I'm too lazy to read the wiki right now
         # https://github.com/getchoo/flake/blob/b237ea94b95a7f75998a646fde903105807b24da/users/seth/programs/firefox/arkenfox.nix
