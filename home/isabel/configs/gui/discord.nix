fc4c1c003059b1b46de886b93f38ee0ae467e1d8
isabel <isabel@isabelroses.com>
2024-06-30 12:28:04 +0100

    chore(discord): add more settings

--- home/isabel/configs/gui/discord.nix
+++ home/isabel/configs/gui/discord.nix
@@ -28,6 +28,21 @@ in
       )
     ];
 
-    xdg.configFile."discord/settings.json".text = builtins.toJSON { SKIP_HOST_UPDATE = true; };
+    xdg.configFile."discord/settings.json".text = ''
+      {
+        "SKIP_HOST_UPDATE": true,
+        "DANGEROUS_ENABLE_DEVTOOLS_ONLY_ENABLE_IF_YOU_KNOW_WHAT_YOUR_DOING": true,
+        "MIN_WIDTH": 940,
+        "MIN_HEIGHT": 500,
+        "openasar": {
+          "setup": true,
+          "quickstart": true
+        },
+        "chromiumSwitches": {},
+        "IS_MAXIMIZED": true,
+        "IS_MINIMIZED": false,
+        "trayBalloonShown": true
+      }
+    '';
   };
 }
