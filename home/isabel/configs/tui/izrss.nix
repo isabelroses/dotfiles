8a702c82b95864b23fc576b96faf9b054b390f35
isabel <isabel@isabelroses.com>
2024-07-03 20:15:07 +0100

    feat(izrss): more config options

--- home/isabel/configs/tui/izrss.nix
+++ home/isabel/configs/tui/izrss.nix
@@ -7,20 +7,24 @@ in
     programs.izrss = {
       enable = true;
 
-      settings.urls = [
-        "https://isabelroses.com/feed.xml"
-        "https://uncenter.dev/feed.xml"
-        "https://charm.sh/blog/rss.xml"
-        "https://antfu.me/feed.xml"
-        "https://fasterthanli.me/index.xml"
-        "https://blog.orhun.dev/rss.xml"
-        "https://mitchellh.com/feed.xml"
-        "https://dataswamp.org/~solene/rss-html.xml"
-        "https://ayats.org/index.xml"
-        "https://nixpkgs.news/rss.xml"
-        "https://maia.crimew.gay/feed.xml"
-        "https://forum.aux.computer/tag/security-advisory.rss"
-      ];
+      settings = {
+        size = "full";
+
+        urls = [
+          "https://isabelroses.com/feed.xml"
+          "https://uncenter.dev/feed.xml"
+          "https://charm.sh/blog/rss.xml"
+          "https://antfu.me/feed.xml"
+          "https://fasterthanli.me/index.xml"
+          "https://blog.orhun.dev/rss.xml"
+          "https://mitchellh.com/feed.xml"
+          "https://dataswamp.org/~solene/rss-html.xml"
+          "https://ayats.org/index.xml"
+          "https://nixpkgs.news/rss.xml"
+          "https://maia.crimew.gay/feed.xml"
+          "https://forum.aux.computer/tag/security-advisory.rss"
+        ];
+      };
     };
   };
 }
