8e22db846600773a7ef9df60dbc0bbaf38806af9
isabel <isabel@isabelroses.com>
2024-07-03 13:59:33 +0100

    chore(security/pam): flatten

--- modules/nixos/security/pam.nix
+++ modules/nixos/security/pam.nix
@@ -1,29 +1,27 @@
 {
-  security = {
-    pam = {
-      # fix "too many files open" errors while writing a lot of data at once
-      # was previously a huge issue when rebuilding
-      loginLimits = [
-        {
-          domain = "@wheel";
-          item = "nofile";
-          type = "soft";
-          value = "524288";
-        }
-        {
-          domain = "@wheel";
-          item = "nofile";
-          type = "hard";
-          value = "1048576";
-        }
-      ];
+  security.pam = {
+    # fix "too many files open" errors while writing a lot of data at once
+    # was previously a huge issue when rebuilding
+    loginLimits = [
+      {
+        domain = "@wheel";
+        item = "nofile";
+        type = "soft";
+        value = "524288";
+      }
+      {
+        domain = "@wheel";
+        item = "nofile";
+        type = "hard";
+        value = "1048576";
+      }
+    ];
 
-      # allow screen lockers to also unlock the screen
-      # (e.g. swaylock, gtklock)
-      services = {
-        swaylock.text = "auth include login";
-        gtklock.text = "auth include login";
-      };
+    # allow screen lockers to also unlock the screen
+    # (e.g. swaylock, gtklock)
+    services = {
+      swaylock.text = "auth include login";
+      gtklock.text = "auth include login";
     };
   };
 }
