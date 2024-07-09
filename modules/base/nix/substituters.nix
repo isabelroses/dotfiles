20a72ef3ad1b8e7cd05c6e33cc8085a8c7db5903
isabel <isabel@isabelroses.com>
2024-07-09 15:19:45 +0100

    chore: cleanup subsitutors

--- modules/base/nix/substituters.nix
+++ modules/base/nix/substituters.nix
@@ -3,32 +3,26 @@
   nix.settings = {
     substituters = [
       "https://cache.nixos.org/" # official binary cache (yes the trailing slash is really neccacery)
-      "https://nixpkgs-wayland.cachix.org" # some wayland packages
       "https://nix-community.cachix.org" # nix-community cache
       "https://nixpkgs-unfree.cachix.org" # unfree-package cache
-      "https://numtide.cachix.org" # another unfree package cache
       "https://isabelroses.cachix.org" # precompiled binarys from flake
       "https://catppuccin.cachix.org" # a cache for ctp nix
       "https://pre-commit-hooks.cachix.org" # pre commit hooks
       "https://cache.garnix.io" # extra things here and there
-      "https://ags.cachix.org" # ags
-      "https://cache.lix.systems" # lix
       "https://hyprland.cachix.org" # hyprland
+      # "https://cache.lix.systems" # lix
     ];
 
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
-      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
       "isabelroses.cachix.org-1:mXdV/CMcPDaiTmkQ7/4+MzChpOe6Cb97njKmBQQmLPM="
       "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
       "pre-commit-hooks.cachix.org-1:Pkk3Panw5AW24TOv6kz3PvLhlH8puAsJTBbOPmBo7Rc="
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-      "ags.cachix.org-1:naAvMrz0CuYqeyGNyLgE010iUiuf/qx6kYrUv3NwAJ8="
-      "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      # "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
     ];
   };
 }
