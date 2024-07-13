ea3c71c5555184f4f9b65bff1bf1e3ba9c763824
isabel <isabel@isabelroses.com>
2024-07-13 16:06:36 +0100

    chore(shell): remove nodejs-slim
    i don't realllllly need nodejs when im devin

--- parts/programs/shell.nix
+++ parts/programs/shell.nix
@@ -16,22 +16,16 @@
 
           shellHook = config.pre-commit.installationScript;
 
-          # tell direnv to shut up
           DIRENV_LOG_FORMAT = "";
 
-          packages =
-            with pkgs;
-            [
-              git # flakes require git
-              nil # nix language server
-              just # quick and easy task runner
-              nodejs-slim # needed ags
-              self'.formatter # nix formatter
-              nix-output-monitor # get clean diff between generations
-              inputs'.agenix.packages.agenix # secrets
-              inputs'.deploy-rs.packages.deploy-rs # remote deployment
-            ]
-            ++ lib.optionals stdenv.isDarwin [ inputs'.darwin.packages.darwin-rebuild ];
+          packages = [
+            pkgs.git # flakes require git
+            pkgs.just # quick and easy task runner
+            self'.formatter # nix formatter
+            pkgs.nix-output-monitor # get clean diff between generations
+            inputs'.agenix.packages.agenix # secrets
+            inputs'.deploy-rs.packages.deploy-rs # remote deployment
+          ] ++ lib.optionals pkgs.stdenv.isDarwin [ inputs'.darwin.packages.darwin-rebuild ];
 
           inputsFrom = [ config.treefmt.build.devShell ];
         };
