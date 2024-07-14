69428f226504ff009fc06ef37c13b2448f1422ab
isabel <isabel@isabelroses.com>
2024-07-14 22:48:42 +0100

    fix(args): don't import

--- parts/args.nix
+++ parts/args.nix
@@ -5,7 +5,7 @@
   perSystem =
     { config, system, ... }:
     {
-      imports = [ { _module.args.pkgs = config.legacyPackages; } ];
+      _module.args.pkgs = config.legacyPackages;
 
       legacyPackages = import inputs.nixpkgs {
         inherit system;
