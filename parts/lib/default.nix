9c27ae969360fa6623925ece695d6d105a014407
isabel <isabel@isabelroses.com>
2024-07-15 10:09:40 +0100

    docs: lib

--- parts/lib/default.nix
+++ parts/lib/default.nix
@@ -19,7 +19,10 @@ let
       services = import ./services.nix { inherit lib; };
       validators = import ./validators.nix { inherit lib; };
 
-      inherit (self.builders) mkSystems mkNixosIsos;
+      # we have to rexport the functions we want to use, but don't want to refer to the whole lib
+      # "path". e.g. lib.hardware.isx86Linux can be shortened to lib.isx86Linux
+      # NOTE: never rexport templates
+      inherit (self.builders) mkSystems mkIsos;
       inherit (self.hardware) isx86Linux primaryMonitor ldTernary;
       inherit (self.helpers)
         filterNixFiles
@@ -43,6 +46,8 @@ let
     }
   );
 
+  # we need to extend gardenLib with the nixpkgs lib to get the full set of functions
+  # if we do it the otherway around we will get errors saying mkMerge and so on don't exist
   finalLib = gardenLib.extend (_: _: lib0);
 in
 {
