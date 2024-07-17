d5b38964def651e499832dfbcc8213d6dd31e8e1
isabel <isabel@isabelroses.com>
2024-07-17 21:57:54 +0100

    feat: save more space on iso

--- modules/iso/space.nix
+++ modules/iso/space.nix
@@ -11,10 +11,24 @@ in
   };
 
   # we don't need this, plus it adds extra programs to the iso
-  services.logrotate.enable = false;
+  services = {
+    logrotate.enable = false;
+    udisks2.enable = false;
+  };
 
   # disable fontConfig
   fonts.fontconfig.enable = mkForce false;
+
+  # disable containers as it also pulls in pearl
+  boot.enableContainers = false;
+
+  programs = {
+    # disable less as it pulls in pearl
+    less.lessopen = null;
+
+    # disable command-not-found and other similar programs
+    command-not-found.enable = false;
+  };
 
   # Use environment options, minimal profile
   environment = {
@@ -25,5 +39,12 @@ in
 
     # no packages other, other then the ones I provide
     defaultPackages = [ ];
+  };
+
+  xdg = {
+    autostart.enable = false;
+    icons.enable = false;
+    mime.enable = false;
+    sounds.enable = false;
   };
 }
