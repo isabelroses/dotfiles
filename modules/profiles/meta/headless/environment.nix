366c8d3d23a212094eba7f22ef6d6d5b63d80085
isabel <isabel@isabelroses.com>
2024-07-18 18:04:34 +0100

    fix: build errors on wsl

--- modules/profiles/meta/headless/environment.nix
+++ modules/profiles/meta/headless/environment.nix
@@ -2,7 +2,7 @@
   environment = {
     # normally we wouldn't need any Xlibs on a headless server but for whatever reason
     # this affects whether or not some programs can build - such as pipewire
-    noXlibs = true;
+    # noXlibs = true;
 
     # print the URL instead on servers
     variables.BROWSER = "echo";
