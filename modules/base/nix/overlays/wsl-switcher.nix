d886097eae7028435c488f8aa39f0bf914cc4723
isabel <isabel@isabelroses.com>
2024-07-22 16:03:20 +0100

    refactor: only enable the wsl fix on wsl

--- modules/base/nix/overlays/wsl-switcher.nix
+++ modules/base/nix/overlays/wsl-switcher.nix
@@ -1,6 +1,15 @@
-{ inputs', ... }:
 {
-  nixpkgs.overlays = [
+  lib,
+  config,
+  inputs',
+  ...
+}:
+let
+  inherit (lib.lists) optionals;
+  inherit (config.garden.device) type;
+in
+{
+  nixpkgs.overlays = optionals (type == "wsl") [
     (_: _: { inherit (inputs'.nixpkgs-wsl.legacyPackages) switch-to-configuration-ng; })
   ];
 }
