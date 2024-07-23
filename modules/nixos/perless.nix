7200c2e93168454ffe73bb7a26226b2525e65968
isabel <isabel@isabelroses.com>
2024-07-23 16:42:12 +0100

    feat: disable perlless bc its a bit goofy
    it was really fun to do this but its pretty goofy

--- modules/nixos/perless.nix
+++ modules/nixos/perless.nix
@@ -1,26 +1,26 @@
 # WARNING: it will become very apparent very quickly that this is not a good idea
 # or perhaps a bad idea, or something like that. maybe we should turn back now
 # but im committed to the bit, now we pray i don't endup with a broken system
-{ lib, ... }:
+# { lib, ... }:
 {
   system = {
     # WARNING: agenix enables this like such so that user passwords can be encrypted
     # but we specificlly undo this so we can use a perlless implementation
     # system.activationScripts.users.deps = ["agenixInstall"];
-    activationScripts = {
-      users = lib.modules.mkForce "";
-
-      # we have to help agenix out a bit by making it install the secrets
-      # since the original script would only run agenixInstall as a user dep that would
-      # give us an error, so we add the agenixInstall as a dep to the "main" agenix
-      # activation script such that it will run install for us instead
-      agenix.deps = [ "agenixInstall" ];
-    };
+    # activationScripts = {
+    #   users = lib.modules.mkForce "";
+    #
+    #   # we have to help agenix out a bit by making it install the secrets
+    #   # since the original script would only run agenixInstall as a user dep that would
+    #   # give us an error, so we add the agenixInstall as a dep to the "main" agenix
+    #   # activation script such that it will run install for us instead
+    #   agenix.deps = [ "agenixInstall" ];
+    # };
 
     # Mount /etc as an overlayfs instead of generating it via a perl script.
     # WARNING: do not enable this if your not confident in your ability to fix it
     # it is a royal pain and is not worth half the effor it takes to fix it
-    etc.overlay.enable = true;
+    # etc.overlay.enable = true;
 
     # WARNING: this leaves you without commands like `nixos-rebuild` which you don't
     # really need, you may consider enabling nh and using `nh os switch` instead
@@ -41,18 +41,18 @@
     # forbiddenDependenciesRegexes = [ "perl" ];
   };
 
-  # WARNING: this can break things, perticualarly if you use containers
+  # this can break things, perticualarly if you use containers
   # personally I don't so it should be fine to disable this
   boot.enableContainers = false;
 
   # WARNING: this is an experimental feature, and it is not enabled by default
   # and this will create users with a different script then the perl one
   # so it may cause breakages, be warned
-  systemd.sysusers.enable = true;
+  # systemd.sysusers.enable = true;
 
   # since we enable a hover fs we also need to allow for that filesystem in the supported filesystems
-  boot = {
-    supportedFilesystems = [ "erofs" ];
-    initrd.supportedFilesystems = [ "erofs" ];
-  };
+  # boot = {
+  #   supportedFilesystems = [ "erofs" ];
+  #   initrd.supportedFilesystems = [ "erofs" ];
+  # };
 }
