9c21b813823da70f330344447002734cdf12294f
isabel <isabel@isabelroses.com>
2024-07-25 14:34:49 +0100

    docs: packages warning

--- parts/packages/default.nix
+++ parts/packages/default.nix
@@ -1,3 +1,6 @@
+# I would highly advise you do not use my flake as an input and instead you vendor this
+# if you want to use this code, you may want to add my cachix cache to your flake
+# you may want to not have to build this for yourself however
 {
   perSystem =
     { inputs', ... }:
