b52d2c4ce81aeab0aae4582234c599d8708e8ba1
isabel <isabel@isabelroses.com>
2024-07-25 14:06:02 +0100

    chore: remove anything that doesn't exist

--- parts/modules/default.nix
+++ parts/modules/default.nix
@@ -5,15 +5,12 @@ let
 in
 {
   flake = {
-    # i do not provide a default module, so throw an error
-    nixosModules.default = builtins.throw "There is no default module.";
-    darwinModules.default = builtins.throw "There is no default module.";
-
     homeManagerModules = {
       gtklock = mkModule /modules/extra/home-manager/gtklock.nix;
 
       hyfetch = mkModule /modules/extra/home-manager/hyfetch.nix;
 
+      # i do not provide a default module, so throw an error
       default = builtins.throw "There is no default module.";
     };
   };
