b6bdf6fcd34397e2291b0c0c6df5b28b53c09886
isabel <isabel@isabelroses.com>
2024-07-26 21:31:40 +0100

    feat: simplify overlays

--- parts/overlays/default.nix
+++ parts/overlays/default.nix
@@ -1,14 +1,6 @@
 # this file creates an overlay based on the packgaes that out flake provides
-{ lib, self, ... }:
+{ self, ... }:
 {
-  flake.overlays.default =
-    _: prev:
-    let
-      # get a list of packages for the host system, and if none exist use an empty set
-      # this is so we don't fail later, since builtins.attrNames will know that we are
-      # an empty set is a empty list and won't generate an any attrs later
-      myPkgs = self.packages.${prev.stdenv.hostPlatform.system} or { };
-      myPkgsNames = builtins.attrNames myPkgs;
-    in
-    lib.attrsets.genAttrs myPkgsNames (name: myPkgs.${name});
+  # get a list of packages for the host system, and if none exist use an empty set
+  flake.overlays.default = _: prev: self.packages.${prev.stdenv.hostPlatform.system} or { };
 }
