39551c96afc81a87ddb5c7c4e79b9bbb6d9a0916
isabel <isabel@isabelroses.com>
2024-08-01 08:33:25 +0100

    feat(overlay): abstract common code

--- modules/base/nix/overlays/lix.nix
+++ modules/base/nix/overlays/lix.nix
@@ -5,16 +5,26 @@
     # build lix with ci, which is useful
     self.overlays.default
 
-    (_: prev: {
-      # in order to reduce our closure size, we can override these packages to use the nix package
-      # that we have installed, this will trigget a rebuild of the packages that depend on them
-      # so hopefully its worth it for that system space
-      nix-direnv = prev.nix-direnv.override { nix = config.nix.package; };
-      nix-index = prev.nix-index.override { nix = config.nix.package; };
-
-      # NOTE: this is not installed because i have a almost perlless system setup
-      # but this will remain here incase i change my mind
-      nixos-rebuild = prev.nixos-rebuild.override { nix = config.nix.package; };
-    })
+    # in order to reduce our closure size, we can override these packages to use the nix package
+    # that we have installed, this will trigget a rebuild of the packages that depend on them
+    # so hopefully its worth it for that system space
+    (
+      _: prev:
+      let
+        useOurNix =
+          names:
+          builtins.listToAttrs (
+            map (name: {
+              inherit name;
+              value = prev.${name}.override { nix = config.nix.package; };
+            }) names
+          );
+      in
+      useOurNix [
+        "nix-direnv"
+        "nix-index"
+        "nixos-rebuild"
+      ]
+    )
   ];
 }
