366d297d3f53c9f7c3ed6ccda8b5f82e72328470
isabel <isabel@isabelroses.com>
2024-08-07 10:56:11 +0100

    feat(lix): make patches more robust

--- parts/packages/default.nix
+++ parts/packages/default.nix
@@ -14,7 +14,7 @@
 
           versionSuffix = "-isabelroses-${inputs.self.shortRev or inputs.self.dirtyShortRev or "dirty"}";
         }).overrideAttrs
-          (_: {
+          (oa: {
             patches = [
               # preemptive merge of the pipe operator
               # https://gerrit.lix.systems/c/lix/+/1654
@@ -28,10 +28,12 @@
 
             # Kinda funny right
             # worth it https://akko.isabelroses.com/notice/AjlM7Vfq1zlgsEzk0G
-            postPatch = ''
-              substituteInPlace src/libmain/shared.cc \
-                --replace-fail "(Lix, like Nix)" "(Lix, like Nix but for lesbians)"
-            '';
+            postPatch =
+              (oa.postPatch or "")
+              + ''
+                substituteInPlace src/libmain/shared.cc \
+                  --replace-fail "(Lix, like Nix)" "(Lix, like Nix but for lesbians)"
+              '';
 
             # WARNING: This greatly assumes that lix will never merge a CL that breaks
             # the tests. But I choose to disable them anyway because the build time is faster
