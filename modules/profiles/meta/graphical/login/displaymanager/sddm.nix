f222ccea352f387fc196322bc4d33fab8e5543b2
isabel <isabel@isabelroses.com>
2024-08-09 20:31:55 +0100

    fix(sddm): use qt6 themes correctly

--- modules/profiles/meta/graphical/login/displaymanager/sddm.nix
+++ modules/profiles/meta/graphical/login/displaymanager/sddm.nix
@@ -1,11 +1,12 @@
-{ config, ... }:
+{ pkgs, config, ... }:
 let
   inherit (config.garden) environment;
 in
 {
   services.displayManager.sddm = {
     enable = environment.loginManager == "sddm";
-    wayland.enable = true;
+    package = pkgs.kdePackages.sddm; # allow qt6 themes to work
+    wayland.enable = true; # run under wayland rather than X11
     settings.General.InputMethod = "";
   };
 }
