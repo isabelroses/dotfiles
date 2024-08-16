4818cb077aa0248773ba633a59c1971b8b54a495
isabel <isabel@isabelroses.com>
2024-08-16 11:43:01 +0100

    feat: allow users to provide thier own mounts

--- modules/nixos/os/fs/drives.nix
+++ modules/nixos/os/fs/drives.nix
@@ -1,7 +1,8 @@
 { lib, config, ... }:
 let
-  inherit (lib.modules) mkIf;
-  inherit (lib.options) mkEnableOption;
+  inherit (lib.modules) mkIf mkMerge;
+  inherit (lib.types) attrs;
+  inherit (lib.options) mkOption mkEnableOption;
 
   cfg = config.garden.system.fs;
 in
@@ -9,20 +10,35 @@ in
   options.garden.system.fs = {
     enableDefaults = mkEnableOption "Enable default filesystems";
     enableSwap = mkEnableOption "Enable swap";
+
+    mounts = mkOption {
+      type = attrs;
+      default = { };
+      description = ''
+        A list of filesystems available supported by the system
+        it will enable services based on what strings are found in the list.
+
+        It would be a good idea to keep vfat and ext4 so you can mount USBs.
+      '';
+    };
   };
 
-  config = mkIf cfg.enableDefaults {
-    fileSystems = {
-      "/" = {
-        device = "/dev/disk/by-label/root";
-        fsType = "btrfs";
-      };
+  config = {
+    fileSystems = mkMerge [
+      (mkIf cfg.enableDefaults {
+        "/" = {
+          device = "/dev/disk/by-label/root";
+          fsType = "btrfs";
+        };
 
-      "/boot" = {
-        device = "/dev/disk/by-label/boot";
-        fsType = "vfat";
-      };
-    };
+        "/boot" = {
+          device = "/dev/disk/by-label/boot";
+          fsType = "vfat";
+        };
+      })
+
+      cfg.mounts
+    ];
 
     swapDevices = mkIf cfg.enableSwap [ { device = "/dev/disk/by-label/swap"; } ];
   };
