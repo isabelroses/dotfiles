588545d8c5a49d68618967f56ce0411c27304c01
isabel <isabel@isabelroses.com>
2024-08-18 20:03:33 +0100

    fix(forgjo): hash

--- modules/nixos/services/selfhosted/forgejo.nix
+++ modules/nixos/services/selfhosted/forgejo.nix
@@ -18,7 +18,7 @@ let
   # stole it from https://github.com/getchoo
   theme = pkgs.fetchzip {
     url = "https://github.com/catppuccin/gitea/releases/download/v0.4.1/catppuccin-gitea.tar.gz";
-    hash = "sha256-/P4fLvswitlfeaKaUykrEKvjbNpw5Q/nzGQ/GZaLyUI=";
+    hash = "sha256-14XqO1ZhhPS7VDBSzqW55kh6n5cFZGZmvRCtMEh8JPI=";
     stripRoot = false;
   };
 in
