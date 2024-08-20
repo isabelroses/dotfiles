9e8901aa6d4e038649c6191d9c71e0e25da750ee
isabel <isabel@isabelroses.com>
2024-08-20 08:58:38 +0100

    feat(kanidm): enable provisioning

--- modules/nixos/services/selfhosted/kanidm.nix
+++ modules/nixos/services/selfhosted/kanidm.nix
@@ -13,8 +13,8 @@ let
   certDir = certs.directory;
 
   cfg = config.garden.services.kanidm;
+  cfg' = config.garden.services;
 in
-# cfg' = config.garden.services;
 {
   options.garden.services.kanidm = mkServiceOption "kanidm" {
     port = 8443;
@@ -51,79 +51,79 @@ in
             schedule = "0 0 * * *";
           };
 
-          # provision =
-          #   let
-          #     inherit (config.garden.system) mainUser;
-          #   in
-          #   {
-          #     enable = true;
-          #
-          #     adminPasswordFile = config.age.secrets.kanidm-admin-password.path;
-          #     idmAdminPasswordFile = config.age.secrets.kanidm-idm-admin-password.path;
-          #
-          #     persons = {
-          #       ${mainUser} = {
-          #         displayName = mainUser;
-          #         legalName = mainUser;
-          #         mailAddresses = [ "${mainUser}@${rdomain}" ];
-          #         groups = [
-          #           "grafana.access"
-          #           "grafana.admins"
-          #           "forgejo.access"
-          #           "forgejo.admins"
-          #         ];
-          #       };
-          #     };
-          #
-          #     groups = {
-          #       "grafana.access" = { };
-          #       "grafana.admins" = { };
-          #
-          #       "forgejo.access" = { };
-          #       "forgejo.admins" = { };
-          #     };
-          #
-          #     systems.oauth2 = {
-          #       grafana = {
-          #         displayName = "Grafana";
-          #         originUrl = "https://${cfg'.grafana.domain}/";
-          #         basicSecretFile = config.age.secrets.kanidm-oauth2-grafana.path;
-          #         preferShortUsername = true;
-          #         scopeMaps."grafana.access" = [
-          #           "openid"
-          #           "email"
-          #           "profile"
-          #         ];
-          #         claimMaps.groups = {
-          #           joinType = "array";
-          #           valuesByGroup = {
-          #             "grafana.editors" = [ "editor" ];
-          #             "grafana.admins" = [ "admin" ];
-          #             "grafana.server-admins" = [ "server_admin" ];
-          #           };
-          #         };
-          #       };
-          #
-          #       forgejo = {
-          #         displayName = "Forgejo";
-          #         originUrl = "https://${cfg'.forgejo.domain}/";
-          #         basicSecretFile = config.age.secrets.kanidm-oauth2-forgejo.path;
-          #         scopeMaps."forgejo.access" = [
-          #           "openid"
-          #           "email"
-          #           "profile"
-          #         ];
-          #         # WARNING: PKCE is currently not supported by gitea/forgejo,
-          #         # see https://github.com/go-gitea/gitea/issues/21376.
-          #         allowInsecureClientDisablePkce = true;
-          #         preferShortUsername = true;
-          #         claimMaps.groups = {
-          #           joinType = "array";
-          #           valuesByGroup."forgejo.admins" = [ "admin" ];
-          #         };
-          #       };
-          #     };
-          #   };
+          provision =
+            let
+              inherit (config.garden.system) mainUser;
+            in
+            {
+              enable = true;
+
+              adminPasswordFile = config.age.secrets.kanidm-admin-password.path;
+              idmAdminPasswordFile = config.age.secrets.kanidm-idm-admin-password.path;
+
+              persons = {
+                ${mainUser} = {
+                  displayName = mainUser;
+                  legalName = mainUser;
+                  mailAddresses = [ "${mainUser}@${rdomain}" ];
+                  groups = [
+                    "grafana.access"
+                    "grafana.admins"
+                    "forgejo.access"
+                    "forgejo.admins"
+                  ];
+                };
+              };
+
+              groups = {
+                "grafana.access" = { };
+                "grafana.admins" = { };
+
+                "forgejo.access" = { };
+                "forgejo.admins" = { };
+              };
+
+              systems.oauth2 = {
+                grafana = {
+                  displayName = "Grafana";
+                  originUrl = "https://${cfg'.grafana.domain}/";
+                  basicSecretFile = config.age.secrets.kanidm-oauth2-grafana.path;
+                  preferShortUsername = true;
+                  scopeMaps."grafana.access" = [
+                    "openid"
+                    "email"
+                    "profile"
+                  ];
+                  claimMaps.groups = {
+                    joinType = "array";
+                    valuesByGroup = {
+                      "grafana.editors" = [ "editor" ];
+                      "grafana.admins" = [ "admin" ];
+                      "grafana.server-admins" = [ "server_admin" ];
+                    };
+                  };
+                };
+
+                forgejo = {
+                  displayName = "Forgejo";
+                  originUrl = "https://${cfg'.forgejo.domain}/";
+                  basicSecretFile = config.age.secrets.kanidm-oauth2-forgejo.path;
+                  scopeMaps."forgejo.access" = [
+                    "openid"
+                    "email"
+                    "profile"
+                  ];
+                  # WARNING: PKCE is currently not supported by gitea/forgejo,
+                  # see https://github.com/go-gitea/gitea/issues/21376
+                  allowInsecureClientDisablePkce = true;
+                  preferShortUsername = true;
+                  claimMaps.groups = {
+                    joinType = "array";
+                    valuesByGroup."forgejo.admins" = [ "admin" ];
+                  };
+                };
+              };
+            };
         };
       };
 
