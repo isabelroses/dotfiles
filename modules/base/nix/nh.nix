6ddfbf358e278b4e351f5fd3587a900ef335abc0
isabel <isabel@isabelroses.com>
2024-08-22 12:09:39 +0100

    feat(darwin): disableInstallerTools

--- modules/base/nix/nh.nix
+++ modules/base/nix/nh.nix
@@ -8,4 +8,9 @@
       dates = "weekly";
     };
   };
+
+  # WARNING: this leaves you without commands like `nixos-rebuild` which you don't
+  # really need, you may consider enabling nh and using `nh os switch` instead
+  # which is actually a really good alternative to using this
+  system.disableInstallerTools = config.programs.nh.enable;
 }
--- modules/nixos/perless.nix
+++ modules/nixos/perless.nix
@@ -11,21 +11,14 @@ in
 
   config = mkMerge [
     {
-      system = {
-        # WARNING: this leaves you without commands like `nixos-rebuild` which you don't
-        # really need, you may consider enabling nh and using `nh os switch` instead
-        # which is actually a really good alternative to using this
-        disableInstallerTools = true;
-
-        # I would rather use the rust implementation of the switch-to-configuration
-        # called switch-to-configuration-ng, a thankfully perlless switcher.
-        # It is a much better, faster and smaller implementation
-        # WARNING: it is broken on WSL with a PR open to fix it
-        # https://github.com/NixOS/nixpkgs/pull/321662
-        switch = {
-          enable = false; # disable the old implementation
-          enableNg = true; # enable the new implementation
-        };
+      # I would rather use the rust implementation of the switch-to-configuration
+      # called switch-to-configuration-ng, a thankfully perlless switcher.
+      # It is a much better, faster and smaller implementation
+      # WARNING: it is broken on WSL with a PR open to fix it
+      # https://github.com/NixOS/nixpkgs/pull/321662
+      system.switch = {
+        enable = false; # disable the old implementation
+        enableNg = true; # enable the new implementation
       };
 
       # this can break things, perticualarly if you use containers
