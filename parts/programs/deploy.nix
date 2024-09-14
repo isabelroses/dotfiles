061cad6c613a616deb164d3330443b76685e79af
isabel <isabel@isabelroses.com>
2024-09-14 15:42:59 +0100

    feat: generate deployments

--- parts/programs/deploy.nix
+++ parts/programs/deploy.nix
@@ -1,18 +1,42 @@
-{ self, inputs, ... }:
 {
-  flake.deploy = {
-    autoRollback = true;
-    magicRollback = true;
+  lib,
+  self,
+  inputs,
+  ...
+}:
+let
+  inherit (builtins) mapAttrs;
+  inherit (lib.attrsets) filterAttrs;
+  inherit (lib.lists) elem;
 
-    # TODO: make this a function, that detects systems by their name
-    nodes.minerva = {
-      hostname = "minerva";
-      skipChecks = true;
-      sshUser = "isabel";
-      profiles.system = {
-        user = "root";
-        path = inputs.deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.minerva;
-      };
+  # create a list of all systems that are allowed to be deployed
+  allowedSystems = [ "minerva" ];
+  # then extract those systems that we allow to be deployed from our nixosConfigurations
+  systems = filterAttrs (name: _: elem name allowedSystems) self.nixosConfigurations;
+
+  # then create a list of nodes that we want to deploy that we can pass to the deploy configuration
+  nodes = mapAttrs (name: node: {
+    hostname = name;
+    skipChecks = true;
+    sshUser = "isabel";
+    profiles.system = {
+      user = "root";
+      path = inputs.deploy-rs.lib.${node.config.nixpkgs.hostPlatform.system}.activate.nixos node;
+    };
+  }) systems;
+in
+{
+  flake = {
+    checks = {
+      x86_64-linux = inputs.deploy-rs.lib.x86_64-linux.deployChecks self.deploy;
+      aarch64-linux = inputs.deploy-rs.lib.aarch64-linux.deployChecks self.deploy;
+    };
+
+    deploy = {
+      autoRollback = true;
+      magicRollback = true;
+
+      inherit nodes;
     };
   };
 }
