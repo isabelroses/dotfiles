89dfc1fbd6845a345725266f21575a7132f5a6e2
isabel <isabel@isabelroses.com>
2024-09-15 14:16:04 +0100

    chore: cleanup package list

--- home/isabel/packages/tui/default.nix
+++ home/isabel/packages/tui/default.nix
@@ -6,16 +6,25 @@
 }:
 let
   inherit (lib.modules) mkIf;
+  inherit (lib.lists) optionals;
+
+  cfg = osConfig.garden.programs;
 in
 {
-  config = mkIf osConfig.garden.programs.tui.enable {
-    home.packages = builtins.attrValues {
-      inherit (pkgs)
-        # wishlist # fancy ssh
-        glow # fancy markdown
-        # fx # fancy jq
-        # gum # a nicer scripting
-        ;
-    };
+  config = mkIf cfg.tui.enable {
+    home.packages =
+      builtins.attrValues {
+        inherit (pkgs)
+          # wishlist # fancy ssh
+          glow # fancy markdown
+          # fx # fancy jq
+          # gum # a nicer scripting
+          ;
+      }
+      ++ optionals cfg.gui.enable (
+        builtins.attrValues {
+          inherit (pkgs) manga-tui; # tui manga finder + reader
+        }
+      );
   };
 }
