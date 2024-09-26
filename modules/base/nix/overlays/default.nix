8b9d190b9bff07b747173114da91d7cf340e1e2e
isabel <isabel@isabelroses.com>
2024-09-26 15:33:05 +0100

    docs(overlays): make simpler to understand

--- modules/base/nix/overlays/default.nix
+++ modules/base/nix/overlays/default.nix
@@ -1,13 +1,16 @@
 { self, config, ... }:
 {
   nixpkgs.overlays = [
+    # i want to use my own overlays
+    # this is also how we pull in the patches applied to lix,
+    # we do it this way such that we can build lix with ci, which is useful
+    self.overlays.default
+
     # this file exists to work around issues with nixpkgs that may arise
     # hopefully that means its empty a lot
     (final: prev: import ./fixes.nix final prev)
 
-    # this is how we pull in the patches applied to lix, we do it this way such that we can
-    # build lix with ci, which is useful
-    self.overlays.default
+    # we minimize the amount of packages that are installed
     (_: prev: import ./nix.nix { inherit config prev; })
 
     # remove desktop files from apps because i find them annoying
