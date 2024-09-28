8c88960c909e66e13ecf94f432a4ec9a0ab21ae9
isabel <isabel@isabelroses.com>
2024-09-28 16:55:02 +0100

    refactor(ccache): cleanup

--- modules/profiles/meta/graphical/programs/ccache.nix
+++ modules/profiles/meta/graphical/programs/ccache.nix
@@ -1,6 +1,9 @@
 { lib, config, ... }:
 let
-  inherit (lib) mkIf;
+  inherit (lib.lists) singleton optional;
+  inherit (lib.trivial) const;
+
+  cfg = config.programs.ccache;
 in
 {
   programs.ccache = {
@@ -16,37 +19,37 @@ in
     argument = "-";
   };
 
-  nix.settings.extra-sandbox-paths = [ config.programs.ccache.cacheDir ];
+  nix.settings.extra-sandbox-paths = [ cfg.cacheDir ];
 
-  nixpkgs.overlays =
-    mkIf (config.programs.ccache.enable && config.programs.ccache.packageNames == [ ])
-      [
-        (_: super: {
-          ccacheWrapper = super.ccacheWrapper.override {
-            extraConfig = ''
-              export CCACHE_COMPRESS=1
-              export CCACHE_DIR="${config.programs.ccache.cacheDir}"
-              export CCACHE_UMASK=007
-              export CCACHE_SLOPPINESS=include_file_mtime,time_macros
-              export CCACHE_NODIRECT=1
-              if [ ! -d "$CCACHE_DIR" ]; then
-                echo "====="
-                echo "Directory '$CCACHE_DIR' does not exist"
-                echo "Please create it with:"
-                echo "  sudo mkdir -m0770 '$CCACHE_DIR'"
-                echo "  sudo chown root:nixbld '$CCACHE_DIR'"
-                echo "====="
-                exit 1
-              fi
-              if [ ! -w "$CCACHE_DIR" ]; then
-                echo "====="
-                echo "Directory '$CCACHE_DIR' is not accessible for user $(whoami)"
-                echo "Please verify its access permissions"
-                echo "====="
-                exit 1
-              fi
-            '';
-          };
-        })
-      ];
+  nixpkgs.overlays = optional (cfg.enable && cfg.packageNames == [ ]) (
+    singleton (
+      const (super: {
+        ccacheWrapper = super.ccacheWrapper.override {
+          extraConfig = ''
+            export CCACHE_COMPRESS=1
+            export CCACHE_DIR="${cfg.cacheDir}"
+            export CCACHE_UMASK=007
+            export CCACHE_SLOPPINESS=include_file_mtime,time_macros
+            export CCACHE_NODIRECT=1
+            if [ ! -d "$CCACHE_DIR" ]; then
+              echo "====="
+              echo "Directory '$CCACHE_DIR' does not exist"
+              echo "Please create it with:"
+              echo "  sudo mkdir -m0770 '$CCACHE_DIR'"
+              echo "  sudo chown root:nixbld '$CCACHE_DIR'"
+              echo "====="
+              exit 1
+            fi
+            if [ ! -w "$CCACHE_DIR" ]; then
+              echo "====="
+              echo "Directory '$CCACHE_DIR' is not accessible for user $(whoami)"
+              echo "Please verify its access permissions"
+              echo "====="
+              exit 1
+            fi
+          '';
+        };
+      })
+    )
+  );
 }
