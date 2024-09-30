586b52d3ed997a870e1f9f0f8c84c7ee2b268fcd
isabel <isabel@isabelroses.com>
2024-09-30 15:50:40 +0100

    feat!: only show diff when nh is not being used

--- modules/base/activation/diff.nix
+++ modules/base/activation/diff.nix
@@ -9,20 +9,23 @@ let
   inherit (lib.options) mkEnableOption;
 in
 {
-  # NOTE: this is not enabled by default because we have nh, which does the same
-  options.garden.system.activation.diff.enable = mkEnableOption "Enable a system diff";
+  options.garden.system.activation.diff.enable = mkEnableOption "Enable a system diff" // {
+    default = !config.programs.nh.enable;
+  };
 
   # if the system supports dry activation, this means that we can compare
   # the current system with the one we are about to switch to
   # this can be useful to see what will change, and the clousure size
-  config.system.activationScripts.diff = mkIf config.garden.system.activation.diff.enable {
-    supportsDryActivation = true;
-    text = ''
-      if [[ -e /run/current-system ]]; then
-        echo "=== diff to current-system ==="
-        ${pkgs.nvd}/bin/nvd --nix-bin-dir='${config.nix.package}/bin' diff /run/current-system "$systemConfig"
-        echo "=== end of the system diff ==="
-      fi
-    '';
-  };
+  config.system.activationScripts.diff =
+    mkIf (config.garden.system.activation.diff.enable && !config.programs.nh.enable)
+      {
+        supportsDryActivation = true;
+        text = ''
+          if [[ -e /run/current-system ]]; then
+            echo "=== diff to current-system ==="
+            ${pkgs.nvd}/bin/nvd --nix-bin-dir='${config.nix.package}/bin' diff /run/current-system "$systemConfig"
+            echo "=== end of the system diff ==="
+          fi
+        '';
+      };
 }
