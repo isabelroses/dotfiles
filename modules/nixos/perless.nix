441b4f5d1bccd6b645c8c81c746715eab1a61249
isabel <isabel@isabelroses.com>
2024-10-02 15:47:29 +0100

    chore: remove redunatnt code; switchNg beacame the default

--- modules/nixos/perless.nix
+++ modules/nixos/perless.nix
@@ -11,16 +11,6 @@ in
 
   config = mkMerge [
     {
-      # I would rather use the rust implementation of the switch-to-configuration
-      # called switch-to-configuration-ng, a thankfully perlless switcher.
-      # It is a much better, faster and smaller implementation
-      # WARNING: it is broken on WSL with a PR open to fix it
-      # https://github.com/NixOS/nixpkgs/pull/321662
-      system.switch = {
-        enable = false; # disable the old implementation
-        enableNg = true; # enable the new implementation
-      };
-
       # this can break things, perticualarly if you use containers
       # personally I don't so it should be fine to disable this
       boot.enableContainers = false;
