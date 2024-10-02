04cb7edbffa76269ca86049d4c6f6094b4c54d23
isabel <isabel@isabelroses.com>
2024-10-02 16:49:31 +0100

    chore: bump darwin system

--- modules/base/nix/system.nix
+++ modules/base/nix/system.nix
@@ -12,7 +12,7 @@ in
   system = {
     # this is the NixOS version that the configuration was generated with
     # this should be change to the version of the NixOS release that the configuration was generated with
-    stateVersion = mkDefault (ldTernary pkgs "23.05" 4);
+    stateVersion = mkDefault (ldTernary pkgs "23.05" 5);
 
     # we can get the git rev that we are working on and set that to the configurationRevision
     # this *might* be useful for debugging
