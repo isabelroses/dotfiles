7d82d7980bb32102346e074624e356a079999459
isabel <isabel@isabelroses.com>
2024-10-03 23:28:08 +0100

    docs: manuals are bad

--- home/isabel/system/docs.nix
+++ home/isabel/system/docs.nix
@@ -9,6 +9,11 @@ in
   programs.man.enable = false;
 
   # I don't use docs, so just disable them
+  #
+  # the docs also create issues that are really hard to debug
+  # such as ones where lib does not get passed correctly,
+  # see an example of me complaining and then finding this as the fix
+  # https://akko.isabelroses.com/notice/AkdGj5udRhYIKqTeng
   manual = mapAttrs (_: mkForce) {
     html.enable = false;
     json.enable = false;
