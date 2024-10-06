15f87bb045dbf192823290de68d8e2c19bfd2cf9
isabel <isabel@isabelroses.com>
2024-10-06 11:28:59 +0100

    chore: remove fixes

--- modules/base/nix/overlays/default.nix
+++ modules/base/nix/overlays/default.nix
@@ -8,7 +8,7 @@
 
     # this file exists to work around issues with nixpkgs that may arise
     # hopefully that means its empty a lot
-    (final: prev: import ./fixes.nix final prev)
+    # (final: prev: import ./fixes.nix final prev)
 
     # we minimize the amount of packages that are installed
     (_: prev: import ./nix.nix { inherit config prev; })
--- modules/base/nix/overlays/fixes.nix
+++ modules/base/nix/overlays/fixes.nix
@@ -1,12 +1,3 @@
-_: prev: {
-  # FIXME: https://github.com/NixOS/nixpkgs/pull/341232
-  subversionClient = prev.subversionClient.overrideAttrs (_: {
-    env = prev.lib.optionalAttrs prev.stdenv.cc.isClang {
-      NIX_CFLAGS_COMPILE = toString [
-        "-Wno-error=implicit-function-declaration"
-        "-Wno-error=implicit-int"
-        "-Wno-int-conversion"
-      ];
-    };
-  });
+_: _: {
+
 }
