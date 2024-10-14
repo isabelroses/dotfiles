4db23dbf515519c1f213fbea4b739f54e3154d80
isabel <isabel@isabelroses.com>
2024-10-14 21:17:47 +0100

    chore: cleanup substituers

--- modules/base/nix/substituters.nix
+++ modules/base/nix/substituters.nix
@@ -2,13 +2,12 @@
   # substituters to use
   nix.settings = {
     substituters = [
-      "https://cache.nixos.org/" # official binary cache (yes the trailing slash is really neccacery)
+      "https://cache.nixos.org" # official binary cache
       "https://nix-community.cachix.org" # nix-community cache
       "https://nixpkgs-unfree.cachix.org" # unfree-package cache
       "https://isabelroses.cachix.org" # precompiled binarys from flake
       "https://catppuccin.cachix.org" # a cache for ctp nix
       "https://pre-commit-hooks.cachix.org" # pre commit hooks
-      "https://hyprland.cachix.org" # hyprland
     ];
 
     trusted-public-keys = [
@@ -18,7 +17,6 @@
       "isabelroses.cachix.org-1:mXdV/CMcPDaiTmkQ7/4+MzChpOe6Cb97njKmBQQmLPM="
       "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
       "pre-commit-hooks.cachix.org-1:Pkk3Panw5AW24TOv6kz3PvLhlH8puAsJTBbOPmBo7Rc="
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     ];
   };
 }
