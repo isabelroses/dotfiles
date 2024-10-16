174396fa69b90284e33ab467fb3c463c8eeaf201
isabel <isabel@isabelroses.com>
2024-10-17 00:20:22 +0100

    fix: unset nixpkgs-config env var

--- modules/base/nix/environment.nix
+++ modules/base/nix/environment.nix
@@ -9,11 +9,15 @@ let
   inherit (lib.trivial) pipe;
   inherit (lib.lists) optionals;
   inherit (lib.attrsets) filterAttrs mapAttrs';
+  inherit (lib.modules) mkForce;
 in
 {
   environment = {
     # git is required for flakes
     systemPackages = [ pkgs.git ];
+
+    # https://github.com/NixOS/nixpkgs/blob/eca4605163a534aed1981de0f5f1d7d7639d1640/nixos/modules/programs/environment.nix#L18
+    variables.NIXPKGS_CONFIG = mkForce "";
 
     # something something backwards compatibility something something nix channels
     etc =
