1ffb22a812db46882c5f87d9b17612618f6f04d0
isabel <isabel@isabelroses.com>
2024-10-23 19:34:57 +0100

    fix: tempoary work around qt ctp 24.11

--- home/isabel/themes/qt.nix
+++ home/isabel/themes/qt.nix
@@ -1,6 +1,7 @@
 {
   lib,
   pkgs,
+  config,
   osConfig,
   ...
 }:
@@ -20,8 +21,29 @@ in
     qt = {
       enable = true;
       platformTheme.name = "kvantum";
-      style.name = "kvantum";
+      style = {
+        name = "kvantum";
+        # FIXME: remove when 24.11 is released
+        catppuccin.enable = false;
+      };
     };
+
+    xdg.configFile =
+      let
+        inherit (config.catppuccin) accent;
+        variant = config.catppuccin.flavor;
+        theme = pkgs.catppuccin-kvantum.override { inherit accent variant; };
+        themeName = "catppuccin-${variant}-${accent}";
+      in
+      {
+        "Kvantum/${themeName}".source = "${theme}/share/Kvantum/${themeName}";
+        "Kvantum/kvantum.kvconfig" = {
+          text = ''
+            [General]
+            theme=${themeName}
+          '';
+        };
+      };
 
     home.sessionVariables = {
       # scaling - 1 means no scaling
