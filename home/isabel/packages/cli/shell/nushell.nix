2041a1b54f6217895036be2b05ced3cf42b202e9
isabel <isabel@isabelroses.com>
2024-11-02 22:32:10 +0000

    chore(nushell): misc updates

--- home/isabel/packages/cli/shell/nushell.nix
+++ home/isabel/packages/cli/shell/nushell.nix
@@ -5,19 +5,19 @@
   ...
 }:
 let
-  inherit (lib.meta) getExe;
   inherit (lib.attrsets) mapAttrs;
   inherit (lib.strings) escapeShellArg concatMapStrings;
 in
 {
+  home.packages = [ pkgs.nix-your-shell ];
+
   programs.nushell = {
     enable = false;
 
     shellAliases = builtins.removeAttrs config.home.shellAliases [ "mkdir" ];
 
     environmentVariables = {
-      DIRENV_LOG_FORMAT = "''";
-      SHELL = "'${getExe pkgs.nushell}'";
+      DIRENV_LOG_FORMAT = "";
       # PATH = "($env.PATH | split row (char esep) | append [${escapeShellArgs config.home.sessionPath}])";
     } // mapAttrs (_: v: (escapeShellArg v)) config.home.sessionVariables;
 
@@ -31,21 +31,27 @@ in
 
         theme = "catppuccin-${config.catppuccin.flavor}";
       in
+      completions [
+        "nix"
+        "git"
+        "curl"
+        "bat"
+        "cargo"
+        "gh"
+        "glow"
+        "just"
+        "rg"
+        "npm"
+        "pnpm"
+        "tar"
+      ]
       # nu
-      ''
-        ${completions [
-          "nix"
-          "git"
-          "curl"
-          "bat"
-          "cargo"
-          "gh"
-          "glow"
-          "just"
-          "rg"
-        ]}
-
+      + ''
         use ${pkgs.nu_scripts}/share/nu_scripts/themes/nu-themes/${theme}.nu
+
+        # occasionally generate this with
+        # nix-your-shell nu | save $env.XDG_CONFIG_HOME/nushell/nix-your-shell.nu
+        source nix-your-shell.nu
 
         $env.config = {
           show_banner: false,
@@ -55,12 +61,14 @@ in
           ls: {
             clickable_links: true
           }
-          color_config = (${theme})
+          color_config: (${theme})
+          error_style: "fancy"
           completions: {
             case_sensitive: false
             quick: true
             partial: true
             algorithm: "fuzzy"
+            use_ls_colors: true
           }
         }
       '';
