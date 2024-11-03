46e924feb5c02334d66add28e838646416019e5f
isabel <isabel@isabelroses.com>
2024-11-03 11:38:11 +0000

    chore: cleanup subsituters

--- modules/base/nix/substituters.nix
+++ modules/base/nix/substituters.nix
@@ -1,22 +1,19 @@
 {
   # substituters to use
   nix.settings = {
+    # don't worry that you cannot see cache.nixos.org here, it is added by default
     substituters = [
-      "https://cache.nixos.org" # official binary cache
       "https://nix-community.cachix.org" # nix-community cache
-      "https://nixpkgs-unfree.cachix.org" # unfree-package cache
       "https://isabelroses.cachix.org" # precompiled binarys from flake
-      "https://catppuccin.cachix.org" # a cache for ctp nix
       "https://pre-commit-hooks.cachix.org" # pre commit hooks
+      "https://cosmic.cachix.org" # for the cosmic desktop
     ];
 
     trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
       "isabelroses.cachix.org-1:mXdV/CMcPDaiTmkQ7/4+MzChpOe6Cb97njKmBQQmLPM="
-      "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
       "pre-commit-hooks.cachix.org-1:Pkk3Panw5AW24TOv6kz3PvLhlH8puAsJTBbOPmBo7Rc="
+      "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
     ];
   };
 }
