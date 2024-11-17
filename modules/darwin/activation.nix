a7efc286ad28ea6e40e45d1010c55fafc6978a27
isabel <isabel@isabelroses.com>
2024-11-17 13:21:53 +0000

    fix: using this lead to hm failing to switch silently

--- modules/darwin/activation.nix
+++ modules/darwin/activation.nix
@@ -1,14 +1,5 @@
 {
   system.activationScripts = {
-    # Settings that don't have an option in nix-darwin
-    postActivation.text = ''
-      echo "Allow apps from anywhere"
-      SPCTL=$(spctl --status)
-      if ! [ "$SPCTL" = "assessments disabled" ]; then
-          sudo spctl --master-disable
-      fi
-    '';
-
     # activateSettings -u will reload the settings from the database and apply them to the current session,
     # so we do not need to logout and login again to make the changes take effect.
     #
