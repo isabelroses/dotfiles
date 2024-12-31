fff87a399f056f1259a914ce0aa563da05f8e66c
isabel <isabel@isabelroses.com>
2024-12-31 21:14:26 +0000

    feat: add ctp subsituor

--- modules/base/nix/substituters.nix
+++ modules/base/nix/substituters.nix
@@ -5,6 +5,7 @@
     substituters = [
       "https://nix-community.cachix.org" # nix-community cache
       "https://isabelroses.cachix.org" # precompiled binarys from flake
+      "https://catppuccin.cachix.org" # a cache for all catppuccin ports
       "https://pre-commit-hooks.cachix.org" # pre commit hooks
       "https://cosmic.cachix.org" # for the cosmic desktop
     ];
@@ -12,6 +13,7 @@
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "isabelroses.cachix.org-1:mXdV/CMcPDaiTmkQ7/4+MzChpOe6Cb97njKmBQQmLPM="
+      "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
       "pre-commit-hooks.cachix.org-1:Pkk3Panw5AW24TOv6kz3PvLhlH8puAsJTBbOPmBo7Rc="
       "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
     ];
