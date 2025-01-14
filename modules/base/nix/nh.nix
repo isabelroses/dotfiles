8c9d6176cfb32aba4aa30eabff0dc0bafe13088a
isabel <isabel@isabelroses.com>
2025-01-14 23:53:13 +0000

    feat: use nh unstable

--- modules/base/nix/nh.nix
+++ modules/base/nix/nh.nix
@@ -1,7 +1,13 @@
-{ config, ... }:
+{
+  lib,
+  config,
+  inputs',
+  ...
+}:
 {
   programs.nh = {
     enable = true;
+    package = inputs'.beapkgs.packages.nh;
 
     clean = {
       enable = !config.nix.gc.automatic;
@@ -12,5 +18,11 @@
   # WARNING: this leaves you without commands like `nixos-rebuild` which you don't
   # really need, you may consider enabling nh and using `nh os switch` instead
   # which is actually a really good alternative to using this
-  system.disableInstallerTools = config.programs.nh.enable;
+  system =
+    {
+      disableInstallerTools = config.programs.nh.enable;
+    }
+    // lib.attrsets.optionalAttrs (config.system ? tools) {
+      tools.nixos-version.enable = true;
+    };
 }
