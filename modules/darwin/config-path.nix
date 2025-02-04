5ead108ac20fdca523607b6b4c69ddfbb68ba561
isabel <isabel@isabelroses.com>
2025-02-04 10:45:22 +0000

    chore: update to darwin v6 and remove docs

---
+++ modules/darwin/config-path.nix
@@ -0,0 +1,4 @@
+{
+  # do not set the path to the darwin configuration
+  environment.darwinConfig = null;
+}
--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -7,6 +7,8 @@
     ./services # services exclusive to nix-darwin
 
     ./activation.nix # run when we start the system
+    ./config-path.nix # set the path to the darwin configuration
+    ./documentation.nix # turn off docs
     ./nix.nix # nix settings that can only be applied to nix-darwin
     ./remote-modules.nix # modules that are not in this repo, and don't have a nice place to be imported in
   ];
