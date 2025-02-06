025602fa44cd15de7383444f24d74566ceb0747d
isabel <isabel@isabelroses.com>
2025-02-06 14:57:34 +0000

    feat!: remove catppuccin from robin's config

--- home/robin/themes/global.nix
+++ home/robin/themes/global.nix
@@ -2,51 +2,19 @@
   lib,
   pkgs,
   self,
-  inputs,
   config,
   osConfig,
   ...
 }:
 let
-  inherit (self.lib.validators) isAcceptedDevice;
-  nonAccepted = [
-    "server"
-    "wsl"
-  ];
-
   en = pkgs.stdenv.hostPlatform.isLinux && config.garden.programs.gui.enable;
 in
 {
-  imports = [ inputs.catppuccin.homeManagerModules.catppuccin ];
-
-  config = {
-    catppuccin = {
-      enable = !(isAcceptedDevice osConfig nonAccepted);
-      flavor = "mocha";
-      accent = "pink";
-
-      cursors = {
-        enable = en;
-        accent = "dark";
-      };
-
-      gtk = {
-        enable = true;
-        icon.enable = true;
-      };
-
-      # I want to disable some
-      bat.enable = false;
-      waybar.enable = false;
-      yazi.enable = false;
-    };
-
-    # pointer / cursor theming
-    home.pointerCursor = lib.modules.mkIf en {
-      size = 24;
-      gtk.enable = true;
-      # this adds extra deps, so lets only enable it on wayland
-      x11.enable = !(self.lib.validators.isWayland osConfig);
-    };
+  # pointer / cursor theming
+  config.home.pointerCursor = lib.modules.mkIf en {
+    size = 24;
+    gtk.enable = true;
+    # this adds extra deps, so lets only enable it on wayland
+    x11.enable = !(self.lib.validators.isWayland osConfig);
   };
 }
