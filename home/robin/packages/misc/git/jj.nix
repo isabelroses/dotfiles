8448d94974dd9ec16c0e179e75439b3b1995e3cf
robin <comfysage@isabelroses.com>
2025-02-12 12:20:17 +0000

    feat(robin/jj): add custom log templates

--- home/robin/packages/misc/git/jj.nix
+++ home/robin/packages/misc/git/jj.nix
@@ -21,8 +21,48 @@ in
           default-command = "status";
         };
 
+        templates = {
+          log = "custom_log_compact";
+        };
+
         template-aliases = {
+          # custom components
           "format_short_signature(signature)" = "signature.email().local()";
+          "format_timestamp(timestamp)" = "timestamp.local().format('%Y%m%d %I:%M %P')";
+          "format_custom_commit_header(commit)" = ''
+            separate(" ",
+              format_short_change_id_with_hidden_and_divergent_info(commit),
+              format_short_commit_id(commit.commit_id()),
+              commit.bookmarks(),
+              commit.tags(),
+              commit.working_copies(),
+              if(commit.git_head(), label("git_head", "git_head()")),
+              label("separator", "|"),
+              if(commit.conflict(), label("conflict", "conflict")),
+              format_short_signature(commit.author()),
+              if(config("ui.show-cryptographic-signatures").as_boolean(),
+                format_short_cryptographic_signature(commit.signature())),
+            )
+          '';
+
+          # custom templates
+          "custom_log_compact" = ''
+            if(root,
+              format_root_commit(self),
+              label(if(current_working_copy, "working_copy"),
+                concat(
+                  format_custom_commit_header(self) ++ "\n",
+                  separate(" ",
+                    if(empty, label("empty", "(empty)")),
+                    if(description,
+                      description.first_line(),
+                      label(if(empty, "empty"), description_placeholder),
+                    ),
+                  ) ++ "\n",
+                ),
+              )
+            )
+          '';
         };
       };
     };
