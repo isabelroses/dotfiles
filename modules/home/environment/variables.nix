e86fee0ef3f34bd6c440a03071bd01b11775038d
isabel <isabel@isabelroses.com>
2025-02-14 11:42:47 +0000

    feat(home/enviroment): DO_NOT_TRACK
    https://consoledonottrack.com/

--- modules/home/environment/variables.nix
+++ modules/home/environment/variables.nix
@@ -17,6 +17,7 @@ in
     MANPAGER = defaults.manpager;
     FLAKE = osConfig.garden.environment.flakePath;
     NH_FLAKE = osConfig.garden.environment.flakePath;
+    DO_NOT_TRACK = 1;
   };
 
   programs.nushell.environmentVariables = config.home.sessionVariables;
