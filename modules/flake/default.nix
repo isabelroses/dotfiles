be8411721c309de46dddb7f7146e6a15c7935730
isabel <isabel@isabelroses.com>
2025-02-21 22:40:35 +0000

    chore: nix flake

--- modules/flake/default.nix
+++ modules/flake/default.nix
@@ -10,4 +10,20 @@
 
     ./args.nix # the base args that is passed to the flake
   ];
+
+  config.flake.nixConfig = {
+    extra-experimental-features = [ "pipe-operators" ];
+    extra-substituters = [
+      "https://nix-community.cachix.org" # nix-community cache
+      "https://isabelroses.cachix.org" # precompiled binarys from flake
+      "https://catppuccin.cachix.org" # a cache for all catppuccin ports
+      "https://cosmic.cachix.org" # for the cosmic desktop
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "isabelroses.cachix.org-1:mXdV/CMcPDaiTmkQ7/4+MzChpOe6Cb97njKmBQQmLPM="
+      "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
+      "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
+    ];
+  };
 }
