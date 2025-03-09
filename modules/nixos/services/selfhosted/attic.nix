4862d1b4cb6f700b3aeba9acc8f301ae768bdf0c
isabel <isabel@isabelroses.com>
2025-03-09 16:05:07 +0000

    chore(attic): reduce max size

--- modules/nixos/services/selfhosted/attic.nix
+++ modules/nixos/services/selfhosted/attic.nix
@@ -62,7 +62,7 @@ in
         locations."/" = {
           proxyPass = "http://${cfg.host}:${toString cfg.port}";
           extraConfig = ''
-            client_max_body_size 1000m;
+            client_max_body_size 512m;
           '';
         };
       } // template.ssl rdomain;
