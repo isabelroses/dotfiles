43424db97e60ef650b3517227ae6d7a00bf6e978
isabel <isabel@isabelroses.com>
2025-03-10 22:26:57 +0000

    chore: cleanup overlays

--- modules/base/nix/overlays/default.nix
+++ modules/base/nix/overlays/default.nix
@@ -6,14 +6,15 @@
   nixpkgs.overlays = [
     # this file exists to work around issues with nixpkgs that may arise
     # hopefully that means its empty a lot
-    (final: prev: import ./fixes.nix final prev)
+    # (final: prev: import ./fixes.nix final prev)
+
+    # funni
+    (final: prev: import ./funni.nix final prev)
 
     # we minimize the amount of packages that are installed
     (_: prev: import ./nix.nix { inherit config prev; })
 
     # remove desktop files from apps because i find them annoying
     (final: prev: import ./no-desktop.nix final prev)
-
-    (final: prev: import ./funni.nix final prev)
   ];
 }
