084c0e75c72fec06129727880bad7b990e0bdd3d
isabel <isabel@isabelroses.com>
2025-03-10 14:13:38 +0000

    feat(server): enable diff on activation

--- modules/profiles/server/default.nix
+++ modules/profiles/server/default.nix
@@ -5,5 +5,10 @@ in
 {
   time.timeZone = mkForce "UTC";
 
-  garden.device.type = "server";
+  garden = {
+    device.type = "server";
+
+    # I still want the nvd diff from rebuilding the servers so lets enable that
+    system.activation.diff.enable = true;
+  };
 }
