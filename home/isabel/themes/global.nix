ee78f9e90b3519035437e5ed2f9a27922a1d8617
isabel <isabel@isabelroses.com>
2025-03-12 15:56:05 +0000

    refactor(home/cursors): use .enable pattern

--- home/isabel/themes/global.nix
+++ home/isabel/themes/global.nix
@@ -1,18 +1,16 @@
 {
-  lib,
   self,
   pkgs,
   config,
   osConfig,
   ...
 }:
-let
-  en = pkgs.stdenv.hostPlatform.isLinux && config.garden.programs.gui.enable;
-in
 {
   # pointer / cursor theming
-  home.pointerCursor = lib.modules.mkIf en {
+  home.pointerCursor = {
+    enable = pkgs.stdenv.hostPlatform.isLinux && config.garden.programs.gui.enable;
     size = 24;
+    dotIcons.enable = false;
     gtk.enable = true;
     # this adds extra deps, so lets only enable it on wayland
     x11.enable = !(self.lib.validators.isWayland osConfig);
--- home/robin/themes/global.nix
+++ home/robin/themes/global.nix
@@ -1,20 +1,18 @@
 {
-  lib,
   pkgs,
   self,
   config,
   osConfig,
   ...
 }:
-let
-  en = pkgs.stdenv.hostPlatform.isLinux && config.garden.programs.gui.enable;
-in
 {
   # pointer / cursor theming
-  config.home.pointerCursor = lib.modules.mkIf en {
+  config.home.pointerCursor = {
+    enable = pkgs.stdenv.hostPlatform.isLinux && config.garden.programs.gui.enable;
     name = "Capitaine Cursors";
     package = pkgs.capitaine-cursors-themed;
     size = 24;
+    dotIcons.enable = false;
     gtk.enable = true;
     # this adds extra deps, so lets only enable it on wayland
     x11.enable = !(self.lib.validators.isWayland osConfig);
