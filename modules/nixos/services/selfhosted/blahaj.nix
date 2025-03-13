6640cf2f855aaada834f8f8d719b96cb15f875a9
isabel <isabel@isabelroses.com>
2025-03-13 17:07:32 +0000

    feat use tgirlpkgs module

--- modules/nixos/services/selfhosted/blahaj.nix
+++ modules/nixos/services/selfhosted/blahaj.nix
@@ -2,12 +2,9 @@
   lib,
   self,
   config,
-  inputs',
   ...
 }:
 let
-  inherit (self.lib) template;
-  inherit (lib.meta) getExe;
   inherit (lib.modules) mkIf;
   inherit (self.lib.services) mkServiceOption;
   inherit (self.lib.secrets) mkSecret;
@@ -18,31 +15,9 @@ in
   config = mkIf config.garden.services.blahaj.enable {
     age.secrets.blahaj-env = mkSecret { file = "blahaj-env"; };
 
-    users = {
-      groups.blahaj = { };
-
-      users.blahaj = {
-        isSystemUser = true;
-        createHome = false;
-        group = "blahaj";
-      };
-    };
-
-    systemd.services = {
-      blahaj = {
-        description = "blahaj";
-        after = [ "network.target" ];
-        wantedBy = [ "multi-user.target" ];
-
-        serviceConfig = {
-          Type = "simple";
-          User = "blahaj";
-          Group = "blahaj";
-          EnvironmentFile = config.age.secrets.blahaj-env.path;
-          ExecStart = getExe inputs'.tgirlpkgs.packages.blahaj;
-          Restart = "always";
-        } // template.systemd;
-      };
+    services.blahaj = {
+      enable = true;
+      environmentFile = config.age.secrets.blahaj-env.path;
     };
   };
 }
