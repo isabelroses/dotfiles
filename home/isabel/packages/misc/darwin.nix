be9a212c3b884e4e4137b9d7cf5ea1e11b67b710
isabel <isabel@isabelroses.com>
2025-03-16 15:07:34 +0000

    refactor: use uutils

--- home/isabel/packages/misc/darwin.nix
+++ home/isabel/packages/misc/darwin.nix
@@ -4,7 +4,6 @@
     inherit (pkgs)
       libwebp # WebP image format library
       m-cli # A macOS cli tool to manage macOS - a true swis army knife
-      uutils-coreutils-noprefix # GNU core utilities (rewritten in rust) - the ones provided by default are bad lol
       ;
   };
 }
--- modules/base/programs.nix
+++ modules/base/programs.nix
@@ -1,8 +1,14 @@
-{ config, ... }:
+{ pkgs, config, ... }:
 {
   # home-manager is so strange and needs these declared multiple times
   programs = {
     fish.enable = config.garden.meta.fish;
     zsh.enable = config.garden.meta.zsh;
+  };
+
+  garden.packages = {
+    # GNU core utilities (rewritten in rust)
+    # a good idea for usage on macOS too
+    inherit (pkgs) uutils-coreutils-noprefix;
   };
 }
