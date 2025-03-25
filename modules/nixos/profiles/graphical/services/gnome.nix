871016069869bef9a799243c5a439dc50b998a30
isabel <isabel@isabelroses.com>
2025-03-25 13:58:38 +0000

    feat(gnome): add documentation on gcr

--- modules/nixos/profiles/graphical/services/gnome.nix
+++ modules/nixos/profiles/graphical/services/gnome.nix
@@ -16,6 +16,9 @@ in
 
       gnome = {
         glib-networking.enable = true;
+
+        # TODO: remove this in favour of the new gcr implementation
+        # https://github.com/NixOS/nixpkgs/pull/379731
         gnome-keyring.enable = true; # this makes it so i don't have to enter my password every time i log in
 
         # I don't need remote desktop
