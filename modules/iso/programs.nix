a169b60d33a984b6bd925de31093ae50e825622d
isabel <isabel@isabelroses.com>
2025-04-03 11:08:05 +0100

    refactor(iso): set git.package

--- modules/iso/programs.nix
+++ modules/iso/programs.nix
@@ -12,12 +12,14 @@
     };
   };
 
+  # we only need a basic git install
+  programs.git.package = pkgs.gitMinimal;
+
   # needed packages for the installer
   environment.systemPackages = builtins.attrValues {
     inherit (pkgs)
       vim # we are not installing neovim here so we have a light dev environment
       pciutils # going to need this for lspci
-      gitMinimal # we only need a basic git install
       ;
 
     inherit (self'.packages) iztaller;
