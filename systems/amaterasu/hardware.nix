8b2e364a7fdc897a8fe71911db6a75112f37a8a8
isabel <isabel@isabelroses.com>
2025-04-03 13:26:23 +0100

    chore: update hardware

--- systems/amaterasu/hardware.nix
+++ systems/amaterasu/hardware.nix
@@ -1,24 +1,17 @@
 {
   fileSystems = {
     "/" = {
-      device = "/dev/disk/by-uuid/f26dd3a6-7750-4e1e-a465-97fa9e9b9cc6";
-      fsType = "ext4";
+      device = "/dev/disk/by-label/nixos";
+      fsType = "btrfs";
     };
 
     "/boot" = {
-      device = "/dev/disk/by-uuid/9AD9-AD9F";
+      device = "/dev/disk/by-label/boot";
       fsType = "vfat";
-    };
-
-    "/mnt/windows" = {
-      device = "/dev/disk/by-uuid/01D925D0893A60B0";
-      fsType = "ntfs-3g";
-      options = [
-        "rw"
-        "uid=1000"
-      ];
     };
   };
 
-  swapDevices = [ { device = "/dev/disk/by-uuid/da541a24-b4cc-426f-bbb6-4cced93fa4cf"; } ];
+  swapDevices = [
+    { device = "/dev/disk/by-label/swap"; }
+  ];
 }
