c2ad72abb2d9c277dd847e5772997f1ee9fb4ed3
isabel <isabel@isabelroses.com>
2025-05-06 11:13:34 +0100

    fix: tcpcrypt enable on none servers only

--- modules/nixos/networking/tcpcrypt.nix
+++ modules/nixos/networking/tcpcrypt.nix
@@ -4,13 +4,13 @@
   ...
 }:
 let
-  inherit (lib.modules) mkIf;
+  inherit (lib) mkIf;
 in
 {
   # enable opportunistic TCP encryption
   # this is NOT a pancea, however, if the receiver supports encryption and the attacker is passive
   # privacy will be more plausible (but not guaranteed, unlike what the option docs suggest)
-  networking.tcpcrypt.enable = config.garden.profiles.server.enable;
+  networking.tcpcrypt.enable = !config.garden.profiles.server.enable;
 
   users = mkIf config.networking.tcpcrypt.enable {
     groups.tcpcryptd = { };
