5734953f0943fc33644479574227a8bf3b872c92
isabel <isabel@isabelroses.com>
2025-05-11 23:12:17 +0100

    fix: zsh noshell

--- modules/home/programs/shells.nix
+++ modules/home/programs/shells.nix
@@ -10,6 +10,8 @@ let
   inherit (self.lib) mkProgram;
 
   progs = config.garden.programs;
+  cfg = progs.${progs.defaults.shell};
+  pkg = lib.getExe cfg.package;
 in
 {
   options.garden.programs = {
@@ -27,9 +29,34 @@ in
     nushell = mkProgram pkgs "nushell" { };
   };
 
+  # i hate special casing, i mean come on, i choose you noshell so I could
+  # avoid special casing and yet here we are again special casing
   config = {
-    xdg.configFile.shell = {
-      source = lib.getExe progs.${progs.defaults.shell}.package;
-    };
+    xdg.configFile.shell =
+      if progs.defaults.shell != "zsh" then
+        {
+          source = pkg;
+        }
+      else
+        {
+          executable = true;
+          text = ''
+            #!${pkg}
+
+            if [[ -o interactive ]] ; then
+              if [[ -o login ]] ; then
+                exec ${pkg} --interactive --login "$@"
+              fi
+
+              exec ${pkg} --interactive "$@"
+            fi
+
+            if [[ -o login ]] ; then
+              exec ${pkg} --login "$@"
+            fi
+
+            exec ${pkg} "$@"
+          '';
+        };
   };
 }
