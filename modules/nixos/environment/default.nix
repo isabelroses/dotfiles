c8e8c00b3c99838db133c7b9e5e9a2cf7f244e5b
isabel <isabel@isabelroses.com>
2025-05-23 12:06:12 +0100

    fonts: add back antialiasing++

--- modules/nixos/environment/default.nix
+++ modules/nixos/environment/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ./console.nix # changes to the console
     ./documentation.nix # nixos' provided documentation
+    ./fonts.nix # fonts
     ./locale.nix # locale settings
     ./paths.nix # paths
     ./wayland.nix # wayland settings
---
+++ modules/nixos/environment/fonts.nix
@@ -0,0 +1,12 @@
+{
+  fonts = {
+    fontconfig = {
+      enable = true;
+      hinting.enable = true;
+      antialias = true;
+    };
+
+    # this can allow us to save some storage space
+    fontDir.decompressFonts = true;
+  };
+}
