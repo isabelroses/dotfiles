c898facdce98bbb087a65959c700d2843a9dbce0
isabel <isabel@isabelroses.com>
2025-06-10 11:39:39 +0100

    logind: disable suspend

--- modules/nixos/system/logind.nix
+++ modules/nixos/system/logind.nix
@@ -1,14 +1,23 @@
 { lib, config, ... }:
 let
   inherit (lib) mkIf;
+  prof = config.garden.profiles;
 in
 {
-  config = mkIf config.garden.profiles.laptop.enable {
+  config = mkIf prof.laptop.enable {
     services.logind = {
       lidSwitch = "ignore";
       lidSwitchDocked = "ignore";
       lidSwitchExternalPower = "ignore";
       powerKey = "suspend-then-hibernate";
     };
+
+    # https://wiki.debian.org/Suspend#Disable_suspend_and_hibernation
+    systemd.sleep.extraConfig = mkIf prof.headless.enable ''
+      AllowSuspend=no
+      AllowHibernation=no
+      AllowSuspendThenHibernate=no
+      AllowHybridSleep=no
+    '';
   };
 }
