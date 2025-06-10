7602246a11d71abf4d3f2976d05cd0d64af0f097
isabel <isabel@isabelroses.com>
2025-06-10 11:10:41 +0100

    subsitors: cleanup

--- modules/base/nix/substituters.nix
+++ modules/base/nix/substituters.nix
@@ -1,19 +1,29 @@
+{ lib, config, ... }:
+let
+  inherit (lib) optionals;
+in
 {
   tgirlpkgs.cache.enable = true;
 
   # substituters to use
   nix.settings = {
     # don't worry that you cannot see cache.nixos.org here, it is added by default
-    substituters = [
-      "https://nix-community.cachix.org" # nix-community cache
-      "https://catppuccin.cachix.org" # a cache for all catppuccin ports
-      "https://everviolet.cachix.org" # a cache for all everviolet ports
-    ];
+    substituters =
+      [
+        "https://nix-community.cachix.org" # nix-community cache
+        "https://everviolet.cachix.org" # a cache for all everviolet ports
+      ]
+      ++ optionals config.catppuccin.enable [
+        "https://catppuccin.cachix.org" # a cache for all catppuccin ports
+      ];
 
-    trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
-      "everviolet.cachix.org-1:3nHvJgzKRRRCQZURheH1INddNlyU4OWqfn068t8AuvU="
-    ];
+    trusted-public-keys =
+      [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "everviolet.cachix.org-1:3nHvJgzKRRRCQZURheH1INddNlyU4OWqfn068t8AuvU="
+      ]
+      ++ optionals config.catppuccin.enable [
+        "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
+      ];
   };
 }
