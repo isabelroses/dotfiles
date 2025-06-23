1d7e4064ebc6c7050e7915bf3aa7aaf11972f45a
robin <comfysage@isabelroses.com>
2025-06-23 17:37:25 +0200

    feat(robin/zsh): enable `autopushd`

--- home/robin/cli/shell/zsh.nix
+++ home/robin/cli/shell/zsh.nix
@@ -15,11 +15,13 @@ in
       path = config.xdg.stateHome + "/zsh/history";
     };
 
-    initContent = mkOrder 1000 ''
+    initContent = mkOrder 1000 (''
       function title_precmd_hook() {
         print -Pn "\e]0;$(pwd)\a"
       }
       precmd_functions+=title_precmd_hook
-    '';
+    '' + ''
+      setopt autopushd
+    '');
   };
 }
