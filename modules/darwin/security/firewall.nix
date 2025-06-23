a9ce2d997a526bbdcd1b8ce320dc3efdf5313d6f
isabel <isabel@isabelroses.com>
2025-06-23 16:11:39 +0100

    darwin/security/firewall: use updated options

--- modules/darwin/security/firewall.nix
+++ modules/darwin/security/firewall.nix
@@ -1,12 +1,13 @@
 {
-  system.defaults.alf = {
-    allowdownloadsignedenabled = 0;
-    allowsignedenabled = 1;
-    globalstate = 0;
-    loggingenabled = 0;
+  networking.applicationFirewall = {
+    enable = true;
+    blockAllIncoming = false;
+
+    allowSignedApp = false;
+    allowSigned = true;
 
     # Apple menu > System Preferences > Security and firewall
     # Drops incoming requests via ICMP such as ping requests. Default is 0.
-    stealthenabled = 1;
+    enableStealthMode = true;
   };
 }
