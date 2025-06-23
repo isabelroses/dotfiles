001e3d82a03befd03865c5cc3fcaa6411d48cee1
robin <comfysage@isabelroses.com>
2025-06-23 17:37:25 +0200

    fix(robin/obsidian): remove git from wrapper

--- home/robin/gui/obsidian.nix
+++ home/robin/gui/obsidian.nix
@@ -15,10 +15,6 @@ in
         inherit (pkgs)
           # for the pandoc plugin
           pandoc
-
-          # for the obsidian-git plugin
-          git
-          git-lfs
           ;
       };
       meta.mainProgram = pkgs.obsidian.meta.mainProgram;
