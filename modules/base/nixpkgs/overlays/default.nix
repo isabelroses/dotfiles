c540b2cfa603075585ed583f28c2be9f37e69bdd
isabel <isabel@isabelroses.com>
2025-06-24 09:13:26 +0100

    nixpkgs/overlays: remove nixos-rebuild overlay

--- modules/base/nixpkgs/overlays/default.nix
+++ modules/base/nixpkgs/overlays/default.nix
@@ -1,6 +1,6 @@
 { lib, config, ... }:
 let
-  inherit (lib.lists) map;
+  inherit (lib) map;
 
   overlays = map import [
     ./fixes.nix
@@ -10,7 +10,7 @@ let
 in
 {
   nixpkgs.overlays = overlays ++ [
-    (final: prev: {
+    (_: prev: {
       # in order to reduce our closure size, we can override these packages to
       # use the nix package that we have installed, this will trigger a rebuild
       # of the packages that depend on them so hopefully its worth it for that
@@ -22,14 +22,6 @@ in
       # make sure to restore nix for linking back to nix from nixpkgs as its
       # used for other things then the cli implementation
       nixForLinking = prev.nixVersions.stable;
-
-      # match our nixos-rebuild for all systems that install it
-      nixos-rebuild = final.nixos-rebuild-ng;
-      nixos-rebuild-ng = prev.nixos-rebuild-ng.override {
-        nix = config.nix.package;
-        withNgSuffix = false;
-        withReexec = true;
-      };
     })
   ];
 }
