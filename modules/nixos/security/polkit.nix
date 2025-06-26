360b754d2079b81be7fb1ab9e7ae5efb6abfdaed
isabel <isabel@isabelroses.com>
2025-06-26 16:56:48 +0100

    nixos/security/polkit: use soteria

--- modules/nixos/security/polkit.nix
+++ modules/nixos/security/polkit.nix
@@ -1,45 +1,11 @@
-{
-  lib,
-  pkgs,
-  config,
-  ...
-}:
-let
-  inherit (lib) mkIf mkDefault;
-
-  systemdCond =
-    config.garden.profiles.graphical.enable && !config.services.desktopManager.cosmic.enable;
-in
+{ config, ... }:
 {
   # have polkit log all actions
-  security.polkit = {
-    enable = true;
-    debug = mkDefault true;
+  security = {
+    polkit.enable = true;
 
-    # the below configuration depends on security.polkit.debug being set to true
-    # so we have it written only if debugging is enabled
-    extraConfig = mkIf config.security.polkit.debug ''
-      /* Log authorization checks. */
-      polkit.addRule(function(action, subject) {
-        polkit.log("user " +  subject.user + " is attempting action " + action.id + " from PID " + subject.pid);
-      });
-    '';
-  };
-
-  systemd = mkIf systemdCond {
-    user.services.polkit-pantheon-authentication-agent-1 = {
-      description = "Pantheon PolicyKit agent";
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = "${pkgs.pantheon.pantheon-agent-polkit}/libexec/policykit-1-pantheon/io.elementary.desktop.agent-polkit";
-        Restart = "on-failure";
-        RestartSec = 1;
-        TimeoutStopSec = 10;
-      };
-
-      wantedBy = [ "graphical-session.target" ];
-      wants = [ "graphical-session.target" ];
-      after = [ "graphical-session.target" ];
-    };
+    # this should only be installed on graphical systems
+    soteria.enable =
+      config.garden.profiles.graphical.enable && !config.services.desktopManager.cosmic.enable;
   };
 }
