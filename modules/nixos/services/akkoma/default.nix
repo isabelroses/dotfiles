efbc0d0804403d49ccfb5022123e24d6ea55004b
isabel <isabel@isabelroses.com>
2025-06-30 16:47:54 +0100

    nixos/services/akkoma: update hash

--- modules/nixos/services/akkoma/default.nix
+++ modules/nixos/services/akkoma/default.nix
@@ -28,7 +28,7 @@ in
 
         "favicon.png" = pkgs.fetchurl {
           url = "https://gravatar.com/avatar/c487c810e09878b4bd90df713a7c9523?size=512";
-          hash = "sha256-8V9UIcjDyO2jYY6WS5/gyPNfDUDWXNHFzLU4ix9EyqQ=";
+          hash = "sha256-2lCVoJhDKCdfxaPhtgtJ8hruVXSjxLMsSkz8n5kw9cE=";
         };
 
         "emoji/blobs" = pkgs.blobs_gg;
