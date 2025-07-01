66adaacc73a4b1374a4673c28d7f318148b0155f
isabel <isabel@isabelroses.com>
2025-07-01 09:47:40 +0100

    isabel/quickshell: fmt

--- home/isabel/gui/quickshell.nix
+++ home/isabel/gui/quickshell.nix
@@ -1,6 +1,6 @@
 { lib, pkgs, ... }:
 {
-  garden.packages = lib.mkIf (pkgs.stdenv.hostPlatform.isLinux) {
+  garden.packages = lib.mkIf pkgs.stdenv.hostPlatform.isLinux {
     quickshell = pkgs.quickshell.overrideAttrs (oa: {
       buildInputs = oa.buildInputs or [ ] ++ [
         pkgs.kdePackages.qtimageformats
