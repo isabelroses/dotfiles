6ab4716e49f8a0609b87b5136e4693be143cbab6
isabel <isabel@isabelroses.com>
2025-07-04 17:16:02 +0100

    systems/amaterasu: use more accurate host paths

--- systems/amaterasu/hardware.nix
+++ systems/amaterasu/hardware.nix
@@ -1,17 +1,21 @@
 {
   fileSystems = {
     "/" = {
-      device = "/dev/disk/by-label/root";
+      device = "/dev/disk/by-uuid/3a898325-08a7-4f9f-ab19-380765bcaf92";
       fsType = "btrfs";
     };
 
     "/boot" = {
-      device = "/dev/disk/by-label/boot";
+      device = "/dev/disk/by-uuid/36A9-4288";
       fsType = "vfat";
+      options = [
+        "fmask=0022"
+        "dmask=0022"
+      ];
     };
   };
 
   swapDevices = [
-    { device = "/dev/disk/by-label/swap"; }
+    { device = "/dev/disk/by-uuid/55606623-362b-4c53-a9da-26ea202aff23"; }
   ];
 }
