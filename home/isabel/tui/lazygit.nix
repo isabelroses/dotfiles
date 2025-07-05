57320dd7f0e06af103a9180fb4767fe44fa4f48b
isabel <isabel@isabelroses.com>
2025-07-05 10:53:31 +0100

    isabel/lazygit: use new IFD less aproch

--- home/isabel/tui/lazygit.nix
+++ home/isabel/tui/lazygit.nix
@@ -1,43 +1,21 @@
+{ config, ... }:
 {
-  lib,
-  config,
-  ...
-}:
-let
-  inherit (lib) mkIf;
+  programs.lazygit = {
+    enable = config.garden.profiles.workstation.enable && config.programs.git.enable;
 
-  ctp = config.catppuccin;
+    settings = {
+      update.method = "never";
 
-  configFile = "${config.xdg.configHome}/lazygit/config.yml";
+      gui = {
+        nerdFontsVersion = 3;
+        authorColors.isabel = "#f5c2e7";
+      };
 
-  cond = config.garden.profiles.workstation.enable && config.programs.git.enable;
-in
-{
-  config = mkIf cond {
-    catppuccin.lazygit.enable = false;
-
-    home.sessionVariables = {
-      # Ensure that the default config file is still sourced
-      LG_CONFIG_FILE = "${ctp.sources.lazygit}/${ctp.flavor}/${ctp.accent}.yml,${configFile}";
-    };
-
-    programs.lazygit = {
-      enable = true;
-
-      settings = {
-        update.method = "never";
-
-        gui = {
-          nerdFontsVersion = 3;
-          authorColors.isabel = "#f5c2e7";
-        };
-
-        git = {
-          overrideGpg = true;
-          paging = {
-            colorArg = "always";
-            pager = "delta --paging=never";
-          };
+      git = {
+        overrideGpg = true;
+        paging = {
+          colorArg = "always";
+          pager = "delta --paging=never";
         };
       };
     };
