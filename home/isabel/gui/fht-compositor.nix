219882243efb17f73494ca9983f7470a066e0f12
isabel <isabel@isabelroses.com>
2025-07-06 17:32:39 +0100

    nixos/fht-comp: fix some issues

--- home/isabel/gui/fht-compositor.nix
+++ home/isabel/gui/fht-compositor.nix
@@ -24,6 +24,9 @@ in
         autostart = [
           "wl-paste --type text --watch cliphist store" # Stores only text data
           "wl-paste --type image --watch cliphist store" # Stores only image data
+
+          # seems about right
+          "${pkgs.dbus}/bin/dbus-update-activation-environment --systemd --all && systemctl --user stop fht-compositor.target && systemctl --user start fht-compositor.target"
         ];
 
         input.keyboard = {
@@ -210,21 +213,31 @@ in
       };
     };
 
-    systemd.user.services.xwayland-satellite = {
-      Unit = {
-        Description = "Xwayland satellite service";
-        After = [ "graphical-session.target" ];
-        PartOf = [ "graphical-session.target" ];
+    systemd.user = {
+      targets.fht-compositor.Unit = {
+        Description = "fht-compositor compositor session";
+        Documentation = [ "man:systemd.special(7)" ];
         BindsTo = [ "graphical-session.target" ];
-        Requisite = [ "graphical-session.target" ];
+        Wants = [ "graphical-session-pre.target" ];
+        After = [ "graphical-session-pre.target" ];
       };
-      Service = {
-        Type = "notify";
-        NotifyAccess = "all";
-        ExecStart = "${pkgs.xwayland-satellite}/bin/xwayland-satellite";
-        StandardOutput = "journal";
+
+      services.xwayland-satellite = {
+        Unit = {
+          Description = "Xwayland satellite service";
+          After = [ config.wayland.systemd.target ];
+          PartOf = [ config.wayland.systemd.target ];
+          BindsTo = [ config.wayland.systemd.target ];
+          Requisite = [ config.wayland.systemd.target ];
+        };
+        Service = {
+          Type = "notify";
+          NotifyAccess = "all";
+          ExecStart = "${pkgs.xwayland-satellite}/bin/xwayland-satellite";
+          StandardOutput = "journal";
+        };
+        Install.WantedBy = [ config.wayland.systemd.target ];
       };
-      Install.WantedBy = [ "graphical-session.target" ];
     };
   };
 }
--- modules/nixos/programs/fht-comp.nix
+++ modules/nixos/programs/fht-comp.nix
@@ -4,26 +4,42 @@
   self,
   config,
   inputs',
+  modulesPath,
   ...
 }:
 let
-  inherit (lib) mkIf;
+  inherit (lib) mkIf mkMerge;
   inherit (self.lib) anyHome;
 
   cond = anyHome config (conf: conf.programs.fht-compositor.enable or false);
 in
 {
-  config = mkIf cond {
-    services.displayManager.sessionPackages = [ inputs'.fht-compositor.packages.default ];
+  config = mkIf cond (mkMerge [
+    {
+      xdg.portal = {
+        wlr.enable = false;
+        configPackages = [ inputs'.fht-compositor.packages.default ];
 
-    xdg.portal = {
-      configPackages = [ inputs'.fht-compositor.packages.default ];
-      wlr.enable = false;
-    };
+        config.common = {
+          "org.freedesktop.impl.portal.Access" = "gtk";
+          "org.freedesktop.impl.portal.Notification" = "gtk";
+          "org.freedesktop.impl.portal.ScreenCast" = "fht-compositor";
+        };
+      };
 
-    garden.packages = {
-      fht-share-picker = inputs'.fht-share-picker.packages.default;
-      inherit (pkgs) xdg-utils;
-    };
-  };
+      services.displayManager.sessionPackages = [ inputs'.fht-compositor.packages.default ];
+
+      garden.packages = {
+        fht-compositor = inputs'.fht-compositor.packages.default;
+        fht-share-picker = inputs'.fht-share-picker.packages.default;
+        inherit (pkgs) xdg-utils;
+      };
+    }
+
+    (import "${modulesPath}/programs/wayland/wayland-session.nix" {
+      inherit lib pkgs;
+      enableXWayland = false; # we dont have xwayland support
+      enableWlrPortal = false; # fht-compositor ships its own portal.
+    })
+  ]);
 }
