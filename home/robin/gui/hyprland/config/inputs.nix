838b4472be7e9b0c8545ace7f879e0d3fe35da66
robin <comfysage@isabelroses.com>
2025-07-08 17:44:35 +0200

    feat(robin/inputs): set touchpad sensitivity to constant `1`

--- home/robin/gui/hyprland/config/inputs.nix
+++ home/robin/gui/hyprland/config/inputs.nix
@@ -7,7 +7,7 @@ in
     input = {
       kb_layout = dev.keyboard;
       follow_mouse = 0;
-      sensitivity = if dev.keyboard == "us" then 0.2 else 0; # -1.0 - 1.0, 0 means no modification.
+      sensitivity = 1;
       force_no_accel = 1;
       numlock_by_default = true; # numlock enable
 
