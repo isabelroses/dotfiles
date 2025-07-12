e5d3a4ba47f253a374714d0abad33744f639b5c1
isabel <isabel@isabelroses.com>
2025-07-12 09:50:19 +0100

    base/nixpkgs/overlays: overlay nil

--- modules/base/nixpkgs/overlays/default.nix
+++ modules/base/nixpkgs/overlays/default.nix
@@ -24,6 +24,22 @@ in
       # make sure to restore nix for linking back to nix from nixpkgs as its
       # used for other things then the cli implementation
       nixForLinking = prev.nixVersions.stable;
+
+      nil = (prev.nil.override { nix = config.nix.package; }).overrideAttrs (
+        finalAttrs: oa: {
+          src = prev.fetchFromGitHub {
+            owner = "oxalica";
+            repo = "nil";
+            rev = "b043bfe1f3f4c4be4b688e24c5ae96e81f525805";
+            hash = "sha256-zXag1+8iZC3H5yVFP7KhIi4ps9z8xKrFIkyaeXlZ7Uo=";
+          };
+
+          cargoDeps = prev.rustPlatform.fetchCargoVendor {
+            inherit (finalAttrs) src;
+            hash = "sha256-Sljr3ff8hl/qm/0wqc1GXsEr1wWn7NAXmdrd5wHzUX8=";
+          };
+        }
+      );
     })
   ];
 }
