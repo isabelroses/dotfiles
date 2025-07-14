3b970d16da55253bf73033e7577398bf9dc08d69
isabel <isabel@isabelroses.com>
2025-07-14 11:47:08 +0100

    nixos/system/command-not-found: remove pay-respects

--- modules/nixos/system/command-not-found.nix
+++ modules/nixos/system/command-not-found.nix
@@ -1,11 +1,5 @@
-{ config, ... }:
 {
-  programs = {
-    # disable command-not-found, it doesn't help, and it adds perl
-    # which we don't need, and we know when we don't have a command anyway
-    command-not-found.enable = false;
-
-    # like `thefuck`, but in rust and actually maintained
-    pay-respects.enable = config.garden.profiles.graphical.enable;
-  };
+  # disable command-not-found, it doesn't help, and it adds perl
+  # which we don't need, and we know when we don't have a command anyway
+  programs.command-not-found.enable = false;
 }
