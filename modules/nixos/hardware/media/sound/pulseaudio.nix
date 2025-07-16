33c0eac562f757d0c0cb5c58869075c88e25d298
isabel <isabel@isabelroses.com>
2025-07-16 10:37:08 +0100

    nixos/hardware/sound: fix pulseaudio enable cond

--- modules/nixos/hardware/media/sound/pulseaudio.nix
+++ modules/nixos/hardware/media/sound/pulseaudio.nix
@@ -1,5 +1,6 @@
 { config, ... }:
 {
   # pulseaudio backup
-  services.pulseaudio.enable = !config.services.pipewire.enable;
+  services.pulseaudio.enable =
+    !config.services.pipewire.enable && config.garden.profiles.graphical.enable;
 }
