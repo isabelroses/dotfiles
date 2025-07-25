c5ac70ca0e1c2683d037ce9e6b10c772c6f01680
isabel <isabel@isabelroses.com>
2025-07-25 17:54:06 +0100

    base/users/robin: fix eval

--- modules/base/users/robin.nix
+++ modules/base/users/robin.nix
@@ -1,5 +1,11 @@
+{ lib, config, ... }:
+let
+  inherit (lib) elem mkIf;
+in
 {
-  users.users.robin = {
-    hashedPassword = "$y$j9T$fjikoYmPQxjKqjobJ/vZC/$ULBf6ns9PS8EUHBeRbnd3QQbOSCtAe18JhK3UUy.nv2";
+  config = mkIf (elem "robin" config.garden.system.users) {
+    users.users.robin = {
+      hashedPassword = "$y$j9T$fjikoYmPQxjKqjobJ/vZC/$ULBf6ns9PS8EUHBeRbnd3QQbOSCtAe18JhK3UUy.nv2";
+    };
   };
 }
