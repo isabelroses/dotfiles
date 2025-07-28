8feabdbf37c68706423b8ba0c5cd5565089522bf
isabel <isabel@isabelroses.com>
2025-07-28 21:32:53 +0100

    nixos/systemd: conform to rfc 42

--- modules/nixos/headless.nix
+++ modules/nixos/headless.nix
@@ -36,17 +36,17 @@ in
 
       # For more detail, see:
       #   https://0pointer.de/blog/projects/watchdog.html
-      watchdog = {
+      settings.Manager = {
         # systemd will send a signal to the hardware watchdog at half
         # the interval defined here, so every 10s.
         # If the hardware watchdog does not get a signal for 20s,
         # it will forcefully reboot the system.
-        runtimeTime = "20s";
+        RuntimeWatchdogSec = "20s";
         # Forcefully reboot if the final stage of the reboot
         # hangs without progress for more than 30s.
         # For more info, see:
         #   https://utcc.utoronto.ca/~cks/space/blog/linux/SystemdShutdownWatchdog
-        rebootTime = "30s";
+        RebootWatchdogSec = "30s";
       };
 
       sleep.extraConfig = ''
