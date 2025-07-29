1f2860082bd2a1e5da0166950c91cac99cfaa836
isabel <isabel@isabelroses.com>
2025-07-29 09:38:42 +0100

    robin/gui/spotify: remove confliciting theme

--- home/robin/gui/spotify.nix
+++ home/robin/gui/spotify.nix
@@ -1,6 +1,5 @@
 {
   lib,
-  pkgs,
   inputs,
   config,
   ...
@@ -19,20 +18,5 @@ in
     enable = true;
 
     colorScheme = "fall";
-
-    theme = {
-      name = "evergarden";
-      src = pkgs.fetchFromGitHub {
-        owner = "everviolet";
-        repo = "spicetify";
-        rev = "d37d8ce00ba544f95d42e92eb8c4c23048d39720";
-        hash = "sha256-yt+swt9HjR9maO6atz/It7dzxStEJpykBgwgqeBpn/8=";
-        postFetch = ''
-          cd $out
-          find evergarden -mindepth 1 -maxdepth 1 -exec mv -i -- {} . \;
-          find . -depth -maxdepth 1 -type d -empty -exec rmdir {} \;
-        '';
-      };
-    };
   };
 }
