7f3288e2a9adb06b04f4784440f1d5b068b6f7bd
isabel <isabel@isabelroses.com>
2025-07-31 14:09:59 +0100

    nixos/security/kernel: sort + small docs

--- modules/nixos/security/kernel.nix
+++ modules/nixos/security/kernel.nix
@@ -164,52 +164,53 @@ in
       blacklistedKernelModules = concatLists [
         # Obscure network protocols
         [
-          "dccp" # Datagram Congestion Control Protocol
-          "sctp" # Stream Control Transmission Protocol
-          "rds" # Reliable Datagram Sockets
-          "tipc" # Transparent Inter-Process Communication
-          "n-hdlc" # High-level Data Link Control
-          "netrom" # NetRom
-          "x25" # X.25
+          # keep-sorted start
+          "af_802154" # IEEE 802.15.4
+          "appletalk" # Appletalk
+          "atm" # ATM
           "ax25" # Amateur X.25
-          "rose" # ROSE
+          "can" # Controller Area Network
+          "dccp" # Datagram Congestion Control Protocol
           "decnet" # DECnet
           "econet" # Econet
-          "af_802154" # IEEE 802.15.4
           "ipx" # Internetwork Packet Exchange
-          "appletalk" # Appletalk
-          "psnap" # SubnetworkAccess Protocol
+          "n-hdlc" # High-level Data Link Control
+          "netrom" # NetRom
           "p8022" # IEEE 802.3
           "p8023" # Novell raw IEEE 802.3
-          "can" # Controller Area Network
-          "atm" # ATM
+          "psnap" # SubnetworkAccess Protocol
+          "rds" # Reliable Datagram Sockets
+          "rose" # ROSE
+          "sctp" # Stream Control Transmission Protocol
+          "tipc" # Transparent Inter-Process Communication
+          "x25" # X.25
+          # keep-sorted end
         ]
 
         # Old or rare or insufficiently audited filesystems
         [
+          # keep-sorted start
           "adfs" # Active Directory Federation Services
           "affs" # Amiga Fast File System
           "befs" # "Be File System"
           "bfs" # BFS, used by SCO UnixWare OS for the /stand slice
-          "cramfs" # compressed ROM/RAM file system
           "cifs" # smb (Common Internet File System)
+          "cramfs" # compressed ROM/RAM file system
           "efs" # Extent File System
-          # "erofs" # Enhanced Read-Only File System - required for etc overlay
           "exofs" # EXtended Object File System
-          "freevxfs" # Veritas filesystem driver
           "f2fs" # Flash-Friendly File System
-          "vivid" # Virtual Video Test Driver (unnecessary)
+          "freevxfs" # Veritas filesystem driver
           "gfs2" # Global File System 2
-          "hpfs" # High Performance File System (used by OS/2)
           "hfs" # Hierarchical File System (Macintosh)
           "hfsplus" # " same as above, but with extended attributes
+          "hpfs" # High Performance File System (used by OS/2)
           "jffs2" # Journalling Flash File System (v2)
           "jfs" # Journaled File System - only useful for VMWare sessions
           "ksmbd" # SMB3 Kernel Server
           "minix" # minix fs - used by the minix OS
+          "nfs" # Network File System
           "nfsv3" # " (v3)
           "nfsv4" # Network File System (v4)
-          "nfs" # Network File System
           "nilfs2" # New Implementation of a Log-structured File System
           "omfs" # Optimized MPEG Filesystem
           "qnx4" # extent-based file system used by the QNX4 and QNX6 OSes
@@ -217,18 +218,28 @@ in
           "squashfs" # compressed read-only file system (used by live CDs)
           "sysv" # implements all of Xenix FS, SystemV/386 FS and Coherent FS.
           "udf" # https://docs.kernel.org/5.15/filesystems/udf.html
+          "vivid" # Virtual Video Test Driver (unnecessary)
+          # keep-sorted end
+
+          # in the past this was blacklisted, but i had to remove it to be able
+          # to use `system.etc.overlay.enable`
+          # "erofs" # Enhanced Read-Only File System
         ]
 
         # Disable pc speakers, does anyone actually use these
         [
+          # keep-sorted start
           "pcspkr"
           "snd_pcsp"
+          # keep-sorted end
         ]
 
         # Disable Thunderbolt and FireWire to prevent DMA attacks
         [
-          "thunderbolt"
+          # keep-sorted start
           "firewire-core"
+          "thunderbolt"
+          # keep-sorted end
         ]
 
         # this is why your webcam no worky
