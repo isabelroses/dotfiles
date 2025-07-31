4d9d6e995fa8267284a7d6a8199a4c91cf561740
isabel <isabel@isabelroses.com>
2025-07-31 14:36:50 +0100

    nixos/system/oomd: update docs, simplify

--- modules/nixos/system/oomd.nix
+++ modules/nixos/system/oomd.nix
@@ -5,16 +5,15 @@ in
 {
   systemd = {
     # Systemd OOMd
-    # Fedora enables these options by default. See the 10-oomd-* files here:
-    # https://src.fedoraproject.org/rpms/systemd/tree/acb90c49c42276b06375a66c73673ac3510255
     oomd = {
       enable = mkDefault true;
+
+      # Fedora enables these options by default. See the 10-oomd-* files here:
+      # https://src.fedoraproject.org/rpms/systemd/tree/acb90c49c42276b06375a66c73673ac3510255
       enableRootSlice = true;
       enableUserSlices = true;
       enableSystemSlice = true;
-      extraConfig = {
-        "DefaultMemoryPressureDurationSec" = "20s";
-      };
+      extraConfig.DefaultMemoryPressureDurationSec = "20s";
     };
 
     services.nix-daemon.serviceConfig.OOMScoreAdjust = mkDefault 350;
