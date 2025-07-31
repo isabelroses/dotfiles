9d727e6260c106eaa7bbdee4d554489533337387
isabel <isabel@isabelroses.com>
2025-07-31 14:44:10 +0100

    nixos/system/seatd: use the nixpkgs service

--- modules/nixos/system/seatd.nix
+++ modules/nixos/system/seatd.nix
@@ -1,22 +1,6 @@
+{ config, ... }:
 {
-  lib,
-  pkgs,
-  config,
-  ...
-}:
-let
-  inherit (lib) getExe;
-in
-{
-  systemd.services.seatd = {
+  services.seatd = {
     enable = config.garden.profiles.graphical.enable && !config.services.desktopManager.cosmic.enable;
-    description = "Seat management daemon";
-    script = "${getExe pkgs.seatd} -g wheel";
-    serviceConfig = {
-      Type = "simple";
-      Restart = "always";
-      RestartSec = "1";
-    };
-    wantedBy = [ "multi-user.target" ];
   };
 }
