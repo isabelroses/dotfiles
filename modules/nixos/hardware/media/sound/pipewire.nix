042c9b7676ef2a3a8975ebba415ed6a1cbd00078
robin <comfysage@isabelroses.com>
2025-08-03 22:45:33 +0200

    pipewire: create playback loop module

--- modules/nixos/hardware/media/sound/pipewire.nix
+++ modules/nixos/hardware/media/sound/pipewire.nix
@@ -23,6 +23,30 @@ in
         enable = true;
         support32Bit = isx86Linux pkgs;
       };
+
+      configPackages = [
+        (pkgs.writeTextDir "share/pipewire/pipewire.conf.d/10-loopback.conf" ''
+          context.modules = [
+          {   name = libpipewire-module-loopback
+              args = {
+                node.description = "playback loop"
+                audio.position = [ FL FR ]
+                capture.props = {
+                    node.name = playback_sink
+                    node.description = "playback-sink"
+                    media.class = "Audio/Sink"
+                }
+                playback.props = {
+                    node.name = "playback_sink.output"
+                    node.description = "playback-sink-output"
+                    media.class = "Audio/Source"
+                    node.passive = true
+                }
+              }
+          }
+          ]
+        '')
+      ];
     };
 
     systemd.user.services = {
