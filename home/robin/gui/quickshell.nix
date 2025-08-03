e4a6298162b0c4d10f7f719822a2ea51bd807a04
robin <comfysage@isabelroses.com>
2025-08-03 17:57:26 +0200

    robin/quickshell: re-enable quickshell service

--- home/robin/gui/quickshell.nix
+++ home/robin/gui/quickshell.nix
@@ -2,23 +2,24 @@
 let
   inherit (lib) mkIf mkDefault;
   cfg = config.programs.quickshell;
-
 in
 {
-  config = {
-    programs.quickshell = {
-      enable = mkDefault (
-        config.garden.profiles.graphical.enable && config.garden.programs.defaults.bar == "quickshell"
-      );
-    };
+  config =
+    mkIf
+      (config.garden.profiles.graphical.enable && config.garden.programs.defaults.bar == "quickshell")
+      {
+        programs.quickshell = {
+          enable = mkDefault true;
+          systemd.enable = mkDefault true;
+        };
 
-    systemd.user.services.quickshell-lock = mkIf cfg.enable {
-      Unit = {
-        Description = "launch quickshell lock";
-        Before = "lock.target";
+        systemd.user.services.quickshell-lock = mkIf cfg.enable {
+          Unit = {
+            Description = "launch quickshell lock";
+            Before = "lock.target";
+          };
+          Install.WantedBy = [ "lock.target" ];
+          Service.ExecStart = "${lib.getExe cfg.package} ipc call lock lock";
+        };
       };
-      Install.WantedBy = [ "lock.target" ];
-      Service.ExecStart = "${lib.getExe cfg.package} ipc call lock lock";
-    };
-  };
 }
