807a4e64de26483629400ac6248abb345ba67346
isabel <isabel@isabelroses.com>
2025-08-03 21:49:28 +0100

    flake/programs: cleanup imports prefix_order

--- modules/flake/programs/default.nix
+++ modules/flake/programs/default.nix
@@ -1,6 +1,6 @@
 {
   imports = [
-    # keep-sorted start prefix_order=../../,./
+    # keep-sorted start
     ./formatter.nix # formatter for nix fmt, via treefmt is a formatter for every language
     ./shell.nix # a dev shell that provieds all that you will need to work
     # keep-sorted end
