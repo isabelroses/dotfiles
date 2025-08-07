e7335e8dfb5eb225a18d9014dc111b984d46ffd3
isabel <isabel@isabelroses.com>
2025-08-07 17:38:57 +0100

    base/nix: nix plugins

--- modules/base/nix/plugins.nix
+++ modules/base/nix/plugins.nix
@@ -2,12 +2,19 @@
   lib,
   pkgs,
   config,
+  inputs',
   ...
 }:
-{
-  config = lib.mkIf config.garden.profiles.workstation.enable {
-    garden.packages = {
-      inherit (pkgs) lix-diff;
-    };
+
+lib.mkIf config.garden.profiles.workstation.enable {
+  garden.packages = {
+    inherit (pkgs) lix-diff;
+  };
+
+  nix.settings = {
+    # all this work to get some extra maths functions because i love nix repl
+    plugin-files = [
+      (inputs'.tgirlpkgs.packages.lix-math.override { the_lix = config.nix.package; })
+    ];
   };
 }
