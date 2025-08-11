e76b3300456c72ed7c7569669c7e6c0c9bcbe28f
isabel <isabel@isabelroses.com>
2025-08-11 09:57:43 +0100

    nixos/hardware/pipewire: cleanup

--- modules/nixos/hardware/media/sound/pipewire.nix
+++ modules/nixos/hardware/media/sound/pipewire.nix
@@ -24,29 +24,30 @@ in
         support32Bit = isx86Linux pkgs;
       };
 
-      configPackages = [
-        (pkgs.writeTextDir "share/pipewire/pipewire.conf.d/10-loopback.conf" ''
-          context.modules = [
-          {   name = libpipewire-module-loopback
-              args = {
-                node.description = "playback loop"
-                audio.position = [ FL FR ]
-                capture.props = {
-                    node.name = playback_sink
-                    node.description = "playback-sink"
-                    media.class = "Audio/Sink"
-                }
-                playback.props = {
-                    node.name = "playback_sink.output"
-                    node.description = "playback-sink-output"
-                    media.class = "Audio/Source"
-                    node.passive = true
-                }
-              }
-          }
-          ]
-        '')
-      ];
+      extraConfig.pipewire = {
+        "10-loopback" = {
+          "context.modules" = {
+            "node.description" = "playback loop";
+            "audio.position" = [
+              "FL"
+              "FR"
+            ];
+
+            "capture.props" = {
+              "node.name" = "playback_sink";
+              "node.description" = "playback-sink";
+              "media.class" = "Audio/Sink";
+            };
+
+            "playback.props" = {
+              "node.name" = "playback_sink.output";
+              "node.description" = "playback-sink-output";
+              "media.class" = "Audio/Source";
+              "node.passive" = true;
+            };
+          };
+        };
+      };
     };
 
     systemd.user.services = {
