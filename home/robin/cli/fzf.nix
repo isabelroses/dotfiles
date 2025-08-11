6362e7ef9a2794328e27774f3140176f2ed14650
robin <comfysage@isabelroses.com>
2025-08-12 00:13:30 +0200

    robin/fzf: use ev fzf

--- home/robin/cli/fzf.nix
+++ home/robin/cli/fzf.nix
@@ -36,24 +36,5 @@ in
     changeDirWidgetOptions = [
       "--preview 'tree -C {} | head -200'"
     ];
-
-    colors = {
-      fg = "15"; # Text
-      bg = "-1"; # Background
-      "fg+" = "15"; # Text (current line)
-      "bg+" = "8"; # Background (current line)
-      preview-fg = "-1"; # Preview window text
-      preview-bg = "-1"; # Preview window background
-      hl = "9"; # Highlighted substrings
-      "hl+" = "9"; # Highlighted substrings (current line)
-      gutter = "-1"; # Gutter on the left (defaults to bg+)
-      info = "4"; # Info
-      border = "-1"; # Border of the preview window and horizontal separators (--border)
-      prompt = "5"; # Prompt
-      pointer = "15"; # Pointer to the current line
-      marker = "3"; # Multi-select marker
-      spinner = "15"; # Streaming input indicator
-      header = "15"; # Header
-    };
   };
 }
