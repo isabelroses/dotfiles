098db55d835d61ed8251c4a2c226637ea5a3bd37
isabel <isabel@isabelroses.com>
2025-08-20 10:10:08 +0100

    isabel/neovim: use overrides attr

--- home/isabel/tui/neovim.nix
+++ home/isabel/tui/neovim.nix
@@ -1,41 +1,31 @@
 {
-  lib,
   pkgs,
-  inputs,
-  options,
   config,
+  inputs,
   ...
 }:
-let
-  cfg = config.programs.izvim;
-in
 {
   imports = [ inputs.izvim.homeModules.default ];
 
   programs.izvim = {
     inherit (config.garden.profiles.workstation) enable;
-    includePerLanguageTooling = true;
 
-    package = lib.mkForce (
-      options.programs.izvim.package.default.override {
-        inherit (cfg) includePerLanguageTooling;
+    packageOverrides = {
+      nil = (pkgs.nil.override { nix = config.nix.package; }).overrideAttrs (
+        finalAttrs: oa: {
+          src = pkgs.fetchFromGitHub {
+            owner = "oxalica";
+            repo = "nil";
+            rev = "cd7a6f6d5dc58484e62a8e85677e06e47cf2bd4d";
+            hash = "sha256-fK4INnIJQNAA8cyjcDRZSPleA+N/STI6I0oBDMZ2r+E=";
+          };
 
-        nil = (pkgs.nil.override { nix = config.nix.package; }).overrideAttrs (
-          finalAttrs: oa: {
-            src = pkgs.fetchFromGitHub {
-              owner = "oxalica";
-              repo = "nil";
-              rev = "cd7a6f6d5dc58484e62a8e85677e06e47cf2bd4d";
-              hash = "sha256-fK4INnIJQNAA8cyjcDRZSPleA+N/STI6I0oBDMZ2r+E=";
-            };
-
-            cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
-              inherit (finalAttrs) src;
-              hash = "sha256-wvtCLCvpxbUo7VZPExUI7J+U06jnWBMnVuXqJeL/kOI=";
-            };
-          }
-        );
-      }
-    );
+          cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
+            inherit (finalAttrs) src;
+            hash = "sha256-wvtCLCvpxbUo7VZPExUI7J+U06jnWBMnVuXqJeL/kOI=";
+          };
+        }
+      );
+    };
   };
 }
