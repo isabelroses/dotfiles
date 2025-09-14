c581eb9a03a3ae5e8e24540b7b28217eb54dc478
isabel <isabel@isabelroses.com>
2025-09-14 08:37:22 +0100

    home/isabel/theme: don't install fonts

--- home/isabel/themes/fonts.nix
+++ home/isabel/themes/fonts.nix
@@ -1,28 +1,4 @@
 {
-  lib,
-  pkgs,
-  self,
-  config,
-  ...
-}:
-let
-  fontdir =
-    if pkgs.stdenv.hostPlatform.isDarwin then
-      "${config.home.homeDirectory}/Library/Fonts"
-    else
-      "${config.xdg.dataHome}/fonts";
-in
-{
-  home.activation = lib.mkIf config.garden.style.fonts.enable {
-    installCustomFonts =
-      lib.hm.dag.entryAfter [ "writeBoundary" ]
-        # bash
-        ''
-          mkdir -p "${fontdir}"
-          install -Dm644 ${self}/secrets/fonts/* "${fontdir}"
-        '';
-  };
-
   garden.style.fonts = {
     name = "Berkeley Mono";
     package = null;
