2ebebc66ddf56b2a504b8cd36fa710bba57015e7
isabel <isabel@isabelroses.com>
2025-11-27 19:26:03 +0000

    isabel/streaming: use cuda obs

--- home/isabel/gui/streaming.nix
+++ home/isabel/gui/streaming.nix
@@ -1,6 +1,10 @@
 { pkgs, ... }:
 {
   programs.obs-studio = {
+    package = pkgs.obs-studio.override {
+      cudaSupport = true;
+    };
+
     plugins = with pkgs.obs-studio-plugins; [
       wlrobs
       obs-multi-rtmp
