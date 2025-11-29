d0194d498333b5cce5ed6329ab33eab5bbd68b18
isabel <isabel@isabelroses.com>
2025-11-29 22:04:37 +0000

    isabel/mpv: add syncplay

--- home/isabel/gui/mpv.nix
+++ home/isabel/gui/mpv.nix
@@ -1,6 +1,15 @@
-{ config, ... }:
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
 {
   programs.mpv = {
     inherit (config.garden.profiles.graphical) enable;
+  };
+
+  garden.packages = lib.mkIf config.programs.mpv.enable {
+    inherit (pkgs) syncplay;
   };
 }
