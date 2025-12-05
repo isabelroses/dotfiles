eb802d2394ca2c235e3a9b59f4367a48972f092c
isabel <isabel@isabelroses.com>
2025-12-05 12:12:37 +0000

    isabel/syncthing: remove syncthingtray

--- home/isabel/services/syncthing.nix
+++ home/isabel/services/syncthing.nix
@@ -1,21 +1,6 @@
+{ config, ... }:
 {
-  lib,
-  pkgs,
-  config,
-  ...
-}:
-let
-  inherit (lib) mkIf;
-in
-{
-  config = mkIf config.garden.profiles.graphical.enable {
-    services.syncthing = {
-      enable = true;
-
-      tray = {
-        enable = pkgs.stdenv.hostPlatform.isLinux;
-        command = "syncthingtray --wait";
-      };
-    };
+  services.syncthing = {
+    inherit (config.garden.profiles.graphical) enable;
   };
 }
