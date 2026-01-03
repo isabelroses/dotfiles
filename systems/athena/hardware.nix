0a621e75b1486af75fdec5b8f3763407cd15355b
isabel <isabel@isabelroses.com>
2026-01-03 10:15:18 +0000

    systems/athena: mount media ssd

--- systems/athena/hardware.nix
+++ systems/athena/hardware.nix
@@ -9,6 +9,11 @@
       device = "/dev/disk/by-uuid/2524-71ED";
       fsType = "vfat";
     };
+
+    "/media" = {
+      device = "/dev/disk/by-uuid/c4f7c302-f492-47dd-8bfd-e3073c1923bd";
+      fsType = "ext4";
+    };
   };
 
   swapDevices = [ { device = "/dev/disk/by-uuid/e45cd5a5-ec02-4933-9adb-5d968f270f54"; } ];
