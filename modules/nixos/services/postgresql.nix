4121b72abd2d658c15bea718219baede7ff2f3a3
isabel <isabel@isabelroses.com>
2026-01-15 10:34:17 +0000

    nixos/services/postgres: add more advanced configuration options

--- modules/nixos/services/postgresql.nix
+++ modules/nixos/services/postgresql.nix
@@ -45,11 +45,11 @@ in
       # http://pgconfigurator.cybertec.at/;
       # https://git.darmstadt.ccc.de/maralorn/nixos-config/-/blob/master/nixos/roles/matrix-synapse/postgres-tuning.nix
       settings = {
-        # Connectivity;
+        # Connectivity
         max_connections = 100;
         superuser_reserved_connections = 3;
 
-        # Memory Settings;
+        # Memory Settings
         shared_buffers = "1024 MB";
         work_mem = "32 MB";
         maintenance_work_mem = "320 MB";
@@ -58,45 +58,49 @@ in
         effective_io_concurrency = 100; # concurrent IO only really activated if OS supports posix_fadvise function;
         random_page_cost = 1.25; # speed of random disk access relative to sequential access (1.0);
 
-        # Monitoring;
+        # Monitoring
         shared_preload_libraries = "pg_stat_statements"; # per statement resource usage stats
         track_io_timing = "on"; # measure exact block IO times;
         track_functions = "pl"; # track execution times of pl-language procedures if any;
 
-        # Replication;
+        # Replication
         wal_level = "replica"; # consider using at least "replica";
         max_wal_senders = 0;
         synchronous_commit = "on";
 
-        # Checkpointing: ;
+        # Checkpointing
         checkpoint_timeout = "15 min";
         checkpoint_completion_target = 0.9;
         max_wal_size = "1024 MB";
         min_wal_size = "512 MB";
 
-        # WAL writing;
+        # WAL writing
         wal_compression = "on";
         wal_buffers = -1; # auto-tuned by Postgres till maximum of segment size (16MB by default);
         wal_writer_delay = "200ms";
         wal_writer_flush_after = "1MB";
 
-        # Background writer;
+        # Background writer
         bgwriter_delay = "200ms";
         bgwriter_lru_maxpages = 100;
         bgwriter_lru_multiplier = 2.0;
         bgwriter_flush_after = 0;
 
-        # Parallel queries: ;
+        # Parallel queries
         max_worker_processes = 3;
         max_parallel_workers_per_gather = 2;
         max_parallel_maintenance_workers = 2;
         max_parallel_workers = 3;
         parallel_leader_participation = "on";
 
-        # Advanced features ;
+        # Advanced features
         enable_partitionwise_join = "on";
         enable_partitionwise_aggregate = "on";
         jit = "on";
+        max_slot_wal_keep_size = "1000 MB";
+        track_wal_io_timing = "on";
+        maintenance_io_concurrency = 100;
+        wal_recycle = "on";
 
         jit_above_cost = 100000;
         jit_inline_above_cost = 150000;
