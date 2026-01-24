7e4c67f49b6b9a2b0015228de033147b64b897fb
isabel <isabel@isabelroses.com>
2026-01-24 12:50:09 +0000

    isabel/gh: remove gh-copilit

--- home/isabel/cli/gh.nix
+++ home/isabel/cli/gh.nix
@@ -11,7 +11,7 @@
     extensions = lib.attrValues {
       inherit (pkgs)
         # gh-cal # github activity stats in the CLI (not working, maybe we can fix it?)
-        gh-copilot # copilot in the CLI
+        # gh-copilot # copilot in the CLI
         gh-eco # explore the ecosystem
         ;
     };
