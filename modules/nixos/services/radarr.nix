a4bed53b7dcd53e65db75f6e48565256867c84b2
isabel <isabel@isabelroses.com>
2026-01-26 22:29:35 +0000

    nixos/services/{sonarr,radarr}: harden

--- modules/nixos/services/radarr.nix
+++ modules/nixos/services/radarr.nix
@@ -19,9 +19,45 @@ in
     services.radarr = {
       inherit (cfg) enable;
       group = "media";
-      dataDir = "/srv/storage/ranarr";
+      dataDir = "/srv/storage/radarr";
       inherit (config.garden.services.arr) openFirewall;
       settings.server.port = cfg.port;
     };
+
+    systemd.services.radarr.serviceConfig = {
+      CapabilityBoundingSet = "";
+      NoNewPrivileges = true;
+      # ProtectSystem = "strict";
+      ProtectHome = true;
+      ProtectClock = true;
+      ProtectKernelLogs = true;
+      PrivateTmp = true;
+      PrivateDevices = true;
+      PrivateUsers = true;
+      ProtectKernelTunables = true;
+      ProtectKernelModules = true;
+      ProtectControlGroups = true;
+      RestrictSUIDSGID = true;
+      RemoveIPC = true;
+      UMask = "0022";
+      ProtectHostname = true;
+      ProtectProc = "invisible";
+      RestrictAddressFamilies = [
+        "AF_INET"
+        "AF_INET6"
+        "AF_UNIX"
+      ];
+      RestrictNamespaces = true;
+      RestrictRealtime = true;
+      LockPersonality = true;
+      SystemCallArchitectures = "native";
+      SystemCallFilter = [
+        "@system-service"
+        "~@privileged"
+        "~@debug"
+        "~@mount"
+      ];
+    };
+
   };
 }
--- modules/nixos/services/sonarr.nix
+++ modules/nixos/services/sonarr.nix
@@ -23,5 +23,40 @@ in
       inherit (config.garden.services.arr) openFirewall;
       settings.server.port = cfg.port;
     };
+
+    systemd.services.sonarr.serviceConfig = {
+      CapabilityBoundingSet = "";
+      NoNewPrivileges = true;
+      # ProtectSystem = "strict";
+      ProtectHome = true;
+      ProtectClock = true;
+      ProtectKernelLogs = true;
+      PrivateTmp = true;
+      PrivateDevices = true;
+      PrivateUsers = true;
+      ProtectKernelTunables = true;
+      ProtectKernelModules = true;
+      ProtectControlGroups = true;
+      RestrictSUIDSGID = true;
+      RemoveIPC = true;
+      UMask = "0022";
+      ProtectHostname = true;
+      ProtectProc = "invisible";
+      RestrictAddressFamilies = [
+        "AF_INET"
+        "AF_INET6"
+        "AF_UNIX"
+      ];
+      RestrictNamespaces = true;
+      RestrictRealtime = true;
+      LockPersonality = true;
+      SystemCallArchitectures = "native";
+      SystemCallFilter = [
+        "@system-service"
+        "~@privileged"
+        "~@debug"
+        "~@mount"
+      ];
+    };
   };
 }
