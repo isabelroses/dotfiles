497dca23f669fae6a868340a01860af512eea0e0
isabel <isabel@isabelroses.com>
2026-02-09 15:03:35 +0000

    isabel/gui/vicinae: add support for raycast plugins

--- home/isabel/gui/vicinae/extension.nix
+++ home/isabel/gui/vicinae/extension.nix
@@ -7,30 +7,45 @@ lib.extendMkDerivation {
   constructDrv = buildNpmPackage;
 
   extendDrvArgs =
-    _:
+    finalAttrs:
     {
       extName,
       version ? "0",
+      type ? "vicinae",
       ...
     }@args:
-    {
-      pname = args.pname or "vicinae-extension-${extName}";
-      inherit version;
+    lib.checkListOfEnum "${finalAttrs.pname}: type must be one of vicinae or raycast"
+      [ "vicinae" "raycast" ]
+      [ type ]
+      {
+        pname = args.pname or "${type}-extension-${extName}";
+        inherit version;
 
-      src =
-        args.src or (
-          fetchFromGitHub {
-            owner = "vicinaehq";
-            repo = "extensions";
-            rev = "cc3326e7e07b4d2d0aa9ebc1a54ee3b0fb1db469";
-            hash = "sha256-bDC2q3GlDjEE5J2SPHpIdbYKcuLDw3fsxSh3emMOEXU=";
-          }
-          + "/extensions/${extName}"
-        );
+        src =
+          args.src or (
+            if type == "vicinae" then
+              fetchFromGitHub {
+                owner = "vicinaehq";
+                repo = "extensions";
+                rev = "ffbb04567d5108a0fb197aedb7642a0aa6ae7aad";
+                hash = "sha256-1Q/vrarA1M5rIIOZeSmqpC2e33ncpI7dL8AkNIHgtVo=";
+              }
+            else
+              fetchFromGitHub {
+                owner = "raycast";
+                repo = "extensions";
+                rev = "9b5cbcb7204b895e478f58db1485559b7f7d28d8";
+                hash = "sha256-ARrEyBSqw0RSSoRZBCLoiN3Bg1OSKC+uPkwfO29KkfA=";
 
-      dontNpmInstall = true;
-      buildPhase = ''
-        npm run build -- --out=$out
-      '';
-    };
+                # littrally grind to a halt if we don't add this
+                sparseCheckout = [ "/extensions/${extName}" ];
+              }
+          )
+          + "/extensions/${extName}";
+
+        dontNpmInstall = true;
+        buildPhase = ''
+          npm run build -- -o=$out
+        '';
+      };
 }
