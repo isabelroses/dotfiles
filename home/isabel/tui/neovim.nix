d2546ecb9a6a579d1c46e9128861aebb5162b3c4
isabel <isabel@isabelroses.com>
2026-03-04 22:08:06 +0000

    isabel/neovim: add nixd

--- home/isabel/tui/neovim.nix
+++ home/isabel/tui/neovim.nix
@@ -1,31 +1,8 @@
-{
-  pkgs,
-  config,
-  inputs,
-  ...
-}:
+{ config, inputs, ... }:
 {
   imports = [ inputs.izvim.homeModules.default ];
 
   programs.izvim = {
     inherit (config.garden.profiles.workstation) enable;
-
-    packageOverrides = {
-      nil = (pkgs.nil.override { nix = config.nix.package; }).overrideAttrs (
-        finalAttrs: _: {
-          src = pkgs.fetchFromGitHub {
-            owner = "oxalica";
-            repo = "nil";
-            rev = "cd7a6f6d5dc58484e62a8e85677e06e47cf2bd4d";
-            hash = "sha256-fK4INnIJQNAA8cyjcDRZSPleA+N/STI6I0oBDMZ2r+E=";
-          };
-
-          cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
-            inherit (finalAttrs) src;
-            hash = "sha256-wvtCLCvpxbUo7VZPExUI7J+U06jnWBMnVuXqJeL/kOI=";
-          };
-        }
-      );
-    };
   };
 }
