2b6b7903b7e4ecb7b278899ee05ed40b7828ed37
isabel <isabel@isabelroses.com>
2026-03-24 23:40:41 +0000

    isabel/neovim: simplify nil override

--- home/isabel/tui/neovim.nix
+++ home/isabel/tui/neovim.nix
@@ -1,7 +1,7 @@
 {
-  pkgs,
   config,
   inputs,
+  inputs',
   ...
 }:
 {
@@ -11,21 +11,7 @@
     inherit (config.garden.profiles.workstation) enable;
 
     packageOverrides = {
-      nil = (pkgs.nil.override { nix = config.nix.package; }).overrideAttrs (
-        finalAttrs: _: {
-          src = pkgs.fetchFromGitHub {
-            owner = "oxalica";
-            repo = "nil";
-            rev = "cd7a6f6d5dc58484e62a8e85677e06e47cf2bd4d";
-            hash = "sha256-fK4INnIJQNAA8cyjcDRZSPleA+N/STI6I0oBDMZ2r+E=";
-          };
-
-          cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
-            inherit (finalAttrs) src;
-            hash = "sha256-wvtCLCvpxbUo7VZPExUI7J+U06jnWBMnVuXqJeL/kOI=";
-          };
-        }
-      );
+      inherit (inputs'.izlix.packages) nil;
     };
   };
 }
