isabelroses/dotfiles

commit d991d8a8b56a38f4cd1687bb7dc6295616750de4

various: remove cosmic

modules/nixos/system/loginManager.nix
@@ -5,64 +5,32 @@
   ...
 }:
 let
-  inherit (lib)
-    mkIf
-    mkMerge
-    getExe
-    mkOption
-    mkOptionDefault
-    concatStringsSep
-    ;
-  inherit (lib.types) nullOr enum;
+  inherit (lib) getExe concatStringsSep;
 
   sessionData = config.services.displayManager.sessionData.desktops;
   sessionPath = concatStringsSep ":" [
     "${sessionData}/share/xsessions"
     "${sessionData}/share/wayland-sessions"
   ];
-
-  cfg = config.garden.environment.loginManager;
 in
 {
-  options.garden.environment.loginManager = mkOption {
-    type = nullOr (enum [
-      "greetd"
-      "cosmic-greeter"
-    ]);
-    description = "The login manager to be used by the system.";
-  };
+  services.greetd = {
+    enable = true;
+    restart = true;
+    useTextGreeter = true;
 
-  config = mkMerge [
-    {
-      garden.environment.loginManager = mkOptionDefault (
-        if config.garden.profiles.graphical.enable then "greetd" else null
-      );
-    }
-
-    (mkIf (cfg == "greetd") {
-      services.greetd = {
-        enable = true;
-        restart = true;
-        useTextGreeter = true;
-
-        settings = {
-          default_session = {
-            user = "greeter";
-            command = concatStringsSep " " [
-              (getExe pkgs.tuigreet)
-              "--time"
-              "--remember"
-              "--remember-user-session"
-              "--asterisks"
-              "--sessions '${sessionPath}'"
-            ];
-          };
-        };
+    settings = {
+      default_session = {
+        user = "greeter";
+        command = concatStringsSep " " [
+          (getExe pkgs.tuigreet)
+          "--time"
+          "--remember"
+          "--remember-user-session"
+          "--asterisks"
+          "--sessions '${sessionPath}'"
+        ];
       };
-    })
-
-    (mkIf (cfg == "cosmic-greeter") {
-      services.displayManager.cosmic-greeter.enable = true;
-    })
-  ];
+    };
+  };
 }

modules/home/programs/defaults.nix
@@ -51,7 +51,6 @@ in
       type = nullOr (enum [
         "swaylock"
         "gtklock"
-        "cosmic-greeter"
       ]);
       default = "gtklock";
       description = ''

modules/nixos/programs/cosmic.nix
@@ -1,28 +1,0 @@
-{
-  lib,
-  pkgs,
-  config,
-  ...
-}:
-let
-  inherit (lib) mkIf mkEnableOption;
-
-  cfg = config.garden.programs.cosmic;
-in
-{
-  options.garden.programs.cosmic = {
-    enable = mkEnableOption "enable cosmic desktop environment";
-  };
-
-  config = mkIf cfg.enable {
-    services.desktopManager.cosmic.enable = true;
-
-    environment.cosmic.excludePackages = [
-      pkgs.cosmic-edit
-      pkgs.cosmic-term
-      pkgs.cosmic-store
-    ];
-
-    garden.environment.loginManager = "cosmic-greeter";
-  };
-}

modules/nixos/programs/default.nix
@@ -1,7 +1,6 @@
 {
   imports = [
     # keep-sorted start
-    ./cosmic.nix
     ./graphical.nix
     ./hyprland.nix
     # keep-sorted end

modules/nixos/security/polkit.nix
@@ -5,7 +5,6 @@
     polkit.enable = true;
 
     # this should only be installed on graphical systems
-    soteria.enable =
-      config.garden.profiles.graphical.enable && !config.services.desktopManager.cosmic.enable;
+    soteria.enable = config.garden.profiles.graphical.enable;
   };
 }

modules/nixos/system/seatd.nix
@@ -1,6 +1,6 @@
 { config, ... }:
 {
   services.seatd = {
-    enable = config.garden.profiles.graphical.enable && !config.services.desktopManager.cosmic.enable;
+    inherit (config.garden.profiles.graphical) enable;
   };
 }

commit fe9351185fcbbee754eb55bb747b2a1e21a6d478

nixos/system/loginManager: fix accidental enable
bug introduced in d991d8a8b56a38f4cd1687bb7dc6295616750de4

modules/nixos/system/loginManager.nix
@@ -15,7 +15,7 @@ let
 in
 {
   services.greetd = {
-    enable = true;
+    inherit (config.garden.profiles.graphical) enable;
     restart = true;
     useTextGreeter = true;
 