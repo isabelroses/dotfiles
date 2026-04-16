49945c2d6195f0108c2a01ca9efb8b411ba720fd
isabel <isabel@isabelroses.com>
2026-04-16 01:09:46 +0100

    nixos/system/size: don't install speachd by default

--- modules/nixos/system/size.nix
+++ modules/nixos/system/size.nix
@@ -27,4 +27,9 @@ in
 
   # this can allow us to save some storage space
   fonts.fontDir.decompressFonts = true;
+
+  # this enables itself on systems that are graphical. but i don't need it this
+  # module adds pkgs.speachd which is like 700MiB. we still will have
+  # speachd-minmal in our closure due to browsers
+  services.speechd.enable = false;
 }
