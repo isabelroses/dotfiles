693b1689b289857fd14cee48ad343a5de949d77b
isabel <isabel@isabelroses.com>
2026-04-18 18:19:16 +0100

    nixos/security/sudo: reallow passwordless commands
    This reverts commit b3f79051b74d55409e0f3d50a7570c0baafffdcb.

--- modules/nixos/security/sudo.nix
+++ modules/nixos/security/sudo.nix
@@ -1,18 +1,16 @@
-{
-  lib,
-  pkgs,
-  config,
-  ...
-}:
+{ lib, ... }:
 let
-  inherit (lib) getExe getExe';
+  inherit (lib) mkDefault;
 in
 {
   # lets swap out our sudo for sudo-rs just like ubuntu does
   # <https://discourse.ubuntu.com/t/adopting-sudo-rs-by-default-in-ubuntu-25-10/60583>
   security.sudo-rs = {
     enable = true;
-    wheelNeedsPassword = true;
+
+    # wheelNeedsPassword = false means wheel group can execute commands without a password
+    # so just disable it, it only hurt security, BUT ... see below what commands can be run without password
+    wheelNeedsPassword = mkDefault false;
 
     # only allow members of the wheel group to execute sudo
     execWheelOnly = true;
@@ -24,49 +22,5 @@ in
       Defaults env_keep += "EDITOR PATH DISPLAY"
       Defaults timestamp_timeout = 300
     '';
-
-    extraRules = [
-      {
-        groups = [ "wheel" ];
-
-        commands = [
-          # try to make nixos-rebuild work without password
-          {
-            command = getExe config.system.build.nixos-rebuild;
-            options = [ "NOPASSWD" ];
-          }
-          {
-            command = getExe config.nix.package;
-            options = [ "NOPASSWD" ];
-          }
-          {
-            command = getExe' config.nix.package "nix-env";
-            options = [ "NOPASSWD" ];
-          }
-          {
-            command = getExe' config.nix.package "nix-store";
-            options = [ "NOPASSWD" ];
-          }
-          {
-            command = "/nix/store/*/bin/switch-to-configuration";
-            options = [ "NOPASSWD" ];
-          }
-
-          # allow reboot and shutdown without password
-          {
-            command = getExe' pkgs.systemd "systemctl";
-            options = [ "NOPASSWD" ];
-          }
-          {
-            command = getExe' pkgs.systemd "reboot";
-            options = [ "NOPASSWD" ];
-          }
-          {
-            command = getExe' pkgs.systemd "shutdown";
-            options = [ "NOPASSWD" ];
-          }
-        ];
-      }
-    ];
   };
 }
