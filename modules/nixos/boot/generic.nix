4a8c975ca416635d280a32a5a8ba451182921621
isabel <isabel@isabelroses.com>
2026-04-20 16:37:15 +0100

    nixos/boot/generic: explicit remove swraid

--- modules/nixos/boot/generic.nix
+++ modules/nixos/boot/generic.nix
@@ -37,10 +37,6 @@ in
 
     extraModprobeConfig = mkDefault "options hid_apple fnmode=1";
 
-    # whether to enable support for Linux MD RAID arrays
-    # as of 23.11>, this throws a warning if neither MAILADDR nor PROGRAM are set
-    swraid.enable = mkDefault false;
-
     # shared config between bootloaders
     # they are set unless system.boot.loader != none
     loader = {
