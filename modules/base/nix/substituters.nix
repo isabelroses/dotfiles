8dbc1e6ecbfcad080fd222d6ead1e899fd567db8
isabel <isabel@isabelroses.com>
2026-04-21 10:59:34 +0100

    base/nix/substituters: add my cache

--- modules/base/nix/substituters.nix
+++ modules/base/nix/substituters.nix
@@ -19,6 +19,7 @@ in
     # don't worry that you cannot see cache.nixos.org here, it is added by default
     substituters = [
       "https://nix-community.cachix.org" # nix-community cache
+      "https://isabelroses.cachix.org" # my cache
     ]
     ++ optionals hasCtp [
       "https://catppuccin.cachix.org" # a cache for all catppuccin ports
@@ -26,6 +27,7 @@ in
 
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "isabelroses.cachix.org-1:mXdV/CMcPDaiTmkQ7/4+MzChpOe6Cb97njKmBQQmLPM="
     ]
     ++ optionals hasCtp [
       "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
