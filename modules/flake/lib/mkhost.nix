e2f5b8f1a5f30f712e40b6c44856966be69c74a9
isabel <isabel@isabelroses.com>
2026-05-10 11:55:40 +0100

    lib/mkhost: simplify
    mostly just removeing stuff i don't use lol

--- modules/flake/lib/mkhost.nix
+++ modules/flake/lib/mkhost.nix
@@ -5,8 +5,7 @@
 let
   inherit (inputs) self;
 
-  inherit (lib.attrsets) recursiveUpdate mapAttrs;
-  inherit (lib.lists) optionals singleton concatLists;
+  inherit (lib.attrsets) mapAttrs;
 in
 /**
   mkHost is a function that uses withSystem to give us inputs' and self'
@@ -24,8 +23,6 @@ in
     mkHost "myhost" {
       arch = "x86_64";
       class = "nixos";
-      modules = [ ./module.nix ];
-      specialArgs = { foo = "bar"; };
     }
   ```
 */
@@ -33,9 +30,6 @@ name:
 {
   arch ? "x86_64",
   class ? "nixos",
-  modules ? [ ],
-  specialArgs ? { },
-  ...
 }:
 let
   inherit (inputs) darwin nixpkgs;
@@ -50,65 +44,52 @@ let
   system = "${arch}-${os}";
 
   evalHost = if class == "darwin" then darwin.lib.darwinSystem else nixpkgs.lib.nixosSystem;
+
+  # since i'm not using flake.parts at this current time but i still want the
+  # power that comes with using flake.parts i effectively recreate `inputs'`
+  # here. i wrote about this in my blog post:
+  # <https://isabelroses.com/blog/custom-lib-nixossystem/#the-final-touch>
+  inputs' = mapAttrs (_: mapAttrs (_: v: v.${system} or v)) inputs;
 in
 evalHost {
-  # we use recursiveUpdate such that users can "override" the specialArgs
-  #
-  # This should only be used for special arguments that need to be evaluated
-  # when resolving module structure (like in imports).
-  specialArgs = recursiveUpdate {
-    inherit
-      # these are normal args that people expect to be passed,
-      # but we expect to be evaluated when resolving module structure
-      inputs
+  # `specialArgs` should only be used for arguments that need to be evaluated
+  # when resolving module structure (like in imports). for everything else we
+  # have `_module.args`
+  specialArgs = {
+    inherit inputs self;
+  };
 
-      # even though self is just the same as `inputs.self`
-      # we still pass this as some people will use this
-      self
-      ;
-  } specialArgs;
+  modules = [
+    # import our host system paths
+    "${self}/systems/${name}/default.nix"
 
-  modules = concatLists [
-    [
-      # import our host system paths
-      "${self}/systems/${name}/default.nix"
-
-      # import the files required for the class
-      "${self}/modules/${class}/default.nix"
-    ]
-
-    # the next 3 singleton's are split up to make it easier to understand as they do things different things
+    # import the files required for the class
+    "${self}/modules/${class}/default.nix"
 
     # recall `specialArgs` would take be preferred when resolving module structure
     # well this is how we do it use it for all args that don't need to rosolve module structure
-    (singleton (
-      { config, ... }:
-      let
-        inputs' = mapAttrs (_: mapAttrs (_: v: v.${config.nixpkgs.hostPlatform.system} or v)) inputs;
-      in
-      {
-        key = "dotfiles#specialArgs";
-        _file = "${__curPos.file}";
+    {
+      key = "dotfiles#specialArgs";
+      _file = "${__curPos.file}";
 
-        _module.args = {
-          inherit inputs';
-          self' = inputs'.self;
-        };
-      }
-    ))
+      _module.args = {
+        inherit inputs';
+        self' = inputs'.self;
+      };
+    }
 
     # here we make some basic assumptions about the system the person is using
     # like the system type and the hostname
-    (singleton {
+    {
       key = "dotfiles#hostname";
       _file = "${__curPos.file}";
 
       # we set the systems hostname based on the host value
       # which should be a string that is the hostname of the system
       networking.hostName = name;
-    })
+    }
 
-    (singleton {
+    {
       key = "dotfiles#nixpkgs";
       _file = "${__curPos.file}";
 
@@ -123,20 +104,16 @@ evalHost {
         # so that means that we should set it to our nixpkgs flake output path
         flake.source = nixpkgs.outPath;
       };
-    })
+    }
+  ]
+  # if we are on darwin we need to import the nixpkgs source, its used in some
+  # modules, if this is not set then you will get an error
+  ++ lib.optional (class == "darwin") {
+    key = "dotfiles#nixpkgs-darwin";
+    _file = "${__curPos.file}";
 
-    # if we are on darwin we need to import the nixpkgs source, its used in some
-    # modules, if this is not set then you will get an error
-    (optionals (class == "darwin") (singleton {
-      key = "dotfiles#nixpkgs-darwin";
-      _file = "${__curPos.file}";
-
-      # without supplying an upstream nixpkgs source, nix-darwin will not be able to build
-      # and will complain and log an error demanding that you must set this value
-      nixpkgs.source = nixpkgs;
-    }))
-
-    # import any additional modules that the user has provided
-    modules
-  ];
+    # without supplying an upstream nixpkgs source, nix-darwin will not be able to build
+    # and will complain and log an error demanding that you must set this value
+    nixpkgs.source = nixpkgs;
+  };
 }
