983dc001da080057201d2961fb01ede22625b5e9
isabel <isabel@isabelroses.com>
2026-05-22 20:14:08 +0100

    home/environment/xdg: add projects dir

--- modules/home/environment/xdg.nix
+++ modules/home/environment/xdg.nix
@@ -103,6 +103,7 @@ in
       pictures = "${config.home.homeDirectory}/media/pictures";
       publicShare = "${config.home.homeDirectory}/public/share";
       templates = "${config.home.homeDirectory}/public/templates";
+      projects = "${config.home.homeDirectory}/dev";
 
       extraConfig = {
         SCREENSHOTS = "${config.xdg.userDirs.pictures}/screenshots";
