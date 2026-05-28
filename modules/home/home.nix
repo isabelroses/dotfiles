54d99035ac0399a722298065b72cd183ea2c19dd
isabel <isabel@isabelroses.com>
2026-05-28 12:47:37 +0100

    home/home: remove redundant opts

--- modules/home/home.nix
+++ modules/home/home.nix
@@ -1,7 +1,4 @@
-{ lib, osConfig, ... }:
-let
-  inherit (lib.modules) mkDefault;
-in
+{ osConfig, ... }:
 {
   home = {
     stateVersion = osConfig.garden.system.stateVersion;
@@ -9,9 +6,6 @@ in
     # WARNING: this is an experimental option added by my fork of home-manager
     linker.backend = "smfh";
   };
-
-  # reload system units when changing configs
-  systemd.user.startServices = mkDefault "sd-switch"; # or "legacy" if "sd-switch" breaks again
 
   # let HM manage itself when in standalone mode
   programs.home-manager.enable = true;
