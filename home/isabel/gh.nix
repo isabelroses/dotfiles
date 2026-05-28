b1444c9aeb623637e873cf299a26bb4cad46e318
isabel <isabel@isabelroses.com>
2026-05-28 13:56:16 +0100

    isabel/gh: remove extentions

--- home/isabel/gh.nix
+++ home/isabel/gh.nix
@@ -1,20 +1,7 @@
-{
-  lib,
-  pkgs,
-  config,
-  ...
-}:
+{ config, ... }:
 {
   programs.gh = {
     enable = config.programs.git.enable && config.garden.profiles.workstation.enable;
-
-    extensions = lib.attrValues {
-      inherit (pkgs)
-        # gh-cal # github activity stats in the CLI (not working, maybe we can fix it?)
-        # gh-copilot # copilot in the CLI
-        gh-eco # explore the ecosystem
-        ;
-    };
 
     settings = {
       git_protocol = "ssh";
