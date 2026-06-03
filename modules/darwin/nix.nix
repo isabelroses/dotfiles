10bcc9e0b25a802941c117814cc4928595d4e309
isabel <isabel@isabelroses.com>
2026-06-03 14:30:18 +0100

    darwin/nix: don't grab x86_64-darwin binaries

--- modules/darwin/nix.nix
+++ modules/darwin/nix.nix
@@ -1,17 +1,8 @@
 {
-  nix = {
-    # nix gc works slightly differently on darwin, so we need to adjust the
-    # interval such that it works properly here.
-    gc.interval = {
-      Hour = 3;
-      Minute = 15;
-    };
-
-    # we add more platforms here because of the limited number of darwin
-    # maintainers that exist, thus meaning less working packages for darwin.
-    settings.extra-platforms = [
-      "aarch64-darwin"
-      "x86-64-darwin"
-    ];
+  # nix gc works slightly differently on darwin, so we need to adjust the
+  # interval such that it works properly here.
+  nix.gc.interval = {
+    Hour = 3;
+    Minute = 15;
   };
 }
