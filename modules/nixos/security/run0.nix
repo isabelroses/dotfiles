9d85be4011b0b7934a06c209744e8ab1c441a670
isabel <isabel@isabelroses.com>
2026-06-05 17:56:42 +0100

    nixos/security/run0: add run0-sudo-shim

--- modules/nixos/security/run0.nix
+++ modules/nixos/security/run0.nix
@@ -1,11 +1,12 @@
-{ config, ... }:
+{ inputs', ... }:
 {
   security.run0 = {
     # wheelNeedsPassword = false means wheel group can execute commands without
     # a password so just disable it
     wheelNeedsPassword = false;
+  };
 
-    # yeah lets throw that in there too, if sudo is disabled
-    enableSudoAlias = !(config.security.sudo.enable || config.security.sudo-rs.enable);
+  garden.packages = {
+    inherit (inputs'.extersia.packages) run0-sudo-shim;
   };
 }
