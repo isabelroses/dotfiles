b41501c91163906fa9cdea1e6284ea3b44a60987
isabel <isabel@isabelroses.com>
2026-06-09 12:27:37 +0100

    nixos/system/systemd: migrate to rfc 42 style

--- modules/nixos/system/systemd.nix
+++ modules/nixos/system/systemd.nix
@@ -18,12 +18,12 @@ in
         DefaultDeviceTimeoutSec = "15s";
       };
 
-      user.extraConfig = ''
-        DefaultTimeoutStartSec=15s
-        DefaultTimeoutStopSec=15s
-        DefaultTimeoutAbortSec=15s
-        DefaultDeviceTimeoutSec=15s
-      '';
+      user.settings.Manager = {
+        DefaultTimeoutStartSec = "15s";
+        DefaultTimeoutStopSec = "15s";
+        DefaultTimeoutAbortSec = "15s";
+        DefaultDeviceTimeoutSec = "15s";
+      };
 
       services =
         genAttrs
