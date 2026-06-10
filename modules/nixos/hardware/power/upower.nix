01e7298ff6a725aaeec0fcc6d5213c6438395fc8
isabel <isabel@isabelroses.com>
2026-06-10 20:13:58 +0100

    nixos/hardware/power/upower: use enable option

--- modules/nixos/hardware/power/upower.nix
+++ modules/nixos/hardware/power/upower.nix
@@ -1,20 +1,11 @@
+{ config, ... }:
 {
-  lib,
-  config,
-  ...
-}:
-let
-  inherit (lib.modules) mkIf;
-in
-{
-  config = mkIf config.garden.profiles.laptop.enable {
-    # DBus service that provides power management support to applications.
-    services.upower = {
-      enable = true;
-      percentageLow = 15;
-      percentageCritical = 5;
-      percentageAction = 3;
-      criticalPowerAction = "Hibernate";
-    };
+  # DBus service that provides power management support to applications.
+  services.upower = {
+    enable = config.garden.profiles.laptop.enable;
+    percentageLow = 15;
+    percentageCritical = 5;
+    percentageAction = 3;
+    criticalPowerAction = "Hibernate";
   };
 }
