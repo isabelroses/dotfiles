c41963fda7b73466644af45c81789d9627d04f1a
isabel <isabel@isabelroses.com>
2026-06-23 15:21:08 +0100

    nixos/networking/vpn: set packaged; disable some features

--- modules/nixos/networking/vpn.nix
+++ modules/nixos/networking/vpn.nix
@@ -1,15 +1,14 @@
+{ pkgs, config, ... }:
 {
-  lib,
-  pkgs,
-  config,
-  ...
-}:
-{
-  config = lib.mkIf config.garden.profiles.graphical.enable {
-    services.mullvad-vpn.enable = true;
+  services.mullvad-vpn = {
+    inherit (config.garden.profiles.graphical) enable;
 
-    garden.packages = {
-      inherit (pkgs) mullvad mullvad-vpn;
-    };
+    # this is not the default despite what you might think. this is the gui
+    # version of the package
+    package = pkgs.mullvad-vpn;
+
+    # "Might have minor security impact, so consider disabling if you do not use the feature"
+    # well i don't use it so. sure lets do that
+    enableExcludeWrapper = false;
   };
 }
