0e523ad72678a3b9d50c958e07ed1b0d563c1c89
isabel <isabel@isabelroses.com>
2026-07-04 18:57:18 +0100

    isabel/fzf: don't take ctrl-r

--- home/isabel/fzf.nix
+++ home/isabel/fzf.nix
@@ -11,6 +11,9 @@ in
   programs.fzf = {
     inherit (config.garden.profiles.workstation) enable;
 
+    # don't take ctrl-r
+    historyWidget.command = "";
+
     defaultCommand = "${getExe pkgs.fd} --type=f --hidden --exclude=.git";
     defaultOptions = [
       "--height=30%"
