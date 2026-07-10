75f3a022ca102cce0e7a8e4ade18b8b627decd53
isabel <isabel@isabelroses.com>
2026-07-10 15:15:12 +0100

    isabel/qt: remove compat

--- home/isabel/qt.nix
+++ home/isabel/qt.nix
@@ -28,7 +28,7 @@ in
       QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
 
       # remain backwards compatible with qt5
-      DISABLE_QT5_COMPAT = "0";
+      DISABLE_QT5_COMPAT = "1";
 
       # tell calibre to use the dark theme
       CALIBRE_USE_DARK_PALETTE = "1";
