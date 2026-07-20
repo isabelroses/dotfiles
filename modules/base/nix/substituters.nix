1eb7a79869a400a823157602c7b6e6c613f20bad
isabel <isabel@isabelroses.com>
2026-07-20 16:33:34 +0100

    base/nix/substituers: remove nixos cuda cache

--- modules/base/nix/substituters.nix
+++ modules/base/nix/substituters.nix
@@ -7,13 +7,11 @@
     substituters = [
       "https://nix-community.cachix.org" # nix-community cache
       "https://isabelroses.cachix.org" # my cache
-      "https://cache.nixos-cuda.org" # nixpkgs cuda builds
     ];
 
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "isabelroses.cachix.org-1:mXdV/CMcPDaiTmkQ7/4+MzChpOe6Cb97njKmBQQmLPM="
-      "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
     ];
   };
 }
