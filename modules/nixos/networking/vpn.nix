ec05bb09fbad609fc8cd3b356f3a031239216ec1
isabel <isabel@isabelroses.com>
2026-07-31 14:48:50 +0100

    nixos/networking/vpn: use defaults; enable gui

--- modules/nixos/networking/vpn.nix
+++ modules/nixos/networking/vpn.nix
@@ -1,14 +1,12 @@
-{ pkgs, config, ... }:
+{ config, ... }:
 {
   services.mullvad-vpn = {
     inherit (config.garden.profiles.graphical) enable;
 
-    # this is not the default despite what you might think. this is the gui
-    # version of the package
-    package = pkgs.mullvad-vpn;
-
     # "Might have minor security impact, so consider disabling if you do not use the feature"
     # well i don't use it so. sure lets do that
     enableExcludeWrapper = false;
+
+    gui.enable = true;
   };
 }
