54330e36894be89d06edff5953afa77107a1c0eb
isabel <isabel@isabelroses.com>
2026-08-02 20:29:44 +0100

    nixos/hardware/media/video: disable 32 bit

--- modules/nixos/hardware/media/video.nix
+++ modules/nixos/hardware/media/video.nix
@@ -1,18 +1,1 @@
-{
-  self,
-  pkgs,
-  config,
-  ...
-}:
-let
-  inherit (self.lib) isx86Linux;
-in
-{
-  hardware.graphics = {
-    enable = true;
-
-    # 32-bit GL is only ever used by 32-bit userspace (steam, wine), so a
-    # headless system has no use for it no matter which gpu it has
-    enable32Bit = config.garden.profiles.graphical.enable && isx86Linux pkgs;
-  };
-}
+{ hardware.graphics.enable = true; }
