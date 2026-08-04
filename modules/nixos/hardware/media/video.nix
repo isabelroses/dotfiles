69dd57b60afc301b9598596e5d299e6171510428
isabel <isabel@isabelroses.com>
2026-08-04 21:13:42 +0100

    nixos/hardware/media/video: remove gpuless configuration

--- modules/nixos/hardware/media/video.nix
+++ modules/nixos/hardware/media/video.nix
@@ -1,1 +1,9 @@
-{ hardware.graphics.enable = true; }
+{ lib, config, ... }:
+let
+  inherit (lib.modules) mkDefault;
+in
+{
+  hardware.graphics.enable = mkDefault (
+    config.garden.profiles.graphical.enable || config.garden.device.gpu != null
+  );
+}
